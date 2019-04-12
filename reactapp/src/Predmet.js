b785f8d58ce8bc387103989dd3d83ef771043fc0
ntarakcija <nedimt@hotmail.com>
2019-04-12 18:15:46 +0200

    Kreirana komponenta Predmet

---
+++ reactapp/src/Predmet.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class Predmet extends Component {
+    state = {  }
+    render() { 
+        return (
+            <div>
+                
+            </div>
+        );
+    }
+}
+ 
+export default Predmet;

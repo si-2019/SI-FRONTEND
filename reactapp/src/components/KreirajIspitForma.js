b55ea27b2dd2a49d113399b5ecde57a2b80a8c14
amirmu-at-587105673553 <amuminovic3@etf.unsa.ba>
2019-04-12 22:45:55 +0200

    Dodan text field za unos napomene

--- reactapp/src/components/KreirajIspitForma.js
+++ reactapp/src/components/KreirajIspitForma.js
@@ -1,10 +1,16 @@
 import React, {Component} from 'react'
+import './KreirajIspitForma.css'
 
 class KreirajIspitForma extends Component{
   render(){
     return(
     <div>
-      KreirajIspitForma
+      <form>
+        <div className='form-group'>
+          <label htmlFor="ispitnaNapomena">Napomena za ispit</label>
+          <input type="text" className="form-control" id="ispitnaNapomena" placeholder="Nemojte zaboraviti indeks..."/>
+        </div>
+      </form>
     </div>)
   }
 }

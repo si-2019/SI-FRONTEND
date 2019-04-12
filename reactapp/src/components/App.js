05fabfd6005bdaf8f73dff74e115ad1c20c87932
amirmu-at-587105673553 <amuminovic3@etf.unsa.ba>
2019-04-12 22:31:19 +0200

    Kreirana komponenta za prikaz forme za kreiranje ispita

--- reactapp/src/components/App.js
+++ reactapp/src/components/App.js
@@ -1,11 +1,14 @@
 import React from 'react'
 
+import KreirajIspitForma from './KreirajIspitForma'
+
 class App extends React.Component{
 
   render(){
     return(
       <div>
         Hi
+        <KreirajIspitForma/>
       </div>
     )
   }
---
+++ reactapp/src/components/KreirajIspitForma.js
@@ -0,0 +1,12 @@
+import React, {Component} from 'react'
+
+class KreirajIspitForma extends Component{
+  render(){
+    return(
+    <div>
+      KreirajIspitForma
+    </div>)
+  }
+}
+
+export default KreirajIspitForma

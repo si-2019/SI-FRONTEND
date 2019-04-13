c0e5dc68332ea49c78cdfa62067e6f88a0569018
esmagalijatovic <egalijatov1@etf.unsa.ba>
2019-04-13 21:54:48 +0200

    Kreirana pocetna forma

--- reactapp/src/components/App.js
+++ reactapp/src/components/App.js
@@ -2,15 +2,16 @@ import React from 'react'
 
 import KreirajIspitForma from './KreirajIspitForma'
 import InformacijeOIspituForma from './InformacijeOIspituForma/InformacijeOIspituForma';
+import PocetnaForma from './PocetnaForma/PocetnaForma';
 
 class App extends React.Component{
 
   render(){
     return(
       <div className='container'>
-        Hi
+        
         {/* <KreirajIspitForma/> */}
-        <InformacijeOIspituForma />
+        <PocetnaForma />
       </div>
     )
   }
---
+++ reactapp/src/components/PocetnaForma/PocetnaForma.js
@@ -0,0 +1,15 @@
+import React, {Component} from 'react'
+
+class PocetnaForma extends Component{
+  
+  render(){
+    return(
+    <div className='container'>
+      <form>
+            
+      </form>
+    </div>)
+  }
+}
+
+export default PocetnaForma

6272d7a17341e3b0f70c41741e348807fc5d697e
esmagalijatovic <egalijatov1@etf.unsa.ba>
2019-04-13 22:00:21 +0200

    Dodana labela za select

--- reactapp/src/components/PocetnaForma/PocetnaForma.js
+++ reactapp/src/components/PocetnaForma/PocetnaForma.js
@@ -7,12 +7,13 @@ class PocetnaForma extends Component{
     <div className='container'>
       <form>
         <div className='row'>
+            
             <div className='col-3'>
-                
-            </div>
-            <div className='col-3'>
-                
+                <label htmlFor="odabirPredmeta">Odaberite predmet: </label>
             </div>    
+            <div className='col-3'>
+                
+            </div>  
         </div>
       </form>
     </div>)

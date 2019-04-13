700038778541260ffd454568346ab2be1db3ac01
Mirza Delibasic <mdelibasic1@etf.unsa.ba>
2019-04-13 21:24:17 +0200

    Kreiranje Ajax zahtjeva za detalje o odabranom predmetu

--- reactapp/src/Pocetni.js
+++ reactapp/src/Pocetni.js
@@ -19,15 +19,27 @@ class App extends Component {
       );
     else 
       return(
-        <PrikazPredmeta/>
+        <PrikazPredmeta nazivPredmeta={this.state.nazivPredmeta}/>
     )  
   };
   funkcija(){
-    console.log("test");
+    var ajax=new XMLHttpRequest();
+    var komponenta=this;
+    ajax.onreadystatechange=function(){
+      if(ajax.readyState==4 && ajax.status=="200" || true){
+        //var tekst=ajax.responseText;
+        //var json=JSON.parse(tekst);
+        //nakon kreiranja servisa bit će povezan sa formom
+        komponenta.setState({nazivPredmeta:"PIS", forma:"Predmet"});
+      }
+    }
+    ajax.open("GET","#",true);
+    ajax.send();
+    /*
     if(this.state.forma=="Mock"){
       this.setState({
         nazivPredmeta:"ARM",
-          forma:"Prikaz"
+          forma:"Predmet"
       });
     }
     else{
@@ -35,7 +47,7 @@ class App extends Component {
         nazivPredmeta:"SI",
           forma:"Mock"
       });
-    }
+      */
   }
 }
 export default App;

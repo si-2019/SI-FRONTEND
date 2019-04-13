a78d8e78d8e4c4a289573b18852bda766f13a4e8
Envera Husagic <ehusagic1@etf.unsa.ba>
2019-04-13 21:17:23 +0200

    Promjena sadrzaja file-a pocetni

--- reactapp/src/App.js
+++ reactapp/src/App.js
@@ -1,65 +1,27 @@
 import React, { Component } from "react";
-
+import logo from "./logo.svg";
 import "./App.css";
-import Dani from "./dani.js";
 
 class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      dan: "Ponedjeljak"
-    };
-    this.next = this.next.bind(this);
-    this.prev = this.prev.bind(this);
-  }
   render() {
-    return <Dani next={this.next} prev={this.prev} danas={this.state.dan} />;
-  }
-  next() {
-    if (this.state.dan === "Ponedjeljak") {
-      this.setState({
-        dan: "Utorak"
-      });
-    } else if (this.state.dan === "Utorak") {
-      this.setState({
-        dan: "Srijeda"
-      });
-    } else if (this.state.dan === "Srijeda") {
-      this.setState({
-        dan: "Četvrtak"
-      });
-    } else if (this.state.dan === "Četvrtak") {
-      this.setState({
-        dan: "Petak"
-      });
-    } else if (this.state.dan === "Petak") {
-      this.setState({
-        dan: "Ponedjeljak"
-      });
-    }
-  }
-  prev() {
-    if (this.state.dan === "Ponedjeljak") {
-      this.setState({
-        dan: "Petak"
-      });
-    } else if (this.state.dan === "Utorak") {
-      this.setState({
-        dan: "Ponedjeljak"
-      });
-    } else if (this.state.dan === "Srijeda") {
-      this.setState({
-        dan: "Utorak"
-      });
-    } else if (this.state.dan === "Četvrtak") {
-      this.setState({
-        dan: "Srijeda"
-      });
-    } else if (this.state.dan === "Petak") {
-      this.setState({
-        dan: "Četvrtak"
-      });
-    }
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+      </div>
+    );
   }
 }
 

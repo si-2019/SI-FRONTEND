f98592f516f0f4fd3c33227586e11389ca100c9d
esmagalijatovic <egalijatov1@etf.unsa.ba>
2019-04-13 22:33:24 +0200

    Dodani testovi za postojanje forme

---
+++ reactapp/src/components/PocetnaForma/PocetnaForma.test.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {shallow} from 'enzyme';
+import PocetnaForma from './PocetnaForma'
+
+describe('<PocetnaForma />', () => {
+    it('renderuje formu za odabir ispita koji se kreira', () => {
+      const wrapper = shallow(<PocetnaForma/>)
+      expect(wrapper.find('form').exists()).toBe(true)
+    })
+  
+    
+  })

b2bb6e1bb4ff0bc2bf9158a4e76973f8183b2539
esmagalijatovic <egalijatov1@etf.unsa.ba>
2019-04-13 22:09:12 +0200

    dodan test za ispitivanje postojanosti pocetne forme

--- reactapp/src/components/PocetnaForma/PocetnaForma.test.js
+++ reactapp/src/components/PocetnaForma/PocetnaForma.test.js
@@ -3,6 +3,11 @@ import {shallow} from 'enzyme';
 import PocetnaForma from './PocetnaForma'
 
 describe('<PocetnaForma />', () => {
- 
+  it('renderuje formu za odabir ispita koji se kreira', () => {
+    const wrapper = shallow(<PocetnaForma/>)
+    expect(wrapper.find('form').exists()).toBe(true)
+  })
+
+  
 
 })

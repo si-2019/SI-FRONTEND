da190b82e02a125dc3f985a650cbbdcc51d5a566
esmagalijatovic <egalijatov1@etf.unsa.ba>
2019-04-13 22:23:33 +0200

    Pocetna forma red i kolone

---
+++ reactapp/src/components/PocetnaForma/PocetnaForma.js
@@ -0,0 +1,30 @@
+import React, {Component} from 'react'
+
+class PocetnaForma extends Component{
+  
+  render(){
+    return(
+    <div className='container'>
+      <form>
+        <div className='row'>
+            
+            <div className='col-3'>
+                
+            </div> 
+            <div className='col-3'>
+                
+            </div> 
+            <div className='col-3'>
+                
+            </div> 
+            <div className='col-3'>
+                
+            </div>    
+
+        </div>
+      </form>
+    </div>)
+  }
+}
+
+export default PocetnaForma

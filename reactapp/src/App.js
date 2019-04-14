2444feb35a3ae90418ee537081ae5ffc5e7ae378
lamijaalagic <lalagic1@etf.unsa.ba>
2019-04-14 13:53:23 +0200

    Dodana komponenta prikazi listu projekata

--- reactapp/src/App.js
+++ reactapp/src/App.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import PregledZadatakaProjekta from './components/PregledZadatakaProjekta';
+import PregledListeProjekata from './components/PregledListeProjekata';
+
 
 class App extends Component {
   render() {

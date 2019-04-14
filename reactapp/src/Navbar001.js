6213bd23b8aa1ba6adbd0afebd889247ea99adeb
fkalaca1 <fkalaca1@etf.unsa.ba>
2019-04-14 23:52:00 +0200

    Obrisan dummy navbar da bi se implementirao pravi navbar i opcija student

--- reactapp/src/Navbar001.js
+++ reactapp/src/Navbar001.js
@@ -4,14 +4,7 @@ class Navbar001 extends Component {
     render() {
       return (
         <div className="Navbar001">
-            <nav class="navbar navbar-expand-lg navbar-light bg-light">
-                <div class="collapse navbar-collapse" id="navbarNav">
-                    <a class="nav-link" href="#x"> X </a>
-                    <a class="nav-link" href="#y"> Y </a>
-                    <a class="nav-link" href="#z"> Z </a>
-                    <a class="nav-link" href="#w"> W </a>
-                </div>
-            </nav>
+        
         </div>
       );
     }

9cc3e46b1c88c33d7c806da313955e95746ea8bb
Edina <eosmanspah1@etf.unsa.ba>
2019-04-14 12:46:08 +0200

    Kreirana komponenta ReturnButton

---
+++ reactapp/src/ReturnButton.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import Nav from 'react-bootstrap/Nav';
+
+class ReturnButton extends Component {
+  
+    render() {
+      
+      return (
+        <Nav.Link href="/">Nazad na početnu</Nav.Link>
+      )
+    }
+  }
+  export default ReturnButton;

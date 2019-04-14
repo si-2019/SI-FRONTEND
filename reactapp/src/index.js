93dc6bd9de57d75553bf0238fc02d4b2f60bee60
ajajcanin1 <ajajcanin1@etf.unsa.ba>
2019-04-15 00:22:50 +0200

    react-route-dom library added

--- reactapp/src/index.js
+++ reactapp/src/index.js
@@ -2,10 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import 'bootstrap/dist/css/bootstrap.css'
 import './index.css';
-import App from './ProfessorsAvailability/ProfessorsAvailability.js';
+import App from './App.js';
+import ProfessorsAvailability from './ProfessorsAvailability/ProfessorsAvailability'
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
+
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.

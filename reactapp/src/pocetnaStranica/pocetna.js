5c66e173523060bae4db1b87c14c80822815394e
nejiram <37294883+nejiram@users.noreply.github.com>
2019-04-14 16:17:21 +0200

    Kreirana komponenta pocetna.js

--- reactapp/src/pocetnaStranica/pocetna.js
+++ reactapp/src/pocetnaStranica/pocetna.js
@@ -1,13 +1,19 @@
 import React, { Component } from 'react';
-
-class App extends Component {
+import './pocetna.css';
+class AnketePocetna extends Component {
   render() {
     return (
       <div className="App">
-        <h1>Pocetna</h1>
+        <header className="App-header">
+          <h1> ANKETE</h1>
+        </header>
+         <button className="App-edit" id = "kreiranje"><h1>Kreiraj anketu</h1></button>
+        <button className="App-edit" id = "popunjavanje"><h1>Popuni anketu</h1></button>
+        <button className="App-edit" id = "mojeAnkete"><h1>Moje ankete</h1></button>
+        <button className="App-edit" id = "javneAnkete"><h1>Javne ankete</h1></button>
+        <button className="App-edit" id = "rezultati"><h1>Rezultati anketa</h1></button>
       </div>
     );
   }
 }
-
-export default App;
+export default AnketePocetna;

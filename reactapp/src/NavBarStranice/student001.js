815a2f58bd5ee290baaf3590f3d932b7225c988f
fkalaca1 <fkalaca1@etf.unsa.ba>
2019-04-15 01:53:15 +0200

    Stranica za studente (pocetna verzija) student001.js - Izmjena2

--- reactapp/src/NavBarStranice/student001.js
+++ reactapp/src/NavBarStranice/student001.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import Header from '../Header';
 import Footer from '../Footer';
-import TabelaStudenti from '../TabelaStudenti';
 import Navbar001 from '../Navbar001';
 
 class student001 extends Component {

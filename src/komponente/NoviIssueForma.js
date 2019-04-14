c9e1428e7027a1bc3b68535b232715c416a382db
Ajna Zatric <azatric1@etf.unsa.ba>
2019-04-14 23:08:14 +0200

    Obavjestenje o nepopunjenosti polja

--- src/komponente/NoviIssueForma.js
+++ src/komponente/NoviIssueForma.js
@@ -1,49 +1,93 @@
 import React from 'react';
+import {Container, Row, Col} from 'react-bootstrap'
 
+class NoviIssueForma extends React.Component {
 
-function NoviIssueForma(props){
+    constructor(props) {
+        super(props);
 
-    function zatvoriNoviIssue(){
-        document.getElementById('overlay').style.display = "none";
+        this.state = {
+            issueText: ''
+        }
     }
 
-    return (
+    zatvoriNoviIssue = () => {
+        document.getElementById('overlay').style.display = "none";
+    };
 
-        <div>
-            <form>       
 
-                <div className="row" style={{marginTop:'10px'}}>
-                    <label className="col-1" style={{marginLeft:'12px', marginTop:'6px'}}>Naslov</label>
-                    <select className="form-control col-9" id="naslovSelect">
-                        <option>Problem 1</option>
-                        <option>Problem 2</option>
-                        <option>Problem 3</option>
-                        <option>Problem 4</option>
-                        <option>Problem 5</option>
-                    </select>
+    onChangeIssueText = (object) => {
+        this.setState({issueText: object.target.value})
+    };
 
-                    <button onClick={zatvoriNoviIssue} type="button" className="btn btn-danger float-right" style={{marginLeft:'85px', width:'50px'}}>X</button>
-                </div>
 
-                <div className="form-group row col-12" style={{marginTop:'5px', marginLeft:'2px'}}>                    
-                    <textarea className="form-control" id="exampleTextarea" rows="10"></textarea>
-                </div>
+    render() {
+        let warning;
+        if (this.state.issueText === '') {
+            warning = (
+                <Container>
+                    <Row>
+                        <Col md={{ span: 6, offset: 7 }}>
+                            <label style={{color: '#C40D0D'}}>
+                                Ispunite polje za opis issue-a
+                            </label>
+                        </Col>
+                    </Row>
+                </Container>
+            );
+        }
+        return (
+            <div>
+                <form>
 
-                <div className="form-group row col-12">
+                    <div className="row" style={{marginTop: '10px'}}>
+                        <label className="col-1" style={{marginLeft: '12px', marginTop: '6px'}}>Naslov</label>
+                        <select className="form-control col-9" id="naslovSelect">
+                            <option>Problem 1</option>
+                            <option>Problem 2</option>
+                            <option>Problem 3</option>
+                            <option>Problem 4</option>
+                            <option>Problem 5</option>
+                        </select>
 
-                    <div className="custom-file col-8">
-                        
-                        <input type="file" class="form-control-file" id="exampleInputFile" aria-describedby="fileHelp" />
-                                               
+                        <button onClick={this.zatvoriNoviIssue} type="button" className="btn btn-danger float-right"
+                                style={{marginLeft: '85px', width: '50px'}}>X
+                        </button>
                     </div>
 
-                    <button type="submit" class="btn btn-outline-primary col-2">Save as draft</button>
-                    <button type="submit" class="btn btn-outline-primary col-2">Send</button>           
+                    <div className="form-group row col-12" style={{marginTop: '5px', marginLeft: '2px'}}>
+                        <textarea
+                            className="form-control"
+                            id="exampleTextarea"
+                            rows="10"
+                            value={this.state.issueText}
+                            onChange={this.onChangeIssueText}
+                        />
+                    </div>
 
-                </div>
-            </form>
-        </div>
-    );
+                    <div className="form-group row col-12">
+
+                        <div className="custom-file col-8">
+
+                            <input type="file" class="form-control-file" id="exampleInputFile"
+                                   aria-describedby="fileHelp"/>
+
+                        </div>
+
+                        <button type="submit" class="btn btn-outline-primary col-2">Save as draft</button>
+                        <button
+                            type="submit"
+                            class="btn btn-outline-primary col-2"
+                            disabled={!this.state.issueText}
+                        >
+                            Send
+                        </button>
+                        {warning}
+                    </div>
+                </form>
+            </div>
+        );
+    }
 }
 
 export default NoviIssueForma;

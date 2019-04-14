547ae54dd44136c06643a59fc7bdbfc20c8ab2ad
fkalaca1 <fkalaca1@etf.unsa.ba>
2019-04-15 01:34:55 +0200

    Opcija Materijali

--- reactapp/src/Navbar001.js
+++ reactapp/src/Navbar001.js
@@ -15,7 +15,7 @@ class Navbar001 extends Component {
             <Nav.Link href="raspored">Raspored</Nav.Link>
             <Nav.Link href="chat">Chat</Nav.Link>
             <Nav.Link href="forum">Forum</Nav.Link>
-            
+            <Nav.Link href="materijali">Materijali</Nav.Link>
         </Nav>
       );
     }

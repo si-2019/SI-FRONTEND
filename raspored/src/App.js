91984e69665cb4a0a14edc06b485179e23f2532f
gobathegreat <agobeljic1@etf.unsa.ba>
2019-04-14 17:37:35 +0200

    Zavrsen raspored

--- raspored/src/App.js
+++ raspored/src/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { Suspense, Fragment, memo , Component} from "react";
 import Cell from './components/cell.js';
 import uuid from 'uuid';
 import axios from 'axios';
@@ -6,23 +6,191 @@ import Table_head_cell from './components/table_head_cell.js';
 import './App.css';
 import { runInThisContext } from 'vm';
 import Bootstrap from 'bootstrap/dist/css/bootstrap.css';
+import { unstable_createResource } from "react-cache";
+
+const FetcherTermini = unstable_createResource(() =>
+  fetch("http://localhost:3001/getTermini").then(r => r.json())
+);
+
+const FetcherIspiti = unstable_createResource(() =>
+  fetch("http://localhost:3001/getIspiti").then(r => r.json())
+);
+
+const getDataTermini = () => FetcherTermini.read();
+const getDataIspiti = () => FetcherIspiti.read();
+const Helper = () => {
+  var spisakTermina=getDataTermini();
+  var spisakIspita = getDataIspiti();
+
+  var raspored=[];
+
+  spisakIspita.forEach((val, index) => {
+    raspored.push(val);
+   });
+   spisakTermina.forEach((val, index) => {
+    raspored.push(val);  
+  });
+
+  raspored.sort(sortCriteria);
+  var vremenaRasporeda=[];
+  var rendering=[];
 
 
-class App extends Component {
-  state=
-  {    
-    raspored:[
 
-    ],
-    vremenaRasporeda:[
+       //Ovo dalje se desava nakon getanja ispita i termina
+
+       var vrijemeObaveze='08:00';
+       while(vrijemeObaveze!='21:00')
+        {
+          vremenaRasporeda.push(vrijemeObaveze);                   
+          vrijemeObaveze=povecajVrijemePolaSata(vrijemeObaveze);
+          vrijemeObaveze=povecajVrijemePolaSata(vrijemeObaveze);
+        }
+
+       raspored.forEach((val,index) => {
+
+        vrijemeObaveze = val.vrijeme;
+        let trajanjeObaveze = parseInt(val.trajanje);
+        while(trajanjeObaveze!=0)
+        {
+          if(!vremenaRasporeda.includes(vrijemeObaveze) && vrijemeObaveze==val.vrijeme)
+          {
+            vremenaRasporeda.push(vrijemeObaveze);            
+          }          
+          trajanjeObaveze-=30;          
+          vrijemeObaveze=povecajVrijemePolaSata(vrijemeObaveze);
+          
+        }
+       });
+       
+       vremenaRasporeda.sort();
+
+
+       let danas= new Date();
+       var dd = String(danas.getDate()).padStart(2, '0');
+       var mm = String(danas.getMonth() + 1).padStart(2, '0'); //Januar je 0!
+       var yyyy = danas.getFullYear();   
+       let datum = yyyy + '/' + mm + '/' + dd;
+       datum = prviDanuSedmici(datum);
+
+       let matricaTermina =new Array(vremenaRasporeda.length);
+       for (var i = 0; i < matricaTermina.length; i++) {
+          matricaTermina[i] = new Array(7);
+          for(var j=0;j<matricaTermina[i].length;j++)
+            matricaTermina[i][j]=-1;
+       }
+       let datumFix = datum;
+
+       raspored.forEach((val,index) => {
+         datum=datumFix;
+         for(let dan=0;dan<7;dan++)
+         {
+          if(val.datum==datum)
+          {
+            
+            let lokalnoTrajanje = val.trajanje;
+            let indeksVremena=-1;
+            for(let j=0;j<vremenaRasporeda.length;j++)
+            {
+              if(val.vrijeme==vremenaRasporeda[++indeksVremena])
+              {
+                break;
+              }  
+            }
+            while(lokalnoTrajanje!=0)
+            {    
+                  
+              matricaTermina[indeksVremena][dan]=index;
+              let razdaljinaMinute = 0;
+              if(indeksVremena==vremenaRasporeda.length-1)
+              break;
+
+              if(vremenaRasporeda[indeksVremena][3]!=vremenaRasporeda[indeksVremena+1][3])
+              razdaljinaMinute=30;
+              else
+              razdaljinaMinute=60;
+
+              lokalnoTrajanje-=razdaljinaMinute;
+              indeksVremena++;
+            }
+            break; 
+          }
+          datum=sljedeciDan(datum);
+         }
+         
+
+        
+       });
+
+
+
+       
+       
+      for (var i=0; i < vremenaRasporeda.length; i++) {
+        let cells=[];
+        for(let u=0;u<7;u++)
+        {
+          if(matricaTermina[i][u]==-1)
+          {
+            // ako celija nije ispunjena
+            cells.push(<Cell key={(u+1)*10+i} dan={(u+1).toString()} redniBroj={i} termin={undefined} ></Cell>);
+          }
+          else
+          {
+            cells.push(<Cell key={(u+1)*10+i} dan={(u+1).toString()} redniBroj={i} termin={raspored[matricaTermina[i][u]]} ></Cell>);
+          }
+        }
+
+        rendering.push(
+            <tr>
+              <td style={pocetnaKolonaStyle}>{vremenaRasporeda[i]}</td>
+              {cells}            
+            </tr>
+          );
+      }
+
+      danas= new Date();
+       var dd = String(danas.getDate()).padStart(2, '0');
+       var mm = String(danas.getMonth() + 1).padStart(2, '0'); //Januar je 0!
+       var yyyy = danas.getFullYear();   
+    datum = yyyy + '/' + mm + '/' + dd;
+    datum = prviDanuSedmici(datum);
+    var headerRow = [];
+       days.forEach((day,index) => {
+
+         headerRow.push(
+          <Table_head_cell key={day.id} day={day.title} datum={datum}/>
+         );
+         datum = sljedeciDan(datum);
+
+       });
+    
+    console.log(rendering);
+
+    return (
       
-    ],
-    rendering:[]
-  }
+     
+ 
+      <table>
+      <tbody>  
+        <tr>
 
-  
-  
-  sortCriteria = (a,b) =>
+          <th style={pocetnaKolonaStyle}>Vrijeme</th>            
+          {headerRow}
+          </tr>  
+          
+          {rendering}
+        </tbody>
+      </table>             
+
+);
+
+
+
+    
+}
+
+const sortCriteria = (a,b) =>
   {  
     // sortiramo hronoloski  
       if ((a.datum<b.datum) || (a.datum===b.datum && a.vrijeme <b.vrijeme))
@@ -32,7 +200,7 @@ class App extends Component {
       return 1;    
   }
 
- povecajVrijemePolaSata(vrijeme)
+const povecajVrijemePolaSata = (vrijeme) =>
   {
     // vrijeme je string
     // pretpostavljamo da je vrijeme polusatno npr. 09:30 ili 17:00
@@ -65,7 +233,7 @@ class App extends Component {
     return vrijeme; 
   }
 
-  sljedeciDan(datum) // funkcija vraca datum sljedeceg dana
+  const sljedeciDan = (datum) =>// funkcija vraca datum sljedeceg dana
   {
     var today = new Date(datum);
     var tomorrow = new Date();
@@ -81,7 +249,7 @@ class App extends Component {
 
     
   }
-  prethodniDan(datum) // funkcija vraca datum prethodnog dana
+  const prethodniDan = (datum) =>// funkcija vraca datum prethodnog dana
   {
     var today = new Date(datum);
     var yesterday = new Date();
@@ -97,13 +265,13 @@ class App extends Component {
     
   }
 
-  prviDanuSedmici(datum) //funkcija vraca prvi dan u sedmici u kojoj je taj datum
+  const prviDanuSedmici = (datum) => //funkcija vraca prvi dan u sedmici u kojoj je taj datum
   {
     let datumNovi = new Date(datum);
     let brojIteracija = datumNovi.getDay()-1;    
     while(brojIteracija!=0)
     {
-      datum=this.prethodniDan(datum);
+      datum=prethodniDan(datum);
       brojIteracija--;
     }
     return datum;
@@ -111,313 +279,25 @@ class App extends Component {
 
 
 
-  createRaspored = () =>
-  {
-    var spisakTermina=[
-      {
-          id:  1,
-          title:'Predavanje',
-          predmet:'Projektovanje informacionih sistema',
-          datum:'2019/04/08', 
-          vrijeme:'12:00',
-          sala:'MA',
-          trajanje:'120'
-      },
-      {
-          id:  2,
-          title:'Tutorijal',
-          predmet:'Projektovanje informacionih sistema',
-          datum:'2019/04/08', 
-          vrijeme:'17:00',
-          sala:'MA',
-          trajanje:'60'
-      },
-      {
-          id:  3,
-          title:'Predavanje',
-          predmet:'Softver Inzinjering',
-          datum:'2019/04/09', 
-          vrijeme:'09:00',
-          sala:'S1',
-          trajanje:'180'
-      },
-      {
-          id:  4,
-          title:'Vjezbe',
-          predmet:'Projektovanje informacionih sistema',
-          datum:'2019/04/10', 
-          vrijeme:'18:00',
-          sala:'S5',
-          trajanje:'120'
-      },
-      {
-        id:  5,
-        title:'Predavanje',
-        predmet:'Administracija racunarskih mreza',
-        datum:'2019/04/11', 
-        vrijeme:'09:00',
-        sala:'S5',
-        trajanje:'180'
-      },
-      {
-        id:  6,
-        title:'Predavanje',
-        predmet:'Vjestacka inteligencija',
-        datum:'2019/04/11', 
-        vrijeme:'15:00',
-        sala:'S1',
-        trajanje:'120'
-      },
-      {
-        id:  7,
-        title:'Vjezbe',
-        predmet:'Vjestacka inteligencija',
-        datum:'2019/04/11', 
-        vrijeme:'20:00',
-        sala:'1-02',
-        trajanje:'60'
-      },
-      {
-        id:  8,
-        title:'Vjezbe',
-        predmet:'Administracija racunarskih mreza',
-        datum:'2019/04/12', 
-        vrijeme:'09:00',
-        sala:'1-03',
-        trajanje:'120'
-      }
-  ]
-  var spisakIspita=[/*
-    {
-        id:  1,
-        title:'Usmeni ispit',
-        predmet:'Administracija racunarskih mreza',
-        datum:'2019/04/11', 
-        vrijeme:'14:30',
-        sala:'S10',
-        trajanje:'120'
-    },
-    {
-        id:  2,
-        title:'Prvi parcijalni ispit',
-        predmet:'Softver Inzinjering',
-        datum:'2019/04/11', 
-        vrijeme:'13:00',
-        sala:'MA',
-        trajanje:'90'
-    },
-    {
-        id:  3,
-        title:'Usmeni ispit',
-        predmet:'Tehnike programiranja',
-        datum:'2019/04/11', 
-        vrijeme:'17:30',
-        sala:'S9',
-        trajanje:'30'
-    },
-    {
-        id:  4,
-        title:'Prvi parcijalni ispit',
-        predmet:'Linearna algebra i geometrija',
-        datum:'2019/04/11', 
-        vrijeme:'09:30',
-        sala:'VA',
-        trajanje:'180'
-    },
-    {
-        id:  5,
-        title:'Drugi parcijalni ispit',
-        predmet:'Objektno orijentisana analiza i dizajn',
-        datum:'2019/04/12', 
-        vrijeme:'14:30',
-        sala:'S8',
-        trajanje:'120'
-    }*/
-]
-spisakIspita.forEach((val, index) => {
-  this.state.raspored.push(val);
- });
- spisakTermina.forEach((val, index) => {
-  this.state.raspored.push(val);  
-});
-
-    /*var spisakIspita;
-    var spisakTermina;
-    axios.get("http://localhost:3001/getIspiti").then(function (response) {
-      // handle success
-      let odzivIspiti = response.request.response;
-      spisakIspita=JSON.parse(odzivIspiti); 
-      spisakIspita.forEach((val, index) => {
-        this.state.raspored.push(val);
-       });
-       
-      axios.get("http://localhost:3001/getTermini").then(function (response) {
-       // handle success
-       let odzivTermini = response.request.response;
-       spisakTermina=JSON.parse(odzivTermini);   
-       spisakTermina.forEach((val, index) => {
-        this.state.raspored.push(val);  
-      });   */ 
-       this.state.raspored.sort(this.sortCriteria);
-
-
-       //Ovo dalje se desava nakon getanja ispita i termina
-
-       var vrijemeObaveze='08:00';
-       while(vrijemeObaveze!='21:00')
-        {
-          this.state.vremenaRasporeda.push(vrijemeObaveze);                   
-          vrijemeObaveze=this.povecajVrijemePolaSata(vrijemeObaveze);
-          vrijemeObaveze=this.povecajVrijemePolaSata(vrijemeObaveze);
-        }
-
-       this.state.raspored.forEach((val,index) => {
-
-        vrijemeObaveze = val.vrijeme;
-        let trajanjeObaveze = parseInt(val.trajanje);
-        while(trajanjeObaveze!=0)
-        {
-          if(!this.state.vremenaRasporeda.includes(vrijemeObaveze) && vrijemeObaveze==val.vrijeme)
-          {
-            this.state.vremenaRasporeda.push(vrijemeObaveze);            
-          }          
-          trajanjeObaveze-=30;          
-          vrijemeObaveze=this.povecajVrijemePolaSata(vrijemeObaveze);
-          
-        }
-       });
-       
-       this.state.vremenaRasporeda.sort();
-
-
-       let danas= new Date();
-       var dd = String(danas.getDate()).padStart(2, '0');
-       var mm = String(danas.getMonth() + 1).padStart(2, '0'); //Januar je 0!
-       var yyyy = danas.getFullYear();   
-       let datum = yyyy + '/' + mm + '/' + dd;
-       datum = this.prviDanuSedmici(datum);
-
-       let matricaTermina =new Array(this.state.vremenaRasporeda.length);
-       for (var i = 0; i < matricaTermina.length; i++) {
-          matricaTermina[i] = new Array(7);
-          for(var j=0;j<matricaTermina[i].length;j++)
-            matricaTermina[i][j]=-1;
-       }
-       let datumFix = datum;
-
-       this.state.raspored.forEach((val,index) => {
-         datum=datumFix;
-         for(let dan=0;dan<7;dan++)
-         {
-          if(val.datum==datum)
-          {
-            
-            let lokalnoTrajanje = val.trajanje;
-            let indeksVremena=-1;
-            for(let j=0;j<this.state.vremenaRasporeda.length;j++)
-            {
-              if(val.vrijeme==this.state.vremenaRasporeda[++indeksVremena])
-              {
-                break;
-              }  
-            }
-            while(lokalnoTrajanje!=0)
-            {    
-                  
-              matricaTermina[indeksVremena][dan]=index;
-              let razdaljinaMinute = 0;
-              if(indeksVremena==this.state.vremenaRasporeda.length-1)
-              break;
-
-              if(this.state.vremenaRasporeda[indeksVremena][3]!=this.state.vremenaRasporeda[indeksVremena+1][3])
-              razdaljinaMinute=30;
-              else
-              razdaljinaMinute=60;
-
-              lokalnoTrajanje-=razdaljinaMinute;
-              indeksVremena++;
-            }
-            break; 
-          }
-          datum=this.sljedeciDan(datum);
-         }
-         
-
-        
-       });
-
-
-
-       
-       
-      for (var i=0; i < this.state.vremenaRasporeda.length; i++) {
-        let cells=[];
-        for(let u=0;u<7;u++)
-        {
-          if(matricaTermina[i][u]==-1)
-          {
-            // ako celija nije ispunjena
-            cells.push(<Cell key={(u+1)*10+i} dan={(u+1).toString()} redniBroj={i} termin={undefined} ></Cell>);
-          }
-          else
-          {
-            cells.push(<Cell key={(u+1)*10+i} dan={(u+1).toString()} redniBroj={i} termin={this.state.raspored[matricaTermina[i][u]]} ></Cell>);
-          }
-        }
-
-        this.state.rendering.push(
-            <tr>
-              <td style={pocetnaKolonaStyle}>{this.state.vremenaRasporeda[i]}</td>
-              {cells}            
-            </tr>
-          );
-      }
-    /*}.bind(this)) 
-    }.bind(this)) */ 
-  }
-
   
-render() {
-    this.createRaspored(); 
-    console.log(this.state.raspored);
 
-    let danas= new Date();
-       var dd = String(danas.getDate()).padStart(2, '0');
-       var mm = String(danas.getMonth() + 1).padStart(2, '0'); //Januar je 0!
-       var yyyy = danas.getFullYear();   
-    let datum = yyyy + '/' + mm + '/' + dd;
-    datum = this.prviDanuSedmici(datum);
-    var headerRow = [];
-       days.forEach((day,index) => {
-
-         headerRow.push(
-          <Table_head_cell key={day.id} day={day.title} datum={datum}/>
-         );
-         datum = this.sljedeciDan(datum);
-
-       });
+class App extends Component {
+  state=
+  {    
     
-    console.log(this.state.rendering);
+  }
+  
+render = () =>{    
     
     return (
-      
-     
- 
-                <table>
-                <tbody>  
-                  <tr>
-     
-                    <th style={pocetnaKolonaStyle}>Vrijeme</th>            
-                    {headerRow}
-                    </tr>  
-                    
-                    {this.state.rendering}
-                  </tbody>
-                </table>             
+      <Fragment>    
+    <Suspense fallback={<div>Loading...</div>}>
+    <Helper/>
+    
+    </Suspense>
+  </Fragment>          
         
     )
-      
-      
   }
 }
 
@@ -461,4 +341,6 @@ const days =[
 
 
 
-export default App;
+export default React.memo(App);
+
+
--- raspored/src/components/cell.js
+++ raspored/src/components/cell.js
@@ -2,28 +2,61 @@ import React, { Component } from 'react'
 
 export class Cell extends Component {
   render() {
+    if(this.props.redniBroj%2==0)
+    {
       if(this.props.termin==undefined)
       return (
-        <td style={tdStyle}>
+        <td style={tdStyleParan}>
         </td>);
         else 
     return (
-          <td style={tdStyle}>{this.props.termin.datum + ' ' + this.props.termin.predmet + ' ' +  this.props.termin.title + this.props.termin.vrijeme}
+          <td style={tdStyleParan}><b>{this.props.termin.title + ' '}</b>{'('+ this.props.termin.sala+')'} {this.props.termin.predmet}
 
           </td>
         
      
     )
+    }
+    else
+    {
+      if(this.props.termin==undefined)
+      return (
+        <td style={tdStyleNeparan}>
+        </td>);
+        else 
+    return (
+          <td style={tdStyleNeparan}><b>{this.props.termin.title + ' '}</b>{'('+ this.props.termin.sala+')'} {this.props.termin.predmet}
+
+          </td>
+        
+     
+    )
+    }
+  
+
+     
   }
 }
 
-const tdStyle=
+const tdStyleNeparan=
 {
+    rowspan:'5',
     border:'2px solid #504c4c',
     textAlign:'center',
     padding:'8px',
     textAlign: 'left',
     width:'10vw', 
     backgroundColor: 'white' 
+}
+
+const tdStyleParan=
+{
+    rowspan:'5',
+    border:'2px solid #504c4c',
+    textAlign:'center',
+    padding:'8px',
+    textAlign: 'left',
+    width:'10vw', 
+    backgroundColor:'lightgoldenrodyellow' 
 }
 export default Cell

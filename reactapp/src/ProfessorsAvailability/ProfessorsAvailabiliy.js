a374d8d6a853bb70da02f66705aade6df7c1ce81
ajajcanin1 <ajajcanin1@etf.unsa.ba>
2019-04-14 03:56:28 +0200

    bootstrap imported

--- reactapp/src/ProfessorsAvailability/ProfessorsAvailabiliy.js
+++ reactapp/src/ProfessorsAvailability/ProfessorsAvailabiliy.js
@@ -1,31 +1,33 @@
-import React, { Component } from 'react';
+import React, {Component, Fragment} from 'react';
 
 var professorsList = ['Profesor 1', 'Profesor 2', 'Profesor 3', 'Profesor 4', 'Profesor 5'];
 class ProfessorsAvailabiliy extends Component {
     render() {
         const professors = professorsList.map(prof => {
             return(
-                <React>
-                <td>{prof}</td>
-                    <td><i className="material-icons">test</i></td>
-                </React>
+                <Fragment>
+                    <tr>
+                        <td className="col-11">{prof}</td>
+                        <td className="col-1"><i className="material-icons">edit</i></td>
+                    </tr>
+                </Fragment>
             )
         });
         return (
-            <div>
-                <table className="professors-table">
-                    <thead>
-                    <tr>
-                        <th className="col-xs-11">Name</th>
-                        <th className="col-xs-1"></th>
-                    </tr>
-                    </thead>
-                    <tbody>
-                    <tr>
-                        {professors}
-                    </tr>
-                    </tbody>
-                </table>
+            <div className="container">
+                <div className="row">
+                    <table className="table table-bordered professors-table">
+                        <thead>
+                        <tr>
+                            <th className="col-11">Name</th>
+                            <th className="col-1"></th>
+                        </tr>
+                        </thead>
+                        <tbody>
+                            {professors}
+                        </tbody>
+                    </table>
+                </div>
             </div>
         );
     }

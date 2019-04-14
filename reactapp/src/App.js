52a5e5a2a3f885b6149bb18038c481943d9b44e9
Selma Vučijak <svucijak1@etf.unsa.ba>
2019-04-14 23:16:38 +0200

    Dodana komponenta za prikaz nepolozenih predmeta

--- reactapp/src/App.js
+++ reactapp/src/App.js
@@ -1,25 +1,40 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 import './App.css';
+import PredmetiSaDrugihSemestara from'./components/predmetiSaDrugihSemestara';
+import SpisakProfesora from './components/spisakProfesora';
+import NepolozeniPredmeti from './components/spisakNepolozenihPredmeta';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <header>Tareka cekamo</header>
+        <Container>
+        <Row>
+          <Col> <PredmetiSaDrugihSemestara></PredmetiSaDrugihSemestara> </Col>
+          <Col> <SpisakProfesora></SpisakProfesora></Col>
+          <Col> <NepolozeniPredmeti></NepolozeniPredmeti></Col>
+        </Row>
+        <Row>
+          <Col>Ilma</Col>
+          <Col>Azra</Col>
+          <Col>Ferhad</Col>
+        </Row>
+        <Row>
+          <Col>Belmin</Col>
+          <Col>Merima</Col>
+          <Col>ne koristimo sad</Col>
+        </Row>
+        <Row>
+          <Col>ne koristimo sad</Col>
+          <Col>ne koristimo sad</Col>
+          <Col>ne koristimo sad</Col>
+        </Row>
+      </Container>
+      <footer> Footer </footer>
       </div>
     );
   }
---
+++ reactapp/src/components/spisakNepolozenihPredmeta.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+class NepolozeniPredmeti extends Component{
+    render() {
+        return (
+            <div>
+                <ul className="list-group">
+                    <li className="list-group-item active"><h4 className="font-weight-bold" >Nepolozeni predmeti</h4></li>
+                    <li className="list-group-item">Osnove operacionih istrazivanja</li>
+                </ul>
+            </div>
+        );
+    }    
+}
+
+export default NepolozeniPredmeti;

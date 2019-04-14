58ad4ec819f2b63c569a0e6ef243ae2157112049
ajajcanin1 <ajajcanin1@etf.unsa.ba>
2019-04-15 00:32:46 +0200

    added routing

--- reactapp/src/index.js
+++ reactapp/src/index.js
@@ -1,12 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Route, Link, BrowserRouter as Router } from 'react-router-dom'
 import 'bootstrap/dist/css/bootstrap.css'
 import './index.css';
 import App from './App.js';
 import ProfessorsAvailability from './ProfessorsAvailability/ProfessorsAvailability'
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const routing = (
+    <Router>
+        <div>
+            <Route exact path="/" component={App} />
+            <Route path="/schedule" component={ProfessorsAvailability} />
+        </div>
+    </Router>
+);
+
+ReactDOM.render(routing, document.getElementById('root'));
 
 
 // If you want your app to work offline and load faster, you can change

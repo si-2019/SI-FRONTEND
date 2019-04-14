97598849a3d6e8301b151ff7c33bb5aef05ccd1a
fkalaca1 <fkalaca1@etf.unsa.ba>
2019-04-14 23:12:53 +0200

    Preimenovana opcija X u Ispit

--- reactapp/src/Navbar001.js
+++ reactapp/src/Navbar001.js
@@ -6,7 +6,7 @@ class Navbar001 extends Component {
         <div className="Navbar001">
             <nav class="navbar navbar-expand-lg navbar-light bg-light">
                 <div class="collapse navbar-collapse" id="navbarNav">
-                    <a class="nav-link" href="#x"> X </a>
+                    <a class="nav-link" href="#x"> Ispit </a>
                     <a class="nav-link" href="#y"> Y </a>
                     <a class="nav-link" href="#z"> Z </a>
                     <a class="nav-link" href="#w"> W </a>

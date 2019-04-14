67ef1af6bdae46d6d48b90addc041ece68cfdba5
fkalaca1 <fkalaca1@etf.unsa.ba>
2019-04-14 23:08:29 +0200

    Navbar importovan u stranicu predmeta

--- reactapp/src/StranicaPredmeta.js
+++ reactapp/src/StranicaPredmeta.js
@@ -12,7 +12,6 @@ class StranicaPredmeta extends Component {
         <br></br>
         <h1>Stranica predmeta</h1>
         <br></br>
-        <Navbar001/>
         <TabelaStudenti/>
         <Footer/>
       </div>

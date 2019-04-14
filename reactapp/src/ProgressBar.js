a6f06a7b798e481ee7c71a1377934e4d0e92d154
Amila Fejzic <amila.fejzic@foreo.com>
2019-04-14 23:12:31 +0200

    Dodan progress bar prikaz bodova iz zadaca i projekata

---
+++ reactapp/src/ProgressBar.js
@@ -0,0 +1,34 @@
+import React, { Component } from 'react';
+
+const ProgressBar = (props) => {
+    return (
+    <div className="progressBar"> 
+         <Filler percentage = {props.percentage}/>
+    </div>
+    )
+}
+
+const Filler = (props) => {
+    return  (
+    <div className="filler" style={{ width: 185}}>
+    <label>{props.percentage}%</label> </div>)
+}
+
+class ProgressbarZadace extends React.Component  {
+    constructor(props) {
+        super(props)
+        this.state = {
+            percentage: 50
+        }
+    }
+    render(){
+        return (
+            <div className="bodovi-zadace">
+                <label className="zadaceLabela"> Zadaće i projekti </label>
+                <ProgressBar percentage={this.state.percentage} />
+                <button className="zadaceButton"> Zadaće </button>
+            </div>
+        )
+    }
+}
+export default ProgressbarZadace

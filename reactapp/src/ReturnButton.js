12ead1f5381594e3f53f864bdaebd8bc167b3760
Edina <eosmanspah1@etf.unsa.ba>
2019-04-14 12:48:17 +0200

    Modifikovana komponenta ReturnButton

--- reactapp/src/ReturnButton.js
+++ reactapp/src/ReturnButton.js
@@ -1,13 +1,19 @@
 import React, { Component } from 'react';
 import Nav from 'react-bootstrap/Nav';
+import {withRouter} from 'react-router-dom';
 
 class ReturnButton extends Component {
   
     render() {
-      
+      var currentLocation = this.props.location.pathname;
+      if(currentLocation=="/"){
+        return (
+          <Nav.Link disabled href="/">Nazad na početnu</Nav.Link>
+        )
+      }
       return (
         <Nav.Link href="/">Nazad na početnu</Nav.Link>
       )
     }
   }
-  export default ReturnButton;
+  export default withRouter(ReturnButton);

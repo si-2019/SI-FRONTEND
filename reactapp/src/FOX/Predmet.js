20b425be3f6f19b2d57f1e5dc1e2c9f7e588075b
Ajdina Memisevic <amemisevic1@etf.unsa.ba>
2019-04-17 10:55:43 +0200

    Dodan link na naziv grupa

--- reactapp/src/FOX/Predmet.js
+++ reactapp/src/FOX/Predmet.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import Nav from 'react-bootstrap/Nav';
 
 class Predmet extends Component {
     state = {  }
@@ -10,12 +11,34 @@ class Predmet extends Component {
                 </div>
                 <div>
                     <ul>
-                        <li>Grupa1</li>
+                    <li>Grupa1</li>
+                    <Nav className="mr-auto">
+                         <Nav.Link href="predmet">Grupa1</Nav.Link>
+                    </Nav>
                         <li>Grupa2</li>
+                    <Nav className="mr-auto">
+                         <Nav.Link href="predmet">Grupa2</Nav.Link>
+                    </Nav>
                         <li>Grupa3</li>
+                    <Nav className="mr-auto">
+                         <Nav.Link href="predmet">Grupa3</Nav.Link>
+                    </Nav>
                         <li>Grupa4</li>
+                    <Nav className="mr-auto">
+                         <Nav.Link href="predmet">Grupa4</Nav.Link>
+                    </Nav>
                         <li>Grupa5</li>
+                    <Nav className="mr-auto">
+                         <Nav.Link href="predmet">Grupa5</Nav.Link>
+                    </Nav>
                         <li>Grupa6</li>
+                    <Nav className="mr-auto">
+                         <Nav.Link href="predmet">Grupa6</Nav.Link>
+                    </Nav>
+                        <li>Grupa7</li>a
+                    <Nav className="mr-auto">
+                         <Nav.Link href="predmet">Grupa7</Nav.Link>
+                    </Nav>
 
                     </ul>
                 </div>

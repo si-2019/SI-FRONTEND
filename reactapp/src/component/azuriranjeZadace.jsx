e44eaf182aaa6a56808c7b8ede333ab7482f12c0
ekovac2 <ekovac2@etf.unsa.ba>
2019-04-21 00:08:36 +0200

    dodan dropdown

--- reactapp/src/component/azuriranjeZadace.jsx
+++ reactapp/src/component/azuriranjeZadace.jsx
@@ -1,6 +1,8 @@
 import React, { Component } from 'react'
 import {BrowserRouter, Route, Link} from 'react-router-dom';
+import { ButtonDropdown, DropdownToggle, DropdownMenu, DropdownItem, Form } from 'reactstrap';
 import KreiranjeZadace from './kreiranjeZadace';
+
 
 class AzuriranjeZadace extends Component {
     
@@ -19,8 +21,24 @@ class AzuriranjeZadace extends Component {
           sviBodoviIsti: "",
           listaBodova: "",
           ukupnoBodova: "",
+          listaZadaca: ["Zadaća 1", "Zadaća 2", "Zadaća 3", "Zadaća 4"]
           }
-        }
+
+
+          this.toggle = this.toggle.bind(this);
+            this.state = {
+            dropdownOpen: false
+        };
+    }
+
+    toggle() {
+        this.setState({
+          dropdownOpen: !this.state.dropdownOpen
+        });
+    }
+    
+
+        
     
     setAllState = () => {
         // poziv na bazu i popunjavanje state-a
@@ -29,12 +47,30 @@ class AzuriranjeZadace extends Component {
     render() {
         return(
         <div>
+            <Form>
+            <div className="card-header  mb-4">
+                <h4>
+                    <ButtonDropdown isOpen={this.state.dropdownOpen} toggle={this.toggle}>
+                        <DropdownToggle caret>
+                            Lista zadaća za ažuriranje
+                        </DropdownToggle>
+                        <DropdownMenu>
+                            <DropdownItem>Another Action</DropdownItem>
+                            <DropdownItem>Another Action</DropdownItem>
+                        </DropdownMenu>
+                    </ButtonDropdown>
+                </h4>
+            </div>
+            
+
+
             <BrowserRouter>
                 <Link to="/KILO/kreiranjeZadace/?idPredmeta=3">
                     Kreiranje    
                 </Link>
                 <Route path='/KILO/kreiranjeZadace/' exact component={KreiranjeZadace}/>
             </BrowserRouter>
+            </Form>
         </div>)
     }
 }

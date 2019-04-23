5e7089c0139fd2806488ba2b7778a21279340a92
gobathegreat <agobeljic1@etf.unsa.ba>
2019-04-24 01:31:03 +0200

    Editovan bug, sintaksicka greska

--- raspored/src/components/body_cell.js
+++ raspored/src/components/body_cell.js
@@ -188,12 +188,12 @@ export class Body_Cell extends Component {
           if(!isTrueSet)
           {
             // Uspjesno brisanje
-            this.state.setState({signalString:"Uspjesno izbrisano"});
+            this.setState({signalString:"Uspjesno izbrisano"});
           }
           else
           {
             // Neuspjesno brisanje
-            this.state.setState({signalString:"Desila se greska pri brisanju"});
+            this.setState({signalString:"Desila se greska pri brisanju"});
           }
           console.log(response);
          }.bind(this));  

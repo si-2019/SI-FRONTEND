66a28994907bf4ddf41bac2bccf4406bc82ba430
nimsirovic1 <nimsirovic1@etf.unsa.ba>
2019-04-27 22:27:35 +0200

    Kreirana componentDidMount() funkcija

--- reactapp/src/CHARLIE/components/KreiraniIspiti/KreiraniIspiti.js
+++ reactapp/src/CHARLIE/components/KreiraniIspiti/KreiraniIspiti.js
@@ -1,10 +1,22 @@
 import React from 'react'
+import axios from 'axios'
 
 class KreiraniIspiti extends React.Component{
+  state = {response:[]}
+
+  async componentDidMount(){ //Data sta renda
+    const {data} = await axios.get('http://localhost:3001/api/ispit')
+    this.setState({response:data})
+  }
+
+ 
+
   render(){
+    
     return(
       <div>
         Kreirani ispiti
+          
       </div>
     )
   }

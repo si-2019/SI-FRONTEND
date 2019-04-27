d509dac68d92b5b76a638b70339ba8429a5fce02
amirmu-at-587105673553 <amuminovic3@etf.unsa.ba>
2019-04-27 11:44:16 +0200

    Dodan unit test za error icon

--- reactapp/src/CHARLIE/components/SharedComponents/Form.test.js
+++ reactapp/src/CHARLIE/components/SharedComponents/Form.test.js
@@ -14,14 +14,14 @@ describe("<Form/> tests", () => {
     expect(component.find("[className='form-group']").exists()).toBe(true);
   });
 
-  it("shouldn't render the alert initially", () => {
+  it("shouldn't render an alert initially", () => {
     component.setState({ validationError: false });
     expect(component.find("[className='alert alert-danger']").exists()).toBe(
       false
     );
   });
 
-  it("shouldn't render the alert initially", () => {
+  it("should render an alert when error is set to true", () => {
     component.setState({
       validationError: true,
       validationErrorMessage: "Validation failed"
@@ -29,5 +29,15 @@ describe("<Form/> tests", () => {
     expect(component.find("[className='alert alert-danger']").exists()).toBe(
       true
     );
+  });
+
+  it("should render an alert when error is set to true", () => {
+    component.setState({
+      validationError: true,
+      validationErrorMessage: "Validation failed"
+    });
+    expect(
+      component.find("[className='fas fa-exclamation-triangle']").exists()
+    ).toBe(true);
   });
 });

0d7ddac0d35651af50f7a77f19389b16486ee2c8
Amina Salcin <asalcin2@etf.unsa.ba>
2019-04-28 15:40:22 +0200

    Pribavljanje i prikaz informacija o kreatoru

--- reactapp/src/HOTEL/app.js
+++ reactapp/src/HOTEL/app.js
@@ -5,7 +5,6 @@ import Kreiranje from './kreiranje/app'
 import Popunjavanje from './popunjavanje/app.js'
 import Rezultati from './rezultati/app.js'
 import Liste from './liste/app.js'
-
 export default class Hotel extends React.Component {
     render() {
         return (
@@ -13,7 +12,7 @@ export default class Hotel extends React.Component {
                 <div>
                     <Route exact path="/hotel" component={Home} />
                     <Route path="/hotel/kreiranje/" component={Kreiranje} />
-                    <Route path="/hotel/popunjavanje" component={Popunjavanje} />
+                    <Route path="/hotel/popunjavanje/:id" component={Popunjavanje} />
                     <Route path="/hotel/rezultati" component={Rezultati} />
                     <Route path="/hotel/liste" component={Liste} />
                 </div>

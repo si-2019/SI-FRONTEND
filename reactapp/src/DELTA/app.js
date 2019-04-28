d3365f437dd5807285627d6a915deaf34d506899
Karim Alomerovic <kalomerovi1@etf.unsa.ba>
2019-04-29 00:43:45 +0200

    Dobavljanje i prikaz podatak o ispitima

--- reactapp/src/DELTA/app.js
+++ reactapp/src/DELTA/app.js
@@ -1,10 +1,14 @@
 import React, { Component } from 'react';
+import Labele from './components/Labele/Labele'
+import BodoviNaIspitima from './components/BodoviNaIspitima/BodoviNaIspitima'
 
 class App extends Component {
   render() {
     return (
       <div>
         <h1>DELTA</h1>
+        <Labele/><br/>
+        <BodoviNaIspitima/>
       </div>
     );
   }
---
+++ reactapp/src/DELTA/components/Labele/Labele.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react'
+
+export class Labele extends Component {
+  render() {
+    return (
+      <div className = "labele">
+        <h3 style = {labelaStil}>Ime profesora: </h3> <h3 className = "imeProfesora" style = {tekstStil}>Dummy Ime</h3> <br/>
+
+        <h3 style = {labelaStil}>Broj ETCS bodova: </h3> <h3 className = "brojETCSBodova" style = {tekstStil}>Dummy ETCS</h3> <br/>
+
+        <h3 style = {labelaStil}>Grupa: </h3> <h3 className = "grupa" style = {tekstStil}>Dummy grupa</h3> <br/>
+
+        <h3 style = {labelaStil}>Opis predmeta: </h3> <h3 className = "opisPredmeta" style = {tekstStil}>Dummy opis</h3> <br/>
+
+      </div>
+    )
+  }
+}
+
+const labelaStil = {
+    display: 'inline'
+}
+
+const tekstStil = {
+    display: 'inline'
+}
+
+export default Labele

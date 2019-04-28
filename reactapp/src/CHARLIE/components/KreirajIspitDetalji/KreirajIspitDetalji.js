da02a347b838162f882712f2396b3f10f311f64e
Nedim Memisevic <nmemisevic1@etf.unsa.ba>
2019-04-28 23:18:38 +0200

    Dodano dugme za spašavanje ispita

--- reactapp/src/CHARLIE/components/KreirajIspitDetalji/KreirajIspitDetalji.js
+++ reactapp/src/CHARLIE/components/KreirajIspitDetalji/KreirajIspitDetalji.js
@@ -1,21 +1,37 @@
 import React, { Component } from "react";
 import Form from "../SharedComponents/Form";
+import { Link } from 'react-router-dom';
 
 class KreirajIspitDetalji extends Component {
   state = { napomenaGreska: false };
 
   render() {
     return (
-      <div className="col-4">
-        <form>
-          <Form
-            autoFocus
-            labelTitle="Napomena za ispit"
-            id="ispitnaNapomena"
-            placeholder="Nemojte zaboraviti indeks..."
-            validations={["required"]}
-          />
-        </form>
+      <div className="container">
+        <div className="row">
+          <div className="col-4">
+            <form>
+              <Form
+                autoFocus
+                labelTitle="Napomena za ispit"
+                id="ispitnaNapomena"
+                placeholder="Nemojte zaboraviti indeks..."
+                validations={["required"]}
+              />
+            </form>
+          </div>
+        <div className="col-4">
+          <form>
+            <Link to="/charlie">
+              <button type="button" class="btn btn-danger">Odustani</button>
+            </Link>
+            <Link to="/charlie">
+              <button type="button" class="btn btn-primary">Kreiraj ispit</button>
+            </Link>
+          </form>
+        </div>
+        </div>
+        
       </div>
     );
   }

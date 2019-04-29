c88d2d32bb4e89d45573e3fed1313f70895fd9a6
ddupanovic1 <ddupanovic1@etf.unsa.ba>
2019-04-29 05:03:46 +0200

    Dodana opcija pribavljanja tema iz bp

--- reactapp/src/TANGO/components/ListaTema/listaTema.js
+++ reactapp/src/TANGO/components/ListaTema/listaTema.js
@@ -3,40 +3,90 @@ import LISTA_TEMA from './LISTA.js';
 import {
   withRouter
 } from 'react-router-dom'
+import { resolve } from 'path';
+
+const themesApi= 'http://localhost:31919/getThemes/'; //plus id teme
+const korisnikApi='http://localhost:31919/getUser/'; //plus id korisnika
+
 class Tema extends Component{
   render(){
-    console.log(this.props);
-    const {nazivTeme, slika,korisnickoIme,brojKomentara,datumStvaranja}=this.props.tema;
-    console.log(nazivTeme);
+    
+    const {idTeme,idPredmeta, idUser,description,title, timeCreated,brojKomentara}=this.props.tema;
+    //const {nazivTeme, slika,korisnickoIme,brojKomentara,datumStvaranja}=this.props.tema;
     return(
-      <div className='tema'>
-          <div className='slika_ime'>
-            <img src={slika} />
-            <p>{korisnickoIme}</p>
-          </div>
-           <div className='naziv_teme' >
-                <p>{nazivTeme}</p>
-                <input  className='stickyDugme' type='button' value="S"/>
-            </div>
-              <div className='datum_komentari'>
-                <p className='brKom' >
-                    Br. komentara: {brojKomentara}
-                </p>
-                <p className='datStv'>
-                    {datumStvaranja}
-                </p>
-              </div>
+    <div>
+      <div className='naziv_teme' >
+        <p>{title}</p>
+        <input  className='stickyDugme' type='button' value="S"/>
       </div>
+      <div className='datum_komentari'>
+        <p className='brKom' >
+            Br. komentara: {brojKomentara}
+        </p>
+        <p className='datStv'>
+            {timeCreated}
+        </p>
+      </div>  
+    </div>  
+    );
+  }
+}
+
+class Korisnik extends Component{
+  
+  render(){
+    const {id,ime,prezime, fotografija}= this.props.korisnik;
+    return(
+      <div className='slika_ime'>
+            <img src={fotografija} />
+            <p>{ime}</p>
+          </div>
     );
   }
 }
 class Lista extends Component{
+  constructor() {
+    super();
+    this.state = {
+      teme: [],
+      ucitvanje:false,
+      ucitvanjeKorisnika:false
+    };
+  }
+  componentWillMount(){
+    this.setState({ucitvanje:true});
+    fetch(themesApi+'1')
+      .then(response=>response.json())
+      .then(niz=>this.setState({teme:niz,ucitvanje:false}));
+  }
+
+  nadjiKorisnika = (idKorisnika) => {
+    this.setState({ucitvanjeKorisnika:true});
+    fetch(korisnikApi+idKorisnika)
+      .then(response=>response.json())
+      .then((korisnik)=>{this.setState({ucitvanjeKorisnika:false});resolve(korisnik);});
+  }
+
+  
   render(){
+    if(this.state.ucitvanje){
+      return <p>Ucitavanje...</p>
+    }
     return(
       //ovo je lista tema koju dobijemo sa servera
-      LISTA_TEMA.map(TEMA =>{
-          return (<Tema key={TEMA.id} tema={TEMA}/>);  }) 
-        )
+      this.state.teme.map(TEMA =>{
+        
+        return (
+          <div className='tema'>
+            
+             <div className='slika_ime'>
+                {/* <Korisnik korisnik={korisnik}/> */}
+              </div>
+              <Tema key={TEMA.idTheme} tema={TEMA}/>
+          </div>
+        );    
+        })
+      );
   }
 }
 

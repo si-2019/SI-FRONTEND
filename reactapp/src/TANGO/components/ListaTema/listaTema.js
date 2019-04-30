72e976228e87576575f89d73df3f8268192fc028
amasovic1 <amasovic1@etf.unsa.ba>
2019-04-30 13:46:23 +0200

    Sortiranje

--- reactapp/src/TANGO/components/ListaTema/listaTema.js
+++ reactapp/src/TANGO/components/ListaTema/listaTema.js
@@ -4,11 +4,59 @@ import {
   withRouter
 } from 'react-router-dom'
 class Tema extends Component{
+	constructor() {
+          super();
+          this.state = {
+            povratak:false
+          };
+          this.sortirajAZ=this.sortirajAZ.bind(this);
+          this.sortirajZA=this.sortirajZA.bind(this);
+          this.sortirajPoBrojuKomentara=this.sortirajPoBrojuKomentara.bind(this);
+          this.sortirajPoDatumuKreiranja=this.sortirajPoDatumuKreiranja.bind(this);
+        }
+      povratak=false;
+      sortirajAZ(){// = evt => {
+          LISTA_TEMA.sort(function(a,b){
+            var textA = a.nazivTeme.toUpperCase();
+            var textB = b.nazivTeme.toUpperCase();
+            return (textA < textB) ? -1 : (textA > textB) ? 1 : 0;
+          });
+          console.log(LISTA_TEMA);
+      }
+      sortirajZA(){
+          LISTA_TEMA.sort(function(a,b){
+                      var textA = a.nazivTeme.toUpperCase();
+                      var textB = b.nazivTeme.toUpperCase();
+                      return (textA > textB) ? -1 : (textA < textB) ? 1 : 0;
+                    });
+          console.log(LISTA_TEMA);
+      }
+      sortirajPoBrojuKomentara(){
+        LISTA_TEMA.sort(function(a,b){
+                      var textA = a.brojKomentara
+                      var textB = b.brojKomentara
+                      return (textA < textB) ? -1 : (textA > textB) ? 1 : 0;
+                    });
+        console.log(LISTA_TEMA);
+      }
+      sortirajPoDatumuKreiranja(){
+        LISTA_TEMA.sort(function(a,b){
+                      var textA = a.datumStvaranja;
+                      var textB = b.datumStvaranja;
+                      return (textA < textB) ? -1 : (textA > textB) ? 1 : 0;
+                    });
+        console.log(LISTA_TEMA);
+      }
   render(){
     console.log(this.props);
     const {nazivTeme, slika,korisnickoIme,brojKomentara,datumStvaranja}=this.props.tema;
     console.log(nazivTeme);
     return(
+	  <div className='container'>
+	        <button onClick={this.sortirajAZ} class="list-group-item list-group-item-action">Sorziraj A-Z</button>
+	        <button onClick={this.sortirajZA} class="list-group-item list-group-item-action">Sorziraj Z-A</button>
+	        <button onClick={this.sortirajPoBrojuKomentara} class="list-group-item list-group-item-action">Sorziraj po broju komentara</button>
+	        <button onClick={this.sortirajPoDatumuKreiranja} class="list-group-item list-group-item-action">Sorziraj po datumu</button>
       <div className='tema'>
           <div className='slika_ime'>
             <img src={slika} />
@@ -26,6 +74,7 @@ class Tema extends Component{
                     {datumStvaranja}
                 </p>
               </div>
+      </div>
       </div>
     );
   }

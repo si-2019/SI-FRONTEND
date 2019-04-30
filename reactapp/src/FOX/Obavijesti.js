27d2b2da3dd86d87c8b9bf451c7a980415867d09
Sejla Pljakic <sejla.pljakic@gmail.com>
2019-04-30 02:29:53 +0200

    Rendering

--- reactapp/src/FOX/Obavijesti.js
+++ reactapp/src/FOX/Obavijesti.js
@@ -2,3 +2,13 @@ import React, {Component } from 'react';
 import Header from './Header';
 import Footer from './Footer';
 
+class Obavijesti extends Component {
+    render() {
+        return (
+          <div className="Obavijesti">
+          </div>
+
+        );
+    }
+
+}

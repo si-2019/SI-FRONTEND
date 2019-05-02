c1fe9fccdc00de0e7dc00fbd9a339abd8fc39c23
smamela <smamela2@etf.unsa.ba>
2019-05-02 21:48:52 +0200

    Kreiranje stranice Moji Predmeti

--- reactapp/src/GOLF/app.js
+++ reactapp/src/GOLF/app.js
@@ -1,10 +1,13 @@
 import React, { Component } from 'react';
+import {BrowserRouter as Router, Route, Link} from 'react-router-dom'
+
 
 class App extends Component {
   render() {
     return (
       <div>
         <h1>GOLF</h1>
+
       </div>
     );
   }

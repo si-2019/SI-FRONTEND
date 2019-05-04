7272807b9deddc5eb275f2d48419cf5e3661fc91
amujak1 <amujak1@etf.unsa.ba>
2019-05-04 17:23:18 +0200

    kreirana komponenta objavaStudent

---
+++ reactapp/src/GOLF/objavaStudent.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class objavaStudent extends Component {
+  render() {
+    return (
+        <div class="card">
+            <div class="card-body">
+            
+            </div>
+        </div>
+    );
+  }
+}
+
+export default objavaStudent;

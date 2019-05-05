68296030e27216cb354db9e55f5f797d5fdf5bd1
arogo1 <arnes_rogo@hotmail.com>
2019-05-05 15:35:15 +0200

    Dodavanje RI odsjeka u listu odsjeka

---
+++ reactapp/src/GOLF/odsjeci.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+
+class Smjer extends Component {
+    render(){
+        return(
+            <div id="drugi">
+                <ul class="list-group">
+                    <li class="list-group-item d-flex justify-content-between align-items-center" onClick = {() => this.Otvori(1)}>
+                        Računarstvo i informatika
+                    </li>
+                </ul>
+            </div>
+        )
+    }
+}

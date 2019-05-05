0032a9981f6fd3c1a86dd4bb6c697c94727a740c
fsehic1 <fsehic1@etf.unsa.ba>
2019-05-05 11:22:15 +0200

    Dodani i stilizovani inputi i submit dugme

--- reactapp/src/components/Login/Login.js
+++ reactapp/src/components/Login/Login.js
@@ -1,10 +1,44 @@
 import React, { Component } from 'react';
+import './login.css';
 
 class Login extends Component {
+    state = {
+        njumas: [
+            {
+                id: 2,
+                text: 'Ah što ćemo'
+            },
+            {
+                id: 142,
+                text: 'Kad ja moram'
+            }
+        ]
+    }
+
+  componentWillMount() {
+    document.title = 'Login stranica'
+  }
+
   render () {
     return (
-      <div className="Login">
-        <h1>Login</h1>
+      <div id="body">
+        <div id="main">
+          <form>
+            <label htmlFor="korisnickoIme">Korisničko ime:</label>
+            <input type="text" name="korisnickoIme" />
+            <label htmlFor="sifra">Šifra:</label>
+            <input type="password" name="sifra" />
+
+            <input type="submit" name="submit" value="Prijava"/>
+          </form>
+          
+        </div>
+        <div id="greska">
+
+        </div>
+        <div id="footer">
+          Elektrotehnički fakultet u Sarajevu
+        </div>
       </div>
     );
   }

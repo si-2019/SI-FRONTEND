b53fd8d1c526d178a32130d010dd2f15cd434e0a
dsabovic1 <dsabovic1@etf.unsa.ba>
2019-05-05 15:47:39 +0200

    Tabela se popunjava podacima iz apija

--- reactapp/src/FOX/components/TabelaStudenti/TabelaStudenti.js
+++ reactapp/src/FOX/components/TabelaStudenti/TabelaStudenti.js
@@ -1,14 +1,37 @@
 import React, { Component } from 'react';
 import Table from 'react-bootstrap/Table';
 import Container from 'react-bootstrap/Container';
-import Col from 'react-bootstrap/Col';
-import Row from 'react-bootstrap/Row';
-
 import './table.css'
+import axios from 'axios';
+
+
 
 class TabelaStudenti extends Component {
+    state = {
+        studenti: [],
+        ispiti: []
+    }
+
+    constructor(props) {
+        super(props);
+    }
+
+    componentDidMount() {
+        //Promise
+        axios.get('http://localhost:31906/api/fox/tabelaStudenti')
+            .then(
+                res => this.setState({studenti: res.data})
+            );
+
+        axios.get('http://localhost:31906/api/fox/tabelaStudenti/ispiti')
+            .then(
+                res => this.setState({ispiti: res.data})
+            );
+    }
+    
     render() {
-      return (
+        console.log(this.state.studenti);
+        return (
         <Container>
             <Table striped bordered hover size="sm" responsive>
                 <thead>
@@ -18,14 +41,35 @@ class TabelaStudenti extends Component {
                     <th>Ime i Prezime</th>
                     <th>Prisustvo</th>
                     <th>Zadaće</th>
-                    <th>I parcijalni ispit</th>
-                    <th>II parcijalni ispit</th>
-                    <th>Usmeni ispit</th>
+                    {
+                        this.state.ispiti.map((ispit, i)=> {
+                            return <th>{ispit.naziv}</th>
+                        })
+                    }
                     <th>Ukupno</th>
                     <th>Ocjena</th>
                     </tr>
                 </thead>
                 <tbody>
+                    {
+                        this.state.studenti.map((student, i) => {
+                            return <tr>
+                                <td>{i+1}</td>
+                                <td>{student.index}</td>
+                                <td>{student.imePrezime}</td>
+                                <td>{student.prisustvo}</td>
+                                <td>{student.zadace}</td>
+                                {/*ispiti*/
+                                    student.ispiti.map((ispit, i) => {
+                                        return <td>{ispit.bodovi}</td>
+                                    })
+                                }
+                                <td>{student.ukupno}</td>
+                                <td>{student.ocjena}</td>
+                            </tr>
+                            
+                        })
+                    }
                     <tr>
                     <td>1</td>
                     <td>12345</td>
@@ -41,7 +85,7 @@ class TabelaStudenti extends Component {
                 </tbody>
             </Table>
         </Container>   
-      );
+        );
     }
   }
   

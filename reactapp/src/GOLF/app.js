01ccb6f3d3f6b88498e46c048af2105df4e32920
lejlab <lejlabegic46@gmail.com>
2019-05-05 18:10:24 +0200

    Student/profesor ima mogućnost odabira semestra koje nudi odsjek

--- reactapp/src/GOLF/app.js
+++ reactapp/src/GOLF/app.js
@@ -18,7 +18,7 @@ class App extends Component {
                                 <a href='/Golf/mojiPredmeti'>Moji predmeti</a>
                             </li>
                         </ul>
-                     <Route path="/Golf" component={Ciklusi} />
+                     <Ciklusi />
                      
                     </div>
                     <div class='col-9'>

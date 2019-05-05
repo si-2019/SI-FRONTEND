12e309a2a2e782ac5aac8691f40037ec91b850ba
nimsirovic1 <nimsirovic1@etf.unsa.ba>
2019-05-05 20:26:59 +0200

    Napisan konstruktor za tabelu

--- reactapp/src/CHARLIE/components/KreiraniIspiti/KreiraniIspiti.js
+++ reactapp/src/CHARLIE/components/KreiraniIspiti/KreiraniIspiti.js
@@ -1,47 +1,33 @@
 import React from 'react'
 import axios from 'axios'
+import { Link } from "react-router-dom";
+import ReactDOM from 'react-dom';
+import ReactTable from "react-table";
+import "react-table/react-table.css";
+
 
 class KreiraniIspiti extends React.Component{
-  state = {response:[]}
+  
+  constructor () {
+    super();
 
-  async componentDidMount(){ //Data sta renda
-    const {data} = await axios.get('http://localhost:3001/api/ispit')
-    this.setState({response:data})
-  }
+    this.state = {
+        tableData: [{
+            idIspit: '',
+            tipIspita: '',
+            termin: ''
+        }],
+    };
+}
 
-  renderItems = () => {
-    if(!this.state.response) return 
-    return this.state.response.map(element => 
-      <div className='container'>
-      <form>
-        <div className='row'>
-        <div className='col-4'>
-          <li style={{ listStyleType: "none" }}>{element.naziv}</li>
-        </div>
-        <div className='col-4'>
-          <li style={{ listStyleType: "none" }}>{element.tip_ispita}</li>
-        </div>
-        <div className='col-4'>
-          
-        </div>
-         
-        </div>
-        </form>
-        </div>
-
-    );
-  }
-
-  render(){
-    
+ 
+  render() {
     return(
       <div>
         Kreirani ispiti
-        <ul className="lista" id="listaIspita" >
-            {this.renderItems()}
-        </ul>
+        
       </div>
-    )
+    );
   }
 }
 

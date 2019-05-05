779cbe4b0630b92a611ae1aa509e21a2b017195b
nejiram <37294883+nejiram@users.noreply.github.com>
2019-05-05 18:47:25 +0200

    Prikaz svih javnih anketa

--- reactapp/src/HOTEL/liste/app.js
+++ reactapp/src/HOTEL/liste/app.js
@@ -1,24 +1,25 @@
 import React, { Component } from 'react';
 import './stil.css';
+import MojeAnkete from './mojeAnkete'
+import PopunjeneAnkete from './popunjeneAnkete'
+import JavneAnkete from './javneAnkete'
+import {BrowserRouter as Router, Route, Link} from 'react-router-dom'
 
 class App extends Component {
   render() {
     return (
       <div className="App" style={{padding: '25px'}}>
-        <h1 id="naslov">Moje ankete</h1>
-        <hr/>
-        <div className="anketa">
-            <label className="element"><h6>Hamo</h6></label>
-            <button className="dugmeUredi">Uredi</button>
-             <button className="dugmeObrisi">Obrisi</button>
+      <h1 id="naslov">Moje ankete</h1>
+      <Router>
+        <div>
+          <Route exact path ="/hotel/liste" component={MojeAnkete} />
+          <Route path="/hotel/liste/mojeAnkete" component={MojeAnkete}/>
+          <Route path="/hotel/liste/popunjeneAnkete" component={PopunjeneAnkete}/>
+          <Route path="/hotel/liste/javneAnkete" component={JavneAnkete}/>
           </div>
-             <br></br>
-         <div className="anketa">
-            <label className="element"><h6>ahmo</h6></label>
-            <button className="dugmeUredi">Uredi</button>
-             <button className="dugmeObrisi">Obrisi</button>
-          </div>
-        <hr/>
+        </Router>
+
+        
       </div>
     );
   }

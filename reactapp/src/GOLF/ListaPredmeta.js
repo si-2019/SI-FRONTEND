9dcfd34450dce527be990b89fb8e87f23c092be8
iosmankovic1 <iosmankovi1@etf.unsa.ba>
2019-05-05 14:22:48 +0200

    kreiranje komponenti za dodavanje predmeta

---
+++ reactapp/src/GOLF/ListaPredmeta.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class ListaPredmeta extends Component {
+  render() {
+    return (
+      <div>
+        
+      </div>
+    )
+  }
+}
+
+export default ListaPredmeta
---
+++ reactapp/src/GOLF/SviPredmeti.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class SviPredmeti extends Component {
+  render() {
+    return (
+      <div>
+        
+      </div>
+    )
+  }
+}
+
+export default SviPredmeti

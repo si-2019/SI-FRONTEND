36ad24ee08c6897e4e69dd83fc9eda7eeaa19fdb
Alma Hodzic <almahodzic071@outlook.com>
2019-05-06 10:31:44 +0200

    Dodatak pull rq-u
    Hardkodirani podaci za naslov objave materijala

--- reactapp/src/GOLF/objavaStudent.js
+++ reactapp/src/GOLF/objavaStudent.js
@@ -2,10 +2,15 @@ import React, { Component } from 'react';
 
 class objavaStudent extends Component {
   render() {
+  	const objave = [
+  	{
+  		naslov: 'Predavanja',
+  	}
+  	]
     return (
         <div class="card">
             <div class="card-body">
-            <h4 class="card-title">Tutorijal</h4>
+            <h4 class="card-title">{this.props.naslov}</h4>
             </div>
         </div>
     );

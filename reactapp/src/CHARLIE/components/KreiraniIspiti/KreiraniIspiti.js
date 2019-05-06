4ae4eb2cc8fa1f1a7e16c56d9ae4b390138b4047
nimsirovic1 <nimsirovic1@etf.unsa.ba>
2019-05-07 01:56:41 +0200

    Kreiran konstruktor

--- reactapp/src/CHARLIE/components/KreiraniIspiti/KreiraniIspiti.js
+++ reactapp/src/CHARLIE/components/KreiraniIspiti/KreiraniIspiti.js
@@ -1,6 +1,22 @@
 import React from 'react'
+import axios from 'axios'
+import { Link } from "react-router-dom";
+import ReactDOM from 'react-dom';
+import ReactTable from "react-table";
+import "react-table/react-table.css";
 
 class KreiraniIspiti extends React.Component{
+  constructor () {
+    super();
+
+    this.state = {
+        tableData: [{
+            idIspit: '',
+            tipIspita: '',
+            termin: ''
+        }],
+    };
+}
   render(){
     return(
       <div>

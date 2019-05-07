388512548a25b13d756ee421af740ea09f7d4375
samrabejtic <sbejtic1@etf.unsa.ba>
2019-05-08 01:31:56 +0200

    upis studenta

--- src/FormaUpis.js
+++ src/FormaUpis.js
@@ -1,14 +1,20 @@
 import React, {Component} from 'react'
 
+const naziviOdsjeka=["RI", "AiE", "EE", "TK"];
+const opcije = naziviOdsjeka.map((naziv)=>{ 
+  return <option>{naziv}</option>; 
+});
+
 class FormaUpis extends Component {
     constructor(props) {
         super(props)
   
         this.initialState = {
+          name: '',
           ciklus: '',
           sem: '',
           tip: '',
-          datum: ''
+          odsjek: ''
         }
     
         this.state = this.initialState
@@ -28,14 +34,19 @@ class FormaUpis extends Component {
         console.log("Svi potrebni podaci: ", data)        
     }
      
+    
+
 
     render() {
-        const { ciklus, sem, tip, datum } = this.state;
+        const { name, ciklus, sem, tip, odsjek } = this.state;
 
         return (
           <div className="col-md-2">
           
             <form  onSubmit={this.OnSubmit} className="container-fluid">
+              <label>Ime i prezime studenta </label>
+              <input className="form-control" type="text" name="name" value={name} onChange={this.handleInputChange} /><br />
+
               <label className="col-md-2">Ciklus </label>
               <input className="form-control" type="number" name="ciklus" value={ciklus} onChange={this.handleInputChange} /><br />
               
@@ -45,8 +56,9 @@ class FormaUpis extends Component {
               <label >Tip studenta </label>
               <input className="form-control" type="number" name="tip" value={tip} onChange={this.handleInputChange} /><br />
 
-              <label className="col-md-2">Odsjek </label>
-              <input className="form-control" type="number" name="datum" value={datum} onChange={this.handleInputChange} /><br />
+              <label className="col-md-2">Odsjek </label><br />
+              <select className="custom-select" name="odsjek" value={odsjek} onChange={this.handleInputChange}>{opcije}</select><br />
+              
               
               <input type="submit" value="Upiši" className="btn btn-success btn-block" />
             </form>
@@ -56,3 +68,6 @@ class FormaUpis extends Component {
 }
 
 export default FormaUpis
+
+
+/*<input className="form-control" type="number" name="odsjek" value={odsjek} onChange={this.handleInputChange} /><br />*/

50379848cd49e545fca0c9220a9d3b3af61753fa
Edo Skaljo <eskaljo1@etf.unsa.ba>
2019-05-11 14:51:15 +0200

    Dodan unit test

--- reactapp/src/CHARLIE/components/KreiraniIspiti/KreiraniIspiti.test.js
+++ reactapp/src/CHARLIE/components/KreiraniIspiti/KreiraniIspiti.test.js
@@ -4,10 +4,21 @@ import KreiraniIspiti from './KreiraniIspiti'
 
 describe('<KreiraniIspiti />', () => {
  
+  const mockData = { ispiti: { idIspit: 5, tipIspita: 'Usmeni', termin: '09:00'} };
+    beforeEach(() => {
+      const mock = new MockAdapter(axios);
+       mock
+        .onGet("http://localhost:31903/ispiti")
+        .reply(200, mockData);
+    });
+
   it('da li ima tabele za prikazivanje ispita', () => {
     const wrapper = shallow(<KreiraniIspiti/>)
    
     expect(wrapper.find('#tabelica').exists()).toBe(true)
   })
-
+  it('da li ima dugme za uredjivanje ispita', () => {
+    const wrapper = shallow(<KreiraniIspiti/>)
+    expect(wrapper.find('#btnUredi').exists()).toBe(true)
+  })
 }) 

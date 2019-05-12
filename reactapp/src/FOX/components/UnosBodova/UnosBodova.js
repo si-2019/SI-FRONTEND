5ca3f84414fcf9b702ecb15a0e5fd3e3a3149381
Sejla Pljakic <sejla.pljakic@gmail.com>
2019-05-12 17:36:14 +0200

    Dodan responzivan i user-friendly interface

---
+++ reactapp/src/FOX/components/UnosBodova/UnosBodova.js
@@ -0,0 +1,27 @@
+import React, { Component} from 'react';
+
+class UnosBodova extends  Component {
+render() {
+
+    return(
+       <div classname="Unos">
+            <div class="col-sm-6">
+            </div>
+            
+            <div class="col-sm-6">
+            </div>
+
+            <div class="col-sm-6">
+            </div>
+        </div>
+    );
+     }
+}
+
+export default UnosBodova;
+
+
+
+
+
+

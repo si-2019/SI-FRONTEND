53697a9c3d53adc618119240280ce78bca40fcba
mkaramujic1 <maidak_95@hotmail.com>
2019-05-12 15:22:30 +0200

    Dodana forma za pisanje i slanje issue-a

--- reactapp/src/BETA/app.js
+++ reactapp/src/BETA/app.js
@@ -2,15 +2,19 @@ import React, { Component } from 'react';
 import Modal from 'react-responsive-modal'; //paket za gotove modale odnosno popup-e
 import './App.css';
 import NoviIssueForma from './komponente/NoviIssueForma.js';
+import LeftMenuStudent from './komponente/LeftMenuStudent.js';
+import IssueList from './komponente/issueList.js';
 
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      open: false  //open pokazuje da li formu treba prikazati ili ne
+      open: false,  //open pokazuje da li formu treba prikazati ili ne
+      activeContentId: 1
     };
     this.onOpenModal = this.onOpenModal.bind(this)
     this.onCloseModal = this.onCloseModal.bind(this)
+    console.log(this.props)
   }
 
   //Funkcija kojom se otvara forma
@@ -35,21 +39,65 @@ class App extends Component {
 
   onCloseModal2 = (open) => {
     this.setState({
-      open: false
+      open: false,
+      id: 1
     })
   }
+
+  onChangeActiveId = (id) => {
+    this.setState({
+      activeContentId: id,
+    })
+  }
+
+
   render() {
     const { open } = this.state;
     return (
       <div>
-        <h1>BETA</h1>
+        <h2>Student Issues Page :)</h2>
         <div className="App">
 
           <div className="row">
 
-            <button type="button" className="btn btn-primary" id="createNewIssue" onClick={this.onOpenModal} >
-              Kreiraj novi issue
-            </button>
+            <div id = "head">
+
+              <button type="button" className="btn btn-primary" id="createNewIssue" onClick={this.onOpenModal} >
+                Create New Issue
+              </button>
+
+              <div id = "search-issue-tab">Ovdje ce biti search filter
+              </div>
+
+            </div>
+
+            <div id = "main">
+              <div id = "left">
+                <LeftMenuStudent triggerChangeActiveId = {this.onChangeActiveId}/>
+              </div>
+              <div id = "right">
+                <div 
+                    id = "TrackIssuesContent" 
+                    style={{display : this.state.activeContentId == 1 ? 'inherit' : 'none'}}
+                ><IssueList/>
+                </div>
+                <div 
+                    id = "DraftsContent" 
+                    style={{display : this.state.activeContentId == 2 ? 'inherit' : 'none'}}
+                >Drafts komponenta ide ovdje
+                </div>
+                <div 
+                    id = "Archived" 
+                    style={{display : this.state.activeContentId == 3 ? 'inherit' : 'none'}}
+                > Archived komponenta ide ovdje 
+                </div>
+                <div 
+                    id = "FAQContent" 
+                    style={{display : this.state.activeContentId == 4 ? 'inherit' : 'none'}}
+                > FAQ komponenta ide ovdje
+                </div>
+              </div>
+            </div>
 
           </div>    
           

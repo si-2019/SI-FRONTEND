0b811a4671c53242277a4adf815625c47c9ae870
Nerma Hanic <nhanic2@etf.unsa.ba>
2019-05-12 12:54:07 +0200

    Napravljena komponenta za prikaz projektnih grupa

---
+++ reactapp/src/MIKE/components/PregledProjektnihGrupa/PrikazDetaljaProjektneGrupe.js
@@ -0,0 +1,26 @@
+import React from "react";
+
+class DetaljiProjektneGrupe extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {value: ''};
+      }
+    
+     
+    
+      
+    render() {
+       
+        return (
+          <form  >
+            <label class = "text">
+              idProjektneGrupe:
+            </label> 
+            
+           
+          </form>
+        );
+      }
+
+}
+export default DetaljiZadatka;

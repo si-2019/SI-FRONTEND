3306d7cbbe101b19e2d9f704f9c72159b67c8f6a
Alma Hodzic <almahodzic071@outlook.com>
2019-05-12 12:11:35 +0200

    Update literaturaStudent.js
    Dodan prikaz naslova literature - pregled: student

--- reactapp/src/GOLF/literaturaStudent.js
+++ reactapp/src/GOLF/literaturaStudent.js
@@ -2,9 +2,15 @@ import React, { Component } from 'react';
 
 class LiteraturaStudent extends Component {
  render(){
+
+ 	const literatura = [
+ 	{
+ 		naslovLiterature: 'Osnove računarskih arhitektura, Novica Nosović, Sarajevo, 2003, Elektrotehnički fakultet Sarajevo'
+ 	}]
+
     return (
         <div class="divsaokvirom">
-            
+        <h4 class="card-title">{this.props.naslov}</h4>    
         </div>
     );
   }

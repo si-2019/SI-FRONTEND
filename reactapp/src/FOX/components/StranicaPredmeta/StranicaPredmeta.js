207a101c4d91ff33145dbd7557848c26990380c5
Sejla Pljakic <sejla.pljakic@gmail.com>
2019-05-13 00:47:52 +0200

    Prepravke

--- reactapp/src/FOX/components/StranicaPredmeta/StranicaPredmeta.js
+++ reactapp/src/FOX/components/StranicaPredmeta/StranicaPredmeta.js
@@ -37,6 +37,7 @@ class StranicaPredmeta extends Component {
         
       </div>
     );
+
   }
 }
 
--- reactapp/src/FOX/components/TabelaUnosa/TabelaUnosa.js
+++ reactapp/src/FOX/components/TabelaUnosa/TabelaUnosa.js
@@ -1,22 +1,24 @@
 import React, {Component } from 'react';
 import Footer from './components/Footer/Footer';
-
+import Form from 'react-bootstrap/Form'
+import Button from 'react-bootstrap/Button';
 class TabelaUnosa extends Component {
 
 
     render() {
         return(
            <Container>
-                <Row>
-
-                </Row>
-                <Row>
-                    <Col md={{span: 6, offset: 4}} > 
+            <Form.Group>
+                <Form.Row>
+                </Form.Row>
+                <Form.Row>
+                    <Form.Col md={{span: 2, offset: 4}} > 
                         <label> Index: </label>
                         <input type="text" name="name" />
-                        <Button variant="primary">Pretrazi</Button>
-                    </Col>
-                </Row>
+                        <Form.Button variant="primary">Pretrazi</Form.Button>
+                    </Form.Col>
+                </Form.Row>
+                </Form.Group>
            </Container>
 
         );

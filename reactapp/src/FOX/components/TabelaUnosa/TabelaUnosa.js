98a05fe822ec117168c6e89aad2e49d264d4ce13
Sejla Pljakic <sejla.pljakic@gmail.com>
2019-05-13 00:17:54 +0200

    Novi US

---
+++ reactapp/src/FOX/components/TabelaUnosa/TabelaUnosa.js
@@ -0,0 +1,26 @@
+import React, {Component } from 'react';
+import Footer from './components/Footer/Footer';
+
+class TabelaUnosa extends Component {
+
+
+    render() {
+        return(
+           <Container>
+                <Row>
+
+                </Row>
+                <Row>
+                    <Col><Col md={{span: 6, offset: 4}} > { }  </Col></Col>
+                </Row>
+           </Container>
+
+        );
+
+    }
+
+
+}
+
+
+export default TabelaUnosa;

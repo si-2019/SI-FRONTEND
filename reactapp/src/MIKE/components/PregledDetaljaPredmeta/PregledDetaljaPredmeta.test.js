df2d98372c0666ab5221f07da7f82f72dabb38aa
lamijaalagic <lalagic1@etf.unsa.ba>
2019-05-12 16:02:30 +0200

    Kreiran test pa provjeru postojanja liste predmeta

---
+++ reactapp/src/MIKE/components/PregledDetaljaPredmeta/PregledDetaljaPredmeta.test.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Enzyme, { configure, mount, shallow, render } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+Enzyme.configure({ adapter: new Adapter() });
+
+import PregledDetaljaPredmeta from './PregledDetaljaPredmeta';
+
+describe('Pregled detalja nekog predmeta na kojim je asistent', () => {
+    it("Postoji lista predmeta na osnovu koje biramo odredjeni predmet ", () => {
+        const wrapper = shallow(<PregledDetaljaPredmeta/>);
+        expect(wrapper.find("#listaPredmeta").exists()).toBe(true);
+    });
+
+});

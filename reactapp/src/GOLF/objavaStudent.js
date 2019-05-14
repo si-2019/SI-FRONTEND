a4484aabe8599c6428bc87ddaa6566c8045afbaf
rzaketovic1 <rzaketovic1@etf.unsa.ba>
2019-05-14 18:16:01 +0200

    Prikaz poruke/opisa materijala

--- reactapp/src/GOLF/objavaStudent.js
+++ reactapp/src/GOLF/objavaStudent.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import DodavanjeDatuma from './DodavanjeDatuma';
+import OpisMaterijala from './OpisMaterijala'
 
 
 class objavaStudent extends Component {
@@ -16,6 +17,7 @@ class objavaStudent extends Component {
         <div class="card">
             <div class="card-body">
             <h4 class="card-title">{this.props.naslov}</h4>
+            <OpisMaterijala opisMaterijala={this.props.opisMaterijala}></OpisMaterijala>
             {this.props.fileovi.map(predmet => [<a href='#' class='card-link' key='1'>{predmet}</a>,<br key='2'></br>])}
             <DodavanjeDatuma datumobjave={this.state.datumobjave}></DodavanjeDatuma>
 

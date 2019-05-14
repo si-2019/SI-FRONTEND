271b20d21710fe4a2508ea246b735d7d095dad8a
Amila Borancic <aborancic1@etf.unsa.ba>
2019-05-14 16:24:04 +0200

    test oko teme

--- reactapp/src/SIERA/DatumRodjenja.jsx
+++ reactapp/src/SIERA/DatumRodjenja.jsx
@@ -27,11 +27,86 @@ class DatumRodjenja extends React.Component {
     }
     render() {
         return (
-            <div className="col-md-4">
-                <div className="alert alert-dismissible alert-danger">
-                    <button type="button" className="close" data-dismiss="alert">&times;</button>
-                    <strong>Oh snap!</strong> <a href="#" className="alert-link">Change a few things up</a> and try submitting again.
-                </div>
+            <div>
+                <button type="button" class="btn btn-primary">Primary</button>
+                <button type="button" class="btn btn-secondary">Secondary</button>
+                <button type="button" class="btn btn-success">Success</button>
+                <button type="button" class="btn btn-info">Info</button>
+                <button type="button" class="btn btn-warning">Warning</button>
+                <button type="button" class="btn btn-danger">Danger</button>
+                <button type="button" class="btn btn-link">Link</button>
+                <table class="table table-hover">
+                    <thead>
+                        <tr>
+                            <th scope="col">Type</th>
+                            <th scope="col">Column heading</th>
+                            <th scope="col">Column heading</th>
+                            <th scope="col">Column heading</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr class="table-active">
+                            <th scope="row">Active</th>
+                            <td>Column content</td>
+                            <td>Column content</td>
+                            <td>Column content</td>
+                        </tr>
+                        <tr>
+                            <th scope="row">Default</th>
+                            <td>Column content</td>
+                            <td>Column content</td>
+                            <td>Column content</td>
+                        </tr>
+                        <tr class="table-primary">
+                            <th scope="row">Primary</th>
+                            <td>Column content</td>
+                            <td>Column content</td>
+                            <td>Column content</td>
+                        </tr>
+                        <tr class="table-secondary">
+                            <th scope="row">Secondary</th>
+                            <td>Column content</td>
+                            <td>Column content</td>
+                            <td>Column content</td>
+                        </tr>
+                        <tr class="table-success">
+                            <th scope="row">Success</th>
+                            <td>Column content</td>
+                            <td>Column content</td>
+                            <td>Column content</td>
+                        </tr>
+                        <tr class="table-danger">
+                            <th scope="row">Danger</th>
+                            <td>Column content</td>
+                            <td>Column content</td>
+                            <td>Column content</td>
+                        </tr>
+                        <tr class="table-warning">
+                            <th scope="row">Warning</th>
+                            <td>Column content</td>
+                            <td>Column content</td>
+                            <td>Column content</td>
+                        </tr>
+                        <tr class="table-info">
+                            <th scope="row">Info</th>
+                            <td>Column content</td>
+                            <td>Column content</td>
+                            <td>Column content</td>
+                        </tr>
+                        <tr class="table-light">
+                            <th scope="row">Light</th>
+                            <td>Column content</td>
+                            <td>Column content</td>
+                            <td>Column content</td>
+                        </tr>
+                        <tr class="table-dark">
+                            <th scope="row">Dark</th>
+                            <td>Column content</td>
+                            <td>Column content</td>
+                            <td>Column content</td>
+                        </tr>
+                    </tbody>
+                </table>
                 <div className="col-2" >
                     <button type="button" className="btn btn-info" id="datumRodjenja" onClick={this.handleClick}>Datum rodjenja</button>
                     <div style={{ height: "20px" }}></div>

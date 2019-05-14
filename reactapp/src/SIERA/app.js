a29803d0fb376eec89c777d5d9e286ff666b8f84
Amila Borancic <aborancic1@etf.unsa.ba>
2019-05-14 13:12:24 +0200

    Dodano rutiranje za liste predmeta i prikaz studentovih podataka.

--- reactapp/src/SIERA/app.js
+++ reactapp/src/SIERA/app.js
@@ -1,12 +1,11 @@
 import React, { Component } from "react";
-
+import { BrowserRouter, Route } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.css";
 import KontaktPod from "./components/kontaktPod";
 import Stranice from "./components/stranice";
 import Fotografija from "./components/fotografija";
 import LicniPod from "./components/licniPod";
 import axios from "axios";
-
 import Potvrda from "./Potvrda";
 import PopUp from "./PopUp";
 import "./App.css";
@@ -19,25 +18,36 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <div class="container-fluid">
-          <div class="row">
-            <div class="col-sm">
-              <ListaTrenutnihPredmeta />
-              <ListaOdslusanihPredmeta />
+        <BrowserRouter>
+          <Route exact path='/Siera/lista-predmeta' render={() =>
+            <div class="container-fluid">
+              <div class="row">
+                <div class="col-sm">
+                  <ListaTrenutnihPredmeta />
+                  <ListaOdslusanihPredmeta />
+                </div>
+              </div>
             </div>
-            <div class="col-sm">
-              <Fotografija />
-              <KontaktPod />
-              <Stranice />
+          } />
+          <Route exact path="/Siera/podaci-o-studentu" render={() =>
+            <div class="container-fluid">
+              <div class="row">
+                <div class="col-sm">
+                  <Fotografija />
+                  <KontaktPod />
+                  <Stranice />
+                </div>
+                <div class="col-sm">
+                  <LicniPod />
+                </div>
+              </div>
             </div>
-            <div class="col-sm">
-              <LicniPod />
-            </div>
-          </div>
-        </div>
+          }/>
+        </BrowserRouter>
+          
       </div>
-    );
-  }
-}
-
-export default App;
+        );
+      }
+    }
+    
+    export default App;

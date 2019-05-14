1baec3179c7086a920a6c05131d2a5ea793f72f0
Džemil Džigal <ddzigal1@etf.unsa.ba>
2019-05-14 17:25:01 +0200

    Dodavanje komponente za LIMA_Sprint3_27

--- reactapp/src/LIMA/I_US_27/I_US_27.js
+++ reactapp/src/LIMA/I_US_27/I_US_27.js
@@ -5,22 +5,27 @@ import Alert from 'react-bootstrap/Alert';
 import Form from 'react-bootstrap/Form';
 
 export default class I_US_27 extends Component{
-  state = {};
+    state = {
+    error: null,
+    isLoaded: false,
+    show:true,
+    rezultati:[]
+  };
   constructor(props) {
     super(props);
-    this.state = {
-      error: null,
-      isLoaded: false,
-      show:true
-    };
+    
   }
 posalji = (e)=>{
   e.preventDefault();
   console.log("UŠAO");
   let podaci = [];
-  fetch('http://localhost:31912/Izvjestaji/dajDrugeParcijale').then(rez=>rez.json()).then(data=>this.setState({data}));
+  fetch('http://localhost:31912/Izvjestaji/dajDrugeParcijale').then(rez=>rez.json()).then(data=>{this.setState({rezultati:[...this.state.rezultati,data]})
+  podaci = data;
+  console.log(this.state.rezultati);
+});
 }
   render(){
+  
     return(
       <>
         <Form onSubmit={this.posalji}>
@@ -32,7 +37,9 @@ posalji = (e)=>{
             <Button variant = "primary" type ="submit"> Daj ispite </Button>
             </Form.Row>
         </Form>
-        Deja-vu. I've been to space before.
+        <div>{this.state.rezultati.map(rez=>{return <h1>{rez.message[0]} {rez.message[1]} {rez.message[2]}</h1>})}</div>
+        
+        Deja-vu. I've been to space before hehe.
       </>
     );
   }

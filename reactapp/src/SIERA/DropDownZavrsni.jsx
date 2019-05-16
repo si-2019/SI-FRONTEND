98b84ff436d70fb4cf7951deee744f09866bc143
Amila Borancic <aborancic1@etf.unsa.ba>
2019-05-16 17:18:24 +0200

    male ispravke interfejsa

--- reactapp/src/SIERA/DropDownZavrsni.jsx
+++ reactapp/src/SIERA/DropDownZavrsni.jsx
@@ -24,16 +24,19 @@ class DropDownZavrsni extends React.Component {
     render() {
         return (
 
-            <div class="row" style={{ margin:"0px"}}>
+            <div class="row" style={{ margin: "0px" }}>
                 <div class="col"></div>
-                <div class="col" style={{textAlign:"center"}}>
-                    <div class="card" style={{display:"inline-block"}}>
+                <div class="col" style={{ textAlign: "center" }}>
+                    <div class="card" style={{ display: "inline-block" }}>
                         <div class="card-body">
                             <h3 class="card-title">Završni rad</h3>
                             <h6 class="card-subtitle mb-2 text-muted">Ovdje možete vidjeti sve profesore koje možete odabrati za svog mentore, kao i teme koje nude.</h6>
+                            <div style={{ textAlign: "left" }}>
+                                <label class="col-form-label col-form-label-lg" for="inputLarge">Mentori</label>
+                            </div>
 
-                            <select class="custom-select">
-                                <option selected="">Profesori</option>
+                            <select class="custom-select" required>
+                                <option selected="">Neko</option>
                                 <option value="1">One</option>
                                 <option value="2">Two</option>
                                 <option value="3">Three</option>

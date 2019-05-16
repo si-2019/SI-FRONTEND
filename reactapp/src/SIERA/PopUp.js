7788f244e3a1b3d8fc99070b1d22e98628f4967c
nudiey <dpozderac1@etf.unsa.ba>
2019-05-16 23:50:51 +0200

    Mala izmjena SIERA US 20

--- reactapp/src/SIERA/PopUp.js
+++ reactapp/src/SIERA/PopUp.js
@@ -1,17 +1,13 @@
 import React from "react";
 
 class PopUp extends React.Component {
-
-
-    render() {
-        return (
-            <div className={this.props.class} style={this.props.style}>
-                <strong>{this.props.boldiraniTekst}</strong> <br></br>{this.props.ostaliTekst}
-            </div>
-        );
-
-    }
-    
+  render() {
+    return (
+      <div className={this.props.class} style={this.props.style}>
+        <strong>{this.props.boldiraniTekst}</strong> <br />
+        {this.props.ostaliTekst}
+      </div>
+    );
   }
 }
 

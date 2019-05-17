1cdfd5e6eed84dcef8fd9c72e8a689422193acdf
Your Name <you@example.com>
2019-05-17 17:19:23 +0200

    Student/profesor ima mogućnost pregleda naziva odabranog predmeta

--- reactapp/src/GOLF/stranicaPredmetaStudent.js
+++ reactapp/src/GOLF/stranicaPredmetaStudent.js
@@ -1,12 +1,23 @@
 import React, { Component } from 'react'
-
+import axios from 'axios'
 class stranicaPredmetaStudent extends Component {
+    constructor(props){
+        super(props);
+        this.state = {          
+          idPredmeta: props.match.params.idPredmeta,
+          naziv: ""
+        };
+    }
+    
+    componentDidMount(){
+        axios.get(`http://localhost:31907/r5/dajNaziv/${this.state.idPredmeta}`).then(res =>{
+            this.setState({naziv:res.data.naziv})
+        })
+    }
     render(){
         return(
             <div className="card border-info mb-3">
-                <div className="card-header">
-                    <h4>Student stranica</h4>
-                </div>
+                <h1 className="card-header">  {this.state.naziv}</h1>
             </div>
         )
     }

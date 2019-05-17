26a7350105a3422c873c6c4d23de0dbd50b55ecf
Sejla Pljakic <sejla.pljakic@gmail.com>
2019-05-17 15:27:33 +0200

    Button opcija

--- reactapp/src/FOX/components/TabelaUnosa/TabelaUnosa.js
+++ reactapp/src/FOX/components/TabelaUnosa/TabelaUnosa.js
@@ -6,30 +6,21 @@ import Row from 'react-bootstrap/Button';
 import Header from '../Header/Header';
 import Footer from '../Footer/Footer';
 import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button'
 class TabelaUnosa extends Component {
-
-
     render() {
         return(
            <Form>
                 <Form.Row>
                 </Form.Row>
                 <Form.Row>
-                    <Col md={{span: 2, offset: 4}} > 
-                        <Form.Label> Index: 
-                          <input type="text" name="name" />
-                          </Form.Label>
-                    </Col>
+                    <Form.Label> Index: 
+                     <input type="text" name="name" />
+                    </Form.Label>
+                    <Button> Pretrazi </Button>   
                 </Form.Row>
-
            </Form>
-
         );
-
     }
-
-
 }
-
-
 export default TabelaUnosa;

2db29d1518fd0982303d5163c2ba57570f0e8b4f
Sejla Pljakic <sejla.pljakic@gmail.com>
2019-05-17 15:38:53 +0200

    Novi dio

--- reactapp/src/FOX/components/TabelaUnosa/TabelaUnosa.js
+++ reactapp/src/FOX/components/TabelaUnosa/TabelaUnosa.js
@@ -12,12 +12,18 @@ class TabelaUnosa extends Component {
         return(
            <Form>
                 <Form.Row>
+                    <Form.Label>  </Form.Label>
                 </Form.Row>
-                <Form.Row>
+                
+                <Form.Row md = {{span: 8, offset: 6}}>
                     <Form.Label> Index: 
                      <input type="text" name="name" />
                     </Form.Label>
                     <Button> Pretrazi </Button>   
+                </Form.Row>
+
+                <Form.Row md={{span: 4, offset}}>
+                
                 </Form.Row>
            </Form>
         );

f155e2d2d2effbe4561defa4d324de2bd95adddd
Sarita Halilovic <saraa.halilovic@hotmail.com>
2019-05-17 23:09:51 +0200

    ispravljen naslov

--- reactapp/src/BETA/komponente/FAQ.js
+++ reactapp/src/BETA/komponente/FAQ.js
@@ -7,8 +7,7 @@ class FAQ extends React.Component {
     constructor() {
         super();
         this.state = {
-            issues: [    
-            ],
+            issues: [ ],
             id: 4
         }
     }

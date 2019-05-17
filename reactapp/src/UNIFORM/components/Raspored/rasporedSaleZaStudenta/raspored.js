385895ec8c33d5e71981696b3da25489da72f10f
gobathegreat <agobeljic1@etf.unsa.ba>
2019-05-17 14:28:52 +0200

    Popravljena struktura

---
+++ reactapp/src/UNIFORM/components/Raspored/rasporedSaleZaStudenta/raspored.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export class raspored extends Component {
+  render() {
+    return (
+      <div>
+        
+      </div>
+    )
+  }
+}
+
+export default raspored

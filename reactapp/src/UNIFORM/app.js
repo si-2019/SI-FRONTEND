0e86c547c3bd5e7d18c1fcf9b2623328fadbf662
gobathegreat <agobeljic1@etf.unsa.ba>
2019-05-17 14:14:18 +0200

    Sad ima app.js

--- reactapp/src/UNIFORM/app.js
+++ reactapp/src/UNIFORM/app.js
@@ -6,17 +6,19 @@ import RasporedSalaStudent from './components/Raspored/rasporedSaleZaStudenta/ra
 import RasporedSalaProfesor from './components/Raspored/rasporedSaleZaProfesora/raspored.js';
 import GrupeStudent from './components/Grupe/grupeStudent/grupeStudent.js';
 import GrupeProfesor from './components/Grupe/grupeProfesor/grupeProfesor.js';
+import UniformHome from './components/Home/home.js';
 export class App extends Component {
   render() {
     return (
       <BrowserRouter>
         <div>
-          <Route exact path="/rasporedStudent" component={RasporedStudent}/>
-          <Route exact path="/rasporedProfesor" component={RasporedProfesor}/>
-          <Route exact path="/rasporedSaleZaStudenta" component={RasporedSalaStudent}/>
-          <Route exact path="/rasporedSaleZaProfesora" component={RasporedSalaProfesor}/>
-          <Route exact path="/grupeStudent" component={GrupeStudent}/>
-          <Route exact path="/grupeProfesor" component={GrupeProfesor}/>         
+          <Route exact path="/UNIFORM" component={UniformHome}/>
+          <Route path="/UNIFORM/rasporedStudent" component={RasporedStudent}/>
+          <Route path="/UNIFORM/rasporedProfesor" component={RasporedProfesor}/>
+          <Route path="/UNIFORM/rasporedSaleZaStudenta" component={RasporedSalaStudent}/>
+          <Route path="/UNIFORM/rasporedSaleZaProfesora" component={RasporedSalaProfesor}/>
+          <Route path="/UNIFORM/grupeStudent" component={GrupeStudent}/>
+          <Route path="/UNIFORM/grupeProfesor" component={GrupeProfesor}/>         
         </div>
       </BrowserRouter>
     )

41b2e524810bb32b368643ef65cdd739d638ce0a
bomerovic <bomerovic1@etf.unsa.ba>
2019-05-18 17:52:11 +0200

    dodan indikator neuspješno spašenog ispita

--- reactapp/src/CHARLIE/components/KreirajIspitDetalji/KreirajIspitDetalji.js
+++ reactapp/src/CHARLIE/components/KreirajIspitDetalji/KreirajIspitDetalji.js
@@ -1,37 +1,49 @@
 import React, { Component } from "react";
 import Form from "../SharedComponents/Form";
 import { Link } from 'react-router-dom';
+import Axios from "axios";
 
 
 
 class KreirajIspitDetalji extends Component {
-  state = { napomenaGreska: false };
+  state = { 
+    napomena: '',
+    napomenaGreska: false,
+    ispit: {}
+   };
+
+  handleSubmit = event => {
+    event.preventDefault();
+    // poziv na backend
+
+    Axios.post('http://localhost:31903/addIspit', this.state.ispit)
+      .catch((err) => {
+        if(err.response.status === 409) {
+          alert(err.response.data);
+        }
+      })
+    this.setState({ napomena: ''});
+  }
+
+  handleChange = event => {
+    this.setState({ napomena: event.target.value });
+  }
 
   render() {
     return (
       <div className="container">
         <div className="row">
           <div className="col-4">
-            <form>
-              <Form
-                autoFocus
-                labelTitle="Napomena za ispit"
-                id="ispitnaNapomena"
-                placeholder="Nemojte zaboraviti indeks..."
-                validations={["required"]}
-              />
-            </form>
-          </div>
-        <div className="col-4">
-          <form>
-            <Link to="/charlie">
-              <button type="button" class="btn btn-primary" id="btnSpasi">Kreiraj ispit</button>
-            </Link>
-            <Link to="/charlie">
+            <form onSubmit={this.handleSubmit}>
+              <label>Napomena za ispit
+                <input type="text" value={this.state.napomena} onChange={this.handleChange}/>
+              </label>
+              <button type="submit" class="btn btn-primary">Kreiraj</button>
+              <Link to="/charlie">
               <button type="button" class="btn btn-danger" id="btnPovratak">Odustani</button>
             </Link>
-          </form>
-        </div>
+            </form>
+          </div>
         </div>
         
       </div>

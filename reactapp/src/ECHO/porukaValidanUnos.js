ee5d0e96b6bfadd8b871c7eab14f3413f5f05f5f
Ajla Kremo <akremo1@etf.unsa.ba>
2019-05-18 13:29:19 +0200

    Sprint 3

--- reactapp/src/ECHO/porukaValidanUnos.js
+++ reactapp/src/ECHO/porukaValidanUnos.js
@@ -3,3 +3,9 @@ export function uspjesanUnosPodataka(){
      return(alert('Uspješan unos podataka!') )
        
 }
+
+export function uspjesanUnosSale(){
+  
+     return(alert('Podaci validni. Sala uspješno unesena !') )
+       
+}

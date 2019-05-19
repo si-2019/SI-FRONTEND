11be8d66037d9b9ef711e9c457920fc479e42dce
Nerma Hanic <nhanic2@etf.unsa.ba>
2019-05-20 00:01:42 +0200

    Dodan pregled projektnih grupa sa testovima

---
+++ reactapp/src/MIKE/components/PregledProjektnihGrupa/PrikazDetaljaProjektneGrupe.js
@@ -0,0 +1,54 @@
+import React from "react";
+
+class DetaljiProjektneGrupe extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {value: ''};
+      }
+    
+     
+    
+      
+    render() {
+       
+        return (
+          <form  >
+            <label class = "text">
+              idProjektneGrupe:
+            </label> 
+            <br></br>
+            <label>
+                ID projekta:
+            </label>
+            <br></br>
+            <label>
+                Opis:
+            </label>
+            <br></br>
+            <label>
+                Prioritet:
+            </label>
+            <br></br>
+            <label>
+                Od kad:
+            </label>
+            <br></br>
+            <label>
+                Do kad:
+            </label>
+            <br></br>
+            <labe>
+                Zavrsen:
+            </labe>
+            <br></br>
+            <label>
+                Komentar asistenta:
+            </label>
+            
+           
+          </form>
+        );
+      }
+
+}
+export default DetaljiProjektneGrupe;

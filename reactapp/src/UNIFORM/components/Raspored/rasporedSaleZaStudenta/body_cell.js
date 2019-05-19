abd73f36e2c9adedaab5642a09a4c379d806e645
gobathegreat <agobeljic1@etf.unsa.ba>
2019-05-19 14:40:31 +0200

    Dodan css za modal

--- reactapp/src/UNIFORM/components/Raspored/rasporedSaleZaStudenta/body_cell.js
+++ reactapp/src/UNIFORM/components/Raspored/rasporedSaleZaStudenta/body_cell.js
@@ -4,7 +4,16 @@ import Popup from 'react-popup';
 import Modal from 'react-modal';
 import { stringify } from 'querystring';
 import axios from 'axios';
-
+const customStyles = {
+  content : {
+    top                   : '50%',
+    left                  : '50%',
+    right                 : 'auto',
+    bottom                : 'auto',
+    marginRight           : '-50%',
+    transform             : 'translate(-50%, -50%)'
+  }
+};
 
 
 

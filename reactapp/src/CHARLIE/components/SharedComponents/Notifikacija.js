476c62862a4df54a6672abbb4dfb2c507a5be0a5
amirmu-at-587105673553 <amuminovic3@etf.unsa.ba>
2019-05-19 11:45:40 +0200

    Dodana mogucnost da se ukine notifikacija

--- reactapp/src/CHARLIE/components/SharedComponents/Notifikacija.js
+++ reactapp/src/CHARLIE/components/SharedComponents/Notifikacija.js
@@ -3,6 +3,9 @@ import React from 'react'
 const Notifikacija = (props) => {
   return (<div style={{backgroundColor:props.background}}>
     {props.poruka}
+    <i className="fas fa-times" style={{float:'right'}}
+      onClick={props.onClick}
+    />
   </div>)
 }
 

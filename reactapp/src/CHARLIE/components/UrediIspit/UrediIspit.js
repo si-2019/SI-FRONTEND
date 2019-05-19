e800ca0246d94eb3c848f241d7d673058c564bb9
Nedim Memisevic <nmemisevic1@etf.unsa.ba>
2019-05-19 12:19:24 +0200

    Dodan datepicker za promjenu roka prijave ispita

--- reactapp/src/CHARLIE/components/UrediIspit/UrediIspit.js
+++ reactapp/src/CHARLIE/components/UrediIspit/UrediIspit.js
@@ -1,11 +1,28 @@
 import React from 'react'
+import DateTimePicker from 'react-datetime-picker'
 
 class UrediIspit extends React.Component{
+  state = {
+    date: new Date(),
+  }
+ 
+  onChange = date => this.setState({ date })
   render(){
     return(
-      <div>
-        Uredi ispit
-        <button type="button" class="btn btn-primary" id="btnPotvrdi">Potvrdi</button>
+      
+      <div className="col-12">
+        <h3>Uredi ispit</h3>
+        <form>
+          <label>Rok prijave ispita</label>
+          <br></br>
+          <DateTimePicker
+            onChange={this.onChange}
+            value={this.state.date}
+            format="dd-MM-yyyy HH:mm"
+          />
+          <br></br>
+          <input type="button" class="btn btn-primary" id="btnPotvrdi" value="Potvrdi"/>
+        </form>
       </div>
     )
   }

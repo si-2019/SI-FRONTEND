f29e401f4a6d515f2e201b611793cd522f4e3e10
Sejla Pljakic <sejla.pljakic@gmail.com>
2019-05-21 13:46:18 +0200

    Prepravke_1

--- reactapp/src/FOX/components/UnosOcjene/UnosOcjene.js
+++ reactapp/src/FOX/components/UnosOcjene/UnosOcjene.js
@@ -2,11 +2,14 @@ import React, { Component} from 'react';
 import Form from 'react-bootstrap/Form';
 import Ocjena from '../Ocjena/Ocjena';
 import Col from 'react-bootstrap/Col';
+import Header from '../Header/Header';
+
 class UnosOcjene extends Component {
      render()
      {
          return (
             <div>
+                <Header isPocetna={false}/>
             <Form>
                 <Form.Row>
                 </Form.Row>

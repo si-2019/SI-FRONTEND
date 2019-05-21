1a7b8c8c3200cf5452d8971efba7706efd2a96eb
Amila Borancic <aborancic1@etf.unsa.ba>
2019-05-21 17:03:16 +0200

    ispravljena greska u povezivanju s apijem + popravljen interfejs

--- reactapp/src/SIERA/ProfilStudenta.jsx
+++ reactapp/src/SIERA/ProfilStudenta.jsx
@@ -10,41 +10,73 @@ class Profil extends React.Component {
             studentId: 1,
             fotka: null,
             ime: "bacam hejt",
-            prezime: "ovom predmetu"
+            prezime: "ovom predmetu",
+            mail: "lol",
+            website: "lala",
+            linkedIn: "isnjksd",
+            mjRod: "sdkl",
+            datRo: "no"
         }
     }
     componentDidMount() {
-        console.log("match.id: " + this.props.match.params.idStudenta);
         const noviId = this.props.match.params.idStudenta;
         axios
             .get("http://localhost:31918/studenti/" + noviId)
             .then(res => {
+                console.log(res.data.error);
                 this.setState({
-                    ime: res.data.ime,
-                    prezime: res.data.prezime,
-                    studentId: res.data.id
+                    ime: res.data[0].ime,
+                    prezime: res.data[0].prezime,
+                    studentId: res.data[0].id,
+                    fotka: res.data[0].fotografija,
+                    mail: res.data[0].email,
+                    website: res.data[0].website,
+                    linkedIn: res.data[0].linkedin,
+                    mjRod: res.data[0].mjestoRodjenja,
+                    datRo: res.data[0].datumRodjenja
                 });
-                console.log("ime nakon get: " + this.state.ime);
-                }
+            }
             )
-            .catch(res=>{
+            .catch(res => {
                 console.log(res.error);
             });
     }
     render() {
         return (
-            <>
-                <div className="card mb-3" style={{ minWidth: "300px", maxWidth: "500px" }}>
-                    <h3 className="card-header">{this.state.ime} {this.state.prezime}</h3>
-                    <Fotografija fotografija={this.state.fotka} />
-                    <div className="card-body">
-                        <h5 className="card-title"></h5>
-                        <h6 className="card-subtitle text-muted"></h6>
+            <div class="row" style={{ margin: "0px" }}>
+                <div class="col"></div>
+                <div class="col">
+                    <div className="card mb-3" style={{ minWidth: "300px", maxWidth: "500px" }}>
+                        <h3 className="card-header">{this.state.ime} {this.state.prezime}</h3>
+                        <Fotografija fotografija={this.state.fotka} />
+
+                        <ul class="list-group list-group-flush" style={{ width: "100%", display: "inline-block" }}>
+                            <li class="card-header">
+                                Lični podaci
+                        </li>
+                            <li class="list-group-item" >
+                                <div><span class="badge badge-info">Datum i mjesto rođenja</span></div>
+                                {this.state.mjRod}
+                                <br></br>
+                                {this.state.datRo}
+                            </li>
+                            <li class="list-group-item">
+                                <div><span class="badge badge-info" >Email</span></div>
+                                {this.state.mail}
+                            </li>
+                            <li class="list-group-item">
+                                Website:&nbsp;
+                                <a href={this.state.website} class="card-link">{this.state.website}</a>
+                            </li>
+                            <li class="list-group-item">
+                                LinkedIn: <a href={this.state.linkedIn} class="card-link">{this.state.linkedIn}</a>
+                            </li>
+                        </ul>
                     </div>
-
-
                 </div>
-            </>
+                <div class="col"></div>
+
+            </div>
         );
     }
 }

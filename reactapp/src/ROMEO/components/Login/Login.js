9d95ead0bb8e2a9ee86a558f6ad8b7079a8025a4
irozajac1 <irozajac1@etf.unsa.ba>
2019-05-22 21:05:04 +0200

    Provjera greški

--- reactapp/src/ROMEO/components/Login/Login.js
+++ reactapp/src/ROMEO/components/Login/Login.js
@@ -70,23 +70,23 @@ class Login extends Component {
     }
 
     return (
-      <div class="body">
-        <div class="card text-white bg-primary mb-3">
-          <form onSubmit = {this.Submitaj} class="loginForma">
+      <div className="body">
+        <div className="card text-white bg-primary mb-3">
+          <form onSubmit = {this.Submitaj} className="loginForma">
             <label htmlFor="korisnickoIme">Korisničko ime:</label>
-            <input type="text" name="korisnickoIme" class="korisnickoIme" onChange={this.pratiPromjenuKorisnickogImena} required />
+            <input type="text" name="korisnickoIme" className="korisnickoIme" onChange={this.pratiPromjenuKorisnickogImena} required />
             <label htmlFor="password">Password:</label>
-            <input type="password" name="sifra" class="sifra" onChange={this.pratiPromjenuSifre} required/>
+            <input type="password" name="sifra" className="sifra" onChange={this.pratiPromjenuSifre} required/>
 
-            <button type="button" class="btn btn-secondary">LOGIN</button>
+            <button type="button" className="btn btn-secondary">LOGIN</button>
           </form>
           
         </div>
-        <div class="alert alert-dismissible alert-danger">
-  <button type="button" class="close" data-dismiss="alert">&times;</button>
+        <div className="alert alert-dismissible alert-danger">
+  <button type="button" className="close" data-dismiss="alert">&times;</button>
   <strong>Greška!</strong>
 </div>
-        <div class="footer">
+        <div className="footer">
         &copy; 2019 Elektrotehnički fakultet u Sarajevu
         </div>
       </div>

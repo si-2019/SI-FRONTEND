0782ac4d4aa0b0a85c769335b892d4faf5f396a4
ahabibovic1 <ahabibovic1@etf.unsa.ba>
2019-05-22 11:56:19 +0200

    Popravka unosa termina

--- reactapp/src/ECHO/NaslovnaTermin/unosTermina.js
+++ reactapp/src/ECHO/NaslovnaTermin/unosTermina.js
@@ -1,9 +1,19 @@
 import React, { Component } from "react";
+import { Alert } from "reactstrap";
+import Select from "react-select";
 
 class UnosTermina extends Component {
   constructor(props) {
     super(props);
-    this.state = { dan: "Pon", vrijeme: "08:00", brCasova: 1 };
+    this.state = {
+      dan: "Pon",
+      prikazDana: "Ponedjeljak",
+      vrijeme: "08:00",
+      brCasova: 1,
+      alertVisible: false,
+      alertMessage: "",
+      alertColor: "success"
+    };
 
     this.handleSelect = this.handleSelect.bind(this);
     this.handleVrijemeInput = this.handleVrijemeInput.bind(this);
@@ -11,10 +21,9 @@ class UnosTermina extends Component {
     this.handleSubmit = this.handleSubmit.bind(this);
   }
 
-  handleSelect(event) {
-    let x = event.target.value;
-    x = x.substr(0, 3);
-    this.setState({ dan: x });
+  handleSelect(element) {
+    this.setState({ dan: element.value });
+    this.setState({ prikazDana: element });
   }
 
   handleVrijemeInput(event) {
@@ -38,13 +47,17 @@ class UnosTermina extends Component {
     const t = Number(x);
     const br = this.state.brCasova;
     if (Number(br) + Number(t) > 20) {
-      alert("Pogrešan unos. Nastava se održava do 20:00");
+      this.setState({
+        alertMessage: "Pogrešan unos. Nastava se održava do 20:00",
+        alertVisible: true,
+        alertColor: "danger"
+      });
       return false;
     }
     return true;
   }
   postTermin(event) {
-    fetch("http://localhost:8080/si2019/echo/unesiTermine", {
+    fetch("http://localhost:31905/si2019/echo/unesiTermine", {
       method: "POST",
       headers: {
         Accept: "application/json",
@@ -57,66 +70,85 @@ class UnosTermina extends Component {
         idKabinet: 3, //hardkodirano
         vrijeme: this.state.vrijeme
       })
-    }).then(alert("Uspješno ste unijeli termin"));
+    }).then(
+      this.setState({
+        alertMessage: "Uspješno ste unijeli termin",
+        alertVisible: true,
+        alertColor: "success"
+      })
+    );
   }
-
+  toggle(x) {
+    this.setState({ alertVisible: !this.state.alertVisible });
+  }
   render() {
     return (
-      <form>
-        <div className="form-group">
-          <h4 className="col">Dodaj termin</h4>
-          <div className="col">
-            <label>Dan u sedmici</label>
-            <select
-              className="form-control m-2"
-              id="listaDana"
-              value={this.state.dan}
-              onChange={this.handleSelect}
-            >
-              <option>Ponedjeljak</option>
-              <option>Utorak</option>
-              <option>Srijeda</option>
-              <option>Cetvrtak</option>
-              <option>Petak</option>
-            </select>
+      <div>
+        <Alert
+          id="alertID"
+          color={this.state.alertColor}
+          toggle={this.toggle.bind(this)}
+          isOpen={this.state.alertVisible}
+        >
+          {this.state.alertMessage}
+        </Alert>
+        <form>
+          <div className="form-group">
+            <h4 className="col">Dodaj termin</h4>
+            <div className="col">
+              <label>Dan u sedmici</label>
+              <Select
+                className="form-group m-2"
+                id="listaDana"
+                value={this.state.prikazDana}
+                onChange={this.handleSelect}
+                options={[
+                  { value: "Pon", label: "Ponedjeljak" },
+                  { value: "Uto", label: "Utorak" },
+                  { value: "Sri", label: "Srijeda" },
+                  { value: "Cet", label: "Cetvrtak" },
+                  { value: "Pet", label: "Petak" }
+                ]}
+              />
+            </div>
+            <div className="col">
+              <label>Vrijeme</label>
+              <input
+                id="satnica"
+                type="time"
+                max="20:00"
+                min="08:00"
+                step="3600"
+                className="form-control m-2"
+                value={this.state.vrijeme}
+                onChange={this.handleVrijemeInput}
+              />
+            </div>
+            <div className="col">
+              <label>Broj časova</label>
+              <input
+                id="brCasova"
+                type="number"
+                min="1"
+                max="12"
+                className="form-control m-2"
+                value={this.state.brCasova}
+                onChange={this.handleBrCasovaInput}
+              />
+            </div>
+            <div className="col">
+              <button
+                id="dugme"
+                type="button"
+                className="btn btn-dark m-2"
+                onClick={this.handleSubmit}
+              >
+                Unesi
+              </button>
+            </div>
           </div>
-          <div className="col">
-            <label>Vrijeme</label>
-            <input
-              id="satnica"
-              type="time"
-              max="20:00"
-              min="08:00"
-              step="3600"
-              className="form-control m-2"
-              value={this.state.vrijeme}
-              onChange={this.handleVrijemeInput}
-            />
-          </div>
-          <div className="col">
-            <label>Broj časova</label>
-            <input
-              id="brCasova"
-              type="number"
-              min="1"
-              max="12"
-              className="form-control m-2"
-              value={this.state.brCasova}
-              onChange={this.handleBrCasovaInput}
-            />
-          </div>
-          <div className="col">
-            <button
-              id="dugme"
-              type="button"
-              className="btn btn-dark m-2"
-              onClick={this.handleSubmit}
-            >
-              Unesi
-            </button>
-          </div>
-        </div>
-      </form>
+        </form>
+      </div>
     );
   }
 }

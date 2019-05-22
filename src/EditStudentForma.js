eb270bac656d9c3ee0b128e644ba3d5e60984bae
samrabejtic <sbejtic1@etf.unsa.ba>
2019-05-22 23:51:44 +0200

    Edit studenta urađen

--- src/EditStudentForma.js
+++ src/EditStudentForma.js
@@ -1,31 +1,44 @@
 import React, {Component} from 'react'
-//import { postRequest } from './actions/post';
-
+import axios from 'axios'
 
 class Forma extends Component {
     constructor(props) {
         super(props)
-  
-        this.initialState = {
-          ime: '',
-          prezime: '',
-          index: '',
-          datum: '',
-          jmbg: '',
-          email: '',
-          mjesto: '',
-          kanton: '',
-          drzavljanstvo: '',
-          telefon: '',
-          zensko: '',
-          musko: '',
-          roditelj: '',
-          adresa: '',
-          spol: 'zensko'
+
+        this.state = {
+            lista: [],
+            selectedValue: '',
+            id: '',
+            ime: '',
+            prezime: '',
+            index: '',
+            email: '', 
+            telefon: '',
+            adresa: ''  
         }
-    
-        this.state = this.initialState
+       
       }
+
+      componentDidMount(){
+        //Promijeniti URL
+        //
+        axios.get ('https://jsonplaceholder.typicode.com/posts')
+        .then(response => {
+            console.log("Lista: ", response.data);
+            this.setState({lista: response.data});  
+        })
+        . catch (error =>{
+            console.log(error)
+        })
+    }
+
+    onChange = (e) => {
+        var split=e.target.value.split(" - ");     
+        //split 0: id, split 1: title 
+        this.setState({selectedValue: split[1], id: split[0], ime: split[1]})
+        console.log("AA", this.lista);
+    }
+
 
       handleChange = (event) => {
         event.preventDefault()
@@ -41,11 +54,9 @@ class Forma extends Component {
         console.log("Svi potrebni podaci: ", data)
         
         const xhr = new XMLHttpRequest();
-
         const body = JSON.stringify(data);
 
-        //Drugi URL
-        xhr.open('POST', 'http://localhost:31901/api/korisnik/AddNewStudent', true);
+        xhr.open('POST', 'http://localhost:31901/api/korisnik/updateStudent', true);
         xhr.setRequestHeader('Content-Type', 'application/json');
         xhr.onload = () => {
           if(xhr.status === 200) {
@@ -61,59 +72,48 @@ class Forma extends Component {
       }
      
 
-      handleOptionChange = changeEvent => {
-        this.setState({
-          spol: changeEvent.target.value
-        });
-      };
-
-
     render() {
-        const { ime, prezime, datum, jmbg, email, mjesto, kanton, drzavljanstvo, telefon, roditelj, adresa} = this.state;
+        const { ime, prezime, email, telefon, adresa, index, lista, selectedValue, id} = this.state;
 
         return (
           <div className="col-md-4 col-md-offset-4" >
+            <br />
+                <p>Prikaz svih odsjeka: </p><br />
+                <select className="custom-select" value={selectedValue} onChange={this.onChange}> 
+                {
+                    //paziti sta se prikazuje, nece biti list.title!!!
+                    //ako je length!=0 prikazati listu, u suprotnom vratiti null
+                    lista.length ? lista.map(list => <option key={list.id}>{list.id} - {list.title}</option>): null
+                }
+                </select><br /><br />
+                <br />
+             
             <form  onSubmit={this.handleSubmit} className="container-fluid">
+              <label className="font-weight-bold">ID studenta</label>
+              <input className="form-control font-weight-bold" readOnly value={id} /> <br />
+
               <label>Ime </label>
               <input  className="form-control" type="text" name="ime" value={ime} onChange={this.handleChange} /><br />
               
               <label >Prezime </label>
               <input className="form-control" type="text" name="prezime" value={prezime} onChange={this.handleChange}  /><br />
 
-              <label >Datum rođenja </label>
-              <input className="form-control " type="date" name="datum" value={datum} onChange={this.handleChange} /><br />
-              
-              <label>JMBG </label>
-              <input className="form-control " type="text" name="jmbg" value={jmbg} onChange={this.handleChange} /><br />
-
               <label >Email </label>
               <input className="form-control " type="email" name="email" value={email} onChange={this.handleChange} /><br />
-              
-              <label >Mjesto rođenja </label>
-              <input className="form-control " type="text" name="mjesto" value={mjesto} onChange={this.handleChange} /><br />
-              
-              <label >Kanton </label>
-              <input className="form-control " type="text" name="kanton" value={kanton} onChange={this.handleChange} /><br />
-              
-              <label>Državljanstvo </label>
-              <input className="form-control " type="text" name="drzavljanstvo" value={drzavljanstvo} onChange={this.handleChange} /><br />
 
               <label >Telefon </label>
               <input className="form-control " type="tel" name="telefon" value={telefon} onChange={this.handleChange} /><br />
-              
-              <label className="radio-inline">Spol </label>
-              <input id="1" className="custom-control custom-radio" type="radio" value="zensko" onChange={this.handleOptionChange} checked={this.state.spol === "zensko"}/> Žensko
-              <input id="2" className="custom-control custom-radio" type="radio" value="musko" onChange={this.handleOptionChange} checked={this.state.spol === "musko"}/>Muško <br/><br/>
-             
-              <label >Ime i prezime roditelja </label>
-              <input className="form-control " type="text" name="roditelj" value={roditelj} onChange={this.handleChange} /><br />
-              
+                           
               <label>Adresa </label>
               <input className="form-control" type="text" name="adresa" value={adresa} onChange={this.handleChange} /><br />
+
+              <label>Index </label>
+              <input className="form-control" type="text" name="index" value={index} onChange={this.handleChange} /><br />
               
               
-              <input type="submit" value="Submit" className="btn btn-success btn-block" />
-    </form>
+              <input type="submit" value="Edit" className="btn btn-success btn-block" />
+             </form>
+   
     </div>
         );
     }

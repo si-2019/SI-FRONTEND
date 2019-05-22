2de9aa43e9cb24d379bed8b53a37cfda585eb8e4
lamijaalagic <lalagic1@etf.unsa.ba>
2019-05-22 17:18:03 +0200

    Kreiran test

---
+++ reactapp/src/MIKE/components/UnosPodatakaONovomProjektu/UnosPodatakaONovomProjektu.test.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Enzyme, { configure, shallow, mount, render } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+Enzyme.configure({ adapter: new Adapter() });
+
+import UnosPodatakaONovomProjektu from './UnosPodatakaONovomProjektu';
+
+describe('Testovi za provjeru ispravnosti rada komponente', () => {
+    it("Provjera da li postoji forma za unos podataka", () => {
+        const wrapper = shallow(<UnosPodatakaONovomProjektu />);
+        expect(wrapper.find("#formaUnosProjekta").exists()).toBe(true);
+    });
+
+});

396fa40e262ca602b535738b05f045924cd8eec2
amesic <amesic3@etf.unsa.ba>
2019-05-23 23:40:18 +0200

    Promjena u US_21.js

--- reactapp/src/LIMA/Izvjestaji/I_US_30/I_US_30.js
+++ reactapp/src/LIMA/Izvjestaji/I_US_30/I_US_30.js
@@ -4,34 +4,27 @@ import Button from 'react-bootstrap/Button';
 import Alert from 'react-bootstrap/Alert';
 import Form from 'react-bootstrap/Form';
 
-export default class I_US_27 extends Component{
+export default class I_US_30 extends Component{
     state = {
     error: null,
     isLoaded: false,
     show:true,
     rezultati:[],
-    indeks:0,
-    predmet:''
+    indeks:0
   };
   constructor(props) {
     super(props);
       this.updateIndeks = this.updateIndeks.bind(this);
-      this.updatePredmet = this.updatePredmet.bind(this);
   }
   updateIndeks(event){
     //u string indeks dodaje kako se unosi u TB
     this.setState({indeks : event.target.value});
   }
-  updatePredmet(event){
-    //u string predmet dodaje kako se unosi u TB
-    this.setState({predmet: event.target.value});
-  }
   
 posalji = (e)=>{
   e.preventDefault();
-  fetch('http://localhost:31912/Izvjestaji/dajDrugeParcijale/'+this.state.indeks+'/'+this.state.predmet)
+  fetch('http://localhost:31912/Izvjestaji/dajNepolozenePredmete/'+this.state.indeks)
   .then(rez=>rez.json())
-  //.then(data=>{this.setState({rezultati:[...this.state.rezultati,data]})
   .then(data=>{this.setState({rezultati:data})
   console.log(this.state.rezultati);
 });
@@ -42,30 +35,25 @@ posalji = (e)=>{
     return(
       <>
         <form onSubmit={this.posalji}>
-        Unos indeksa: <br /> Trenutno uneseni indeks:{this.state.indeks}
+        Unos indeksa: <br />
         <input type = "text"  id="indeks" onChange={this.updateIndeks}></input>
         <br />
-        Unos naziva predmeta: <br />
-        <br />
-        Trenutno uneseni predmet:{this.state.predmet}
-        <input type = "text"  id="predmet" onChange={this.updatePredmet}></input>
         <input type="submit" id="dajRezultate" value="Daj rezultate"></input>
         </form>
-
-
         <Table striped bordered hover>
         <thead>
         <tr>
           <th>Redni broj</th>
           <th>Indeks</th>
-          <th>Ime</th>
-          <th>Prezime</th>
+          <th>Ime i prezime</th>
+          <th>Naziv predmeta</th>
           <th>Datum ispita</th>
+          <th>Tip ispita</th>
           <th>Broj bodova</th>
         </tr>
         </thead>
         <tbody>
-{this.state.rezultati.map(rez=>{return <tr><td>{redni_broj++}</td> <td>{rez.indeks}</td> <td>{rez.ime}</td> <td>{rez.prezime}</td> <td>{rez.datumIspita}</td> <td>{rez.brojBodova}</td></tr>})}
+{this.state.rezultati.map(rez=>{return <tr><td>{redni_broj++}</td> <td>{rez.indeks}</td> <td>{rez.naziv}</td> <td>{rez.predmet}</td> <td>{rez.datumIspita}</td> <td>{rez.tipIspita}</td> <td>{rez.brojBodova}</td></tr>})}
         </tbody>
         </Table>
         

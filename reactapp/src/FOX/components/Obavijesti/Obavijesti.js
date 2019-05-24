abe735a8614316d94e8b3e9a14b6864b995b07ba
Sejla Pljakic <sejla.pljakic@gmail.com>
2019-05-24 14:13:47 +0200

    Stil

--- reactapp/src/FOX/components/Obavijesti/Obavijesti.js
+++ reactapp/src/FOX/components/Obavijesti/Obavijesti.js
@@ -1,24 +1,26 @@
 import React, {Component } from 'react';
 import Form from 'react-bootstrap/Form';
 import Col from 'react-bootstrap/Col';
+import Header from '../Header/Header';
+import Footer from '../Footer/Footer';
 class Obavijesti extends Component {
     render() {
         return (
           <Form>
+            <Header/>
           <Form.Row>
             <Col md= {{span: 4, offset: 6 }}>
-              <Form.Label style={{fontWeight: "bold", paddingLeft:'20px'}}> Obavijesti </Form.Label>
+              <Form.Label style={{fontWeight: "bold", paddingLeft:'20px', fontSize: '15px'}}> Obavijesti </Form.Label>
             </Col>
           </Form.Row>
             
-            <div class="col-sm-6">
-               <h1> Obavijesti  </h1>
+     
           
                <form>
                     <label> Naslov: </label>
                     <input type="text" class="form-control"></input>
                 </form>
-            </div>
+          
             <div class="col-sm-6">
             </div>
           </Form>

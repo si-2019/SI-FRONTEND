73da5b59c6a311ec03db754f9673c4f9eb1d8925
Sejla Pljakic <sejla.pljakic@gmail.com>
2019-05-24 14:06:16 +0200

    Dodavanje

--- reactapp/src/FOX/components/Obavijesti/Obavijesti.js
+++ reactapp/src/FOX/components/Obavijesti/Obavijesti.js
@@ -1,12 +1,13 @@
 import React, {Component } from 'react';
-
-
+import Form from 'react-bootstrap/Form';
+import Col from 'react-bootstrap/Col';
 class Obavijesti extends Component {
     render() {
         return (
+          <Form>
           <Form.Row>
             <Col md= {{span: 4, offset: 6 }}>
-              <Form.Label className="Obavijesti"> 
+              <Form.Label style={{fontWeight: "bold", paddingLeft:'20px'}}> Obavijesti </Form.Label>
             </Col>
           </Form.Row>
             
@@ -20,10 +21,8 @@ class Obavijesti extends Component {
             </div>
             <div class="col-sm-6">
             </div>
-
+          </Form>
             
-          </div>
-          </div>
         );
     }
 

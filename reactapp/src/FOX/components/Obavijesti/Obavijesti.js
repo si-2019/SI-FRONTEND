2dc2fd1a589e40fe7272dd367d0b7bd291f2a116
Sejla Pljakic <sejla.pljakic@gmail.com>
2019-05-24 14:30:10 +0200

    Prepravke

--- reactapp/src/FOX/components/Obavijesti/Obavijesti.js
+++ reactapp/src/FOX/components/Obavijesti/Obavijesti.js
@@ -3,30 +3,26 @@ import Form from 'react-bootstrap/Form';
 import Col from 'react-bootstrap/Col';
 import Header from '../Header/Header';
 import Footer from '../Footer/Footer';
+import  Row  from 'react-bootstrap/Row';
 class Obavijesti extends Component {
     render() {
         return (
           <Form>
-            <Header/>
-          <Form.Row>
-            <Col md= {{span: 4, offset: 6 }}>
-              <Form.Label style={{fontWeight: "bold", paddingLeft:'20px', fontSize: '15px'}}> Obavijesti </Form.Label>
-            </Col>
-          </Form.Row>
-            
-     
-          
-               <form>
-                    <label> Naslov: </label>
-                    <input type="text" class="form-control"></input>
-                </form>
-          
-            <div class="col-sm-6">
-            </div>
+              <Header/>
+            <Form.Row>
+              <Col md= {{span: 4, offset: 5 }}>
+                <Form.Label style={{fontWeight: "bold", paddingLeft:'10px', fontSize: 18}}> Obavijesti </Form.Label>
+              </Col>
+            </Form.Row>
+            <row>
+                <Col md= {{span: 3, offset: 4 }}>
+                <Form.Label style={{padding: '30px'}}> Naslov: </Form.Label>
+                      <input type="text" class="form-control"></input>
+                </Col>
+            </row>
+            <Footer/>
           </Form>
-            
         );
     }
-
 }
 export default Obavijesti;

018ced6f5d3895570cf4d52dae44fba144c6358f
zfatih <fatih.zukorlic@gmail.com>
2019-05-24 02:06:26 +0200

    Dodan graficki prikaz dijagrama prisustva za godinu na predmetu

--- reactapp/src/LIMA/Izvjestaji/IzvjestajiRouter.js
+++ reactapp/src/LIMA/Izvjestaji/IzvjestajiRouter.js
@@ -3,12 +3,14 @@ import { Route, Link } from "react-router-dom";
 
 import I_US_27 from "./I_US_27";
 import I_US_30 from "./I_US_30";
+import PrisustvoPoPredmetu from "./components/Dijagrami/PrisustvoPoPredmetu";
 
 function IzvjestajiRouter() {
   return (
     <Fragment>
       <Route path="/Lima/izvjestaji/I_US_27" component={I_US_27} />
       <Route path="/Lima/izvjestaji/I_US_30" component={I_US_30} />
+      <Route path="/Lima/izvjestaji/prisustvo" component={PrisustvoPoPredmetu} />
       <Route exact path="/Lima/izvjestaji" component={Paths} />
     </Fragment>
   );
@@ -20,6 +22,7 @@ function Paths() {
       <li>
         <Link to="/Lima/izvjestaji/I_US_27"><div className="btn btn-primary">I_US_27</div></Link>
         <Link to="/Lima/izvjestaji/I_US_30"><div className="btn btn-primary">I_US_30</div></Link>
+        <Link to="/Lima/izvjestaji/prisustvo"><div className="btn btn-primary">Dijagram prisustva</div></Link>
       </li>
     </ul>
   );

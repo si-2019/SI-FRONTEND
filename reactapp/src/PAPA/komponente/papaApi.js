3fb9dc486863a3f88646d3817a4df44f2976de82
akulaglic2 <akulaglic2@etf.unsa.ba>
2019-05-25 10:47:53 +0200

    Pregled obavjestenja predmetnih profesora! Signed-off-by: akulaglic2 <akulaglic2@etf.unsa.ba>

--- reactapp/src/PAPA/komponente/papaApi.js
+++ reactapp/src/PAPA/komponente/papaApi.js
@@ -12,6 +12,9 @@ class papaApi{
     static ispitiPrijava(){
         return axios.get('http://localhost:31916/papa/ispitiPrijava', {params : {idStudent:id}});
     }
+    static obavjestenjaProfesor(){
+        return axios.get('http://localhost:31916/papa/obavjestenjaProfesor', {params : {idStudent:id}});
+    }
     static trenutniProfesori(){
         return axios.get('http://localhost:31916/papa/trenutniProfesori', {params : {idStudent:id}});
     }

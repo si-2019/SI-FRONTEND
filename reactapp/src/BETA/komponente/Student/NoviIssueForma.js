b7191de56e2ba7e61ae22d759964af6477467936
Adis Hodzic <ahodzic9@etf.unsa.ba>
2019-05-26 23:50:16 +0200

    Prepravljeno slanje issua i dodano slanje issua od SS

--- reactapp/src/BETA/komponente/Student/NoviIssueForma.js
+++ reactapp/src/BETA/komponente/Student/NoviIssueForma.js
@@ -24,9 +24,9 @@ class NoviIssueForma extends React.Component {
         // get our form data out of state
         const { issueTitle, issueText } = this.state;
 
-        axios.post('/issue/send', { issueTitle, issueText})
+        axios.post('http://localhost:31902/issue/send/s?issueTitle='+issueTitle+'&issueText='+issueText)
         .then((result) => {
-            alert("Uspjesno upisan issue"); //Ovdje treba pokupiti odgovor od backend-a, ali ne znam kako !!!!!
+            alert(resilt.data); //Ovdje treba pokupiti odgovor od backend-a, ali ne znam kako !!!!!
         });
     };
 

5ba0c855b9846a3312c7faf5cd0b4f9df7c62ed1
nezlabisevac <nbisevac1@etf.unsa.ba>
2019-05-28 01:11:40 +0200

    Zavrseno

--- reactapp/src/CHARLIE/components/UrediIspit/UrediIspit.js
+++ reactapp/src/CHARLIE/components/UrediIspit/UrediIspit.js
@@ -66,6 +66,7 @@ class UrediIspit extends React.Component {
               placeholder="Nemojte zaboraviti indeks..."
               validations={["required"]}
             />
+            <br></br>
             <input type="button" class="btn btn-primary" id="btnPotvrdi" value="Potvrdi" />
             <Link to="/charlie/kreirani-ispiti">
               <button type="button" class="btn btn-danger" id="btnPovratak">Odustani</button>

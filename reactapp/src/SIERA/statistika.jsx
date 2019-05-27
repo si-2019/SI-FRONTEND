5dd522e6e60f14c8c4e746205c0aeea03d1ab79f
nudiey <dpozderac1@etf.unsa.ba>
2019-05-27 20:35:46 +0200

    Dodavanje buttona

--- reactapp/src/SIERA/statistika.jsx
+++ reactapp/src/SIERA/statistika.jsx
@@ -4,7 +4,19 @@ import axios from "axios";
 class Statistika extends Component {
   state = {};
   render() {
-    return <div>Statistika</div>;
+    return (
+      <div className="container-fluid">
+        <div className="row">
+          <div className="align-self-start">
+            <input
+              type="button"
+              className="btn btn-info "
+              value="Sortiraj predmete po ocjeni"
+            />
+          </div>
+        </div>
+      </div>
+    );
   }
 }
 

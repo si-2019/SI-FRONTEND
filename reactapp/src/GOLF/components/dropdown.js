bb25548ea7fa5b2776652ac8da733f2e3c4d67a7
Alma Hodzic <almahodzic071@outlook.com>
2019-05-30 01:27:50 +0200

    Update dropdown.js
    popravljen dropdown item za prikaz akademske godine

--- reactapp/src/GOLF/components/dropdown.js
+++ reactapp/src/GOLF/components/dropdown.js
@@ -1,16 +1,29 @@
 import React, { Component } from 'react';
 
-class dropdown extends Component {
+class dropdown extends React.Component {
+  state = {
+    isOpen: false
+  };
+
+  toggleOpen = () => this.setState({ isOpen: !this.state.isOpen });
+
   render() {
-
+    const menuClass = `dropdown-menu${this.state.isOpen ? " show" : ""}`;
     return (
-
-    	<li class="nav-item dropdown show">
-    		<a class="nav-link dropdown-toggle" data-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="true">Dropdown</a>
-    		<div class="dropdown-menu show" x-placement="bottom-start" style="position: absolute; will-change: transform; top: 0px; left: 0px; transform: translate3d(0px, 38px, 0px);">
-    		<a class="dropdown-item" href="#">{this.props.naslov}</a>
-    		</div>
-  		</li>
+      <div className="dropdown" onClick={this.toggleOpen}>
+        <button
+          className="btn btn-success dropdown-toggle"
+          type="button"
+          id="dropdownMenuButton"
+          data-toggle="dropdown"
+          aria-haspopup="true"
+        >
+          Akademska godina
+        </button>
+        <div className={menuClass} aria-labelledby="dropdownMenuButton">
+          <a class="dropdown-item" href="#">{this.props.naslov}</a>
+        </div>
+      </div>
     );
   }
 }

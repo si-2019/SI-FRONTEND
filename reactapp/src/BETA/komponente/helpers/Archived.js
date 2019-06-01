63886e0c5d8b515101bff40d11b28a6ede19523d
mkaramujic1 <maidak_95@hotmail.com>
2019-06-01 23:39:09 +0200

    Sprint5

--- reactapp/src/BETA/komponente/helpers/Archived.js
+++ reactapp/src/BETA/komponente/helpers/Archived.js
@@ -1,23 +1,22 @@
 import React from 'react';
-import IssueMessage from "./IssueMessage";
+import ArchivedMessage from "./ArchivedMessage";
 import Card from 'react-bootstrap/Card';
 import ListGroup from 'react-bootstrap/ListGroup'
-import Button from 'react-bootstrap/Button';
+import Button from 'react-bootstrap/Button'
 import axios from 'axios'
 
-export default class Issue extends React.Component {
+export default class Archived extends React.Component {
 
     constructor(props) {
         super(props);
 
         this.state = {
-            trashStudent: 1,
-            trashSS: 0,
-            issueID: null,
             clickedItem: {
                 data: null,
                 expanded: false,
             },
+            trashStudent: 2,
+            trashSS: 0
         }
     }
 
@@ -29,30 +28,29 @@ export default class Issue extends React.Component {
         });
     };
 
-    deleteIssue = (idIssue) => {
-
-        const { trashStudent, trashSS} = this.state;
-        axios.put('http://localhost:31902/issues/archived/add', { trashStudent, trashSS, idIssue})
+    deleteIssue = (id_Issue) => {
+        const {trashStudent, trashSS} = this.state;
+        const id = id_Issue;
+            
+        axios.put('http://localhost:31902/issues/archived/delete', { trashStudent, trashSS, id })
         .then((result) => {
             alert(result);
         });
     }
-
-    componentWillReceiveProp(){
-        alert("Usao sam sada ovo");
-    }
+    
 
     render() {
             return this.props.data.map((issue, index) => {
                 return (
-                    <div className="row">
+                    
+                    <div className = "row">
                         <Card.Body
                             key={index}
                         >
                             <Card.Title>
                                 <div className = "issueView">
                                     <div className = "issueButtonDelete">
-                                        <Button onClick={() => this.deleteIssue(issue.id)}>Archive</Button>
+                                        <Button onClick = {() => this.deleteIssue(issue.id)}>Delete</Button>
                                     </div>
                                     <div className = "issueID">id:{issue.id}</div>
                                     <div onClick={() => this.setIssue(issue.id)}>{issue.title}</div>
@@ -63,7 +61,7 @@ export default class Issue extends React.Component {
 
                             {this.state.clickedItem.data === issue.id && this.state.clickedItem.expanded ?
                             <ListGroup>
-                                <IssueMessage
+                                <ArchivedMessage
                                 messages={issue.messages}
                                 /> 
                             </ListGroup> : null
@@ -72,6 +70,7 @@ export default class Issue extends React.Component {
                         </Card.Body>
                     </div>
                 );
+                
             })
         }
 }
---
+++ reactapp/src/BETA/komponente/helpers/ArchivedMessage.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import Button from 'react-bootstrap/Button';
+import ListGroup from 'react-bootstrap/ListGroup'
+
+const ArchivedMessage = (props) => {
+    return props.messages.map((message, index) => {
+        return (
+            <ListGroup.Item key={index}>
+            <ListGroup.Item>
+                <div className="row">
+                    <div className="col-1">id:{message.id}</div>
+                    <div className="col-11">{message.tekst}</div>
+                </div>
+            </ListGroup.Item>
+            <ListGroup.Item>
+            <div className="row">
+                    <div className="col-1"></div>
+                    <div className="col-11">
+                <Button variant="primary" type='submit'>Reply</Button>
+                <Button variant="primary" type='submit'>Delete</Button>
+                </div></div>
+            </ListGroup.Item>
+        </ListGroup.Item>
+
+        );
+    })
+
+};
+
+export default ArchivedMessage;

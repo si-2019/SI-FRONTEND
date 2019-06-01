5282c93b67b0ed6074c19ab0f128887e10187a09
smamela <smamela2@etf.unsa.ba>
2019-06-01 13:55:00 +0200

    Kreiranje stranice za prethodne akademske godine

---
+++ reactapp/src/GOLF/components/prethodnaAkademskaGodina.js
@@ -0,0 +1,13 @@
+import React, { Component} from 'react'
+import'./golf.css'
+
+class PrethodnaAkademskaGodina extends Component {
+
+    render(){
+        return(
+
+        )
+    }
+}
+
+export default PrethodnaAkademskaGodina

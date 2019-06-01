125b5b51976b1c1fd6cbb8b800ecef0bc73a7ead
nimsirovic1 <nimsirovic1@etf.unsa.ba>
2019-06-01 14:48:53 +0200

    Dodan it blok

--- reactapp/src/CHARLIE/components/PregledStudenata/PregledStudenata.test.js
+++ reactapp/src/CHARLIE/components/PregledStudenata/PregledStudenata.test.js
@@ -3,4 +3,6 @@ import {shallow} from 'enzyme';
 import PregledStudenata from './PregledStudenata'
 
 describe('<PregledStudenata />', () => {
+    it('da li ima button-a za vracanje nazad', () => {
+    })
 })

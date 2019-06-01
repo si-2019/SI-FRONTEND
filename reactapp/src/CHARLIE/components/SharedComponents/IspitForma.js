3e4fcdecf047e643c874159efc73e205db05316a
Naida Rascic <nrascic1@etf.unsa.ba>
2019-06-01 18:14:33 +0200

    Dodana generalna struktura stranice

--- reactapp/src/CHARLIE/components/SharedComponents/IspitForma.js
+++ reactapp/src/CHARLIE/components/SharedComponents/IspitForma.js
@@ -4,16 +4,31 @@ import { Link } from "react-router-dom";
 
 class IspitiForma extends Component {
     constructor(props) {
-      super(props);
-      const {termin}=props;
-      this.state = {
-        termin: termin || new Date()
-      };
+        super(props);
+        const { termin } = props;
+        this.state = {
+            termin: termin || new Date()
+        };
     }
     handleTerminChange = e => {
         this.setState({
-          termin: e.target
+            termin: e.target
         });
-      };
-    
+    };
+    render() {
+        <div>
+            <h2>{title || "Titl Stranice"}</h2>
+            <form style={{ display: "flex", flexDirection: "row" }}>
+                <div
+                    className="left_fields"
+                    style={{
+                        display: "inline-block",
+                        width: "30%",
+                        marginRight: "20px"
+                    }} >
+                        
+                </div>
+            </form>
+        </div>
+    }
 }

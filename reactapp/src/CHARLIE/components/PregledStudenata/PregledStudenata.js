4fd69000277f94aec626eee2e56f949c0ab101f3
nimsirovic1 <nimsirovic1@etf.unsa.ba>
2019-06-01 14:24:40 +0200

    Kreiran button

--- reactapp/src/CHARLIE/components/PregledStudenata/PregledStudenata.js
+++ reactapp/src/CHARLIE/components/PregledStudenata/PregledStudenata.js
@@ -1,10 +1,12 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 class PregledStudenata extends React.Component{
   render(){
     return(
       <div>
         Pregled studenata
+        <button type="button" class="btn btn-primary">Nazad</button>
       </div>
     )
   }

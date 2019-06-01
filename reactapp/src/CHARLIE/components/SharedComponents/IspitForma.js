21fca7979af43c4d13f1c35d2d3473898ed41500
Naida Rascic <nrascic1@etf.unsa.ba>
2019-06-01 18:05:42 +0200

    Dodana klasa IspitiForma u .js fajlu

--- reactapp/src/CHARLIE/components/SharedComponents/IspitForma.js
+++ reactapp/src/CHARLIE/components/SharedComponents/IspitForma.js
@@ -1,3 +1,10 @@
 import React, { Component } from "react";
 import DatePicker from "react-datetime";
 import { Link } from "react-router-dom";
+
+class IspitiForma extends Component {
+    constructor(props) {
+      super(props);
+      const {termin}=props;
+    }
+}

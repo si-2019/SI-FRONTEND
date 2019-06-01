b5353ce001375653696bf560fa3735421cb04a86
arogo1 <arnes_rogo@hotmail.com>
2019-06-01 18:23:05 +0200

    Dodavanje stila

--- reactapp/src/GOLF/components/proba.js
+++ reactapp/src/GOLF/components/proba.js
@@ -50,7 +50,7 @@ class proba extends Component {
       
         <div>
             <h1>Moji predmeti</h1>
-            <DodavanjeObjave></DodavanjeObjave>
+            
         </div>
         
 

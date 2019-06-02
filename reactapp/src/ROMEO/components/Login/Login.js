c880c261e3045edc2a9c304e97bd812441029955
faruk <faruk@p3rsonals-MacBook-Pro.local>
2019-06-02 14:35:11 +0200

    Dodan poruke za nepostojece korisnike ili ako nema odgovora od servera

--- reactapp/src/ROMEO/components/Login/Login.js
+++ reactapp/src/ROMEO/components/Login/Login.js
@@ -39,7 +39,15 @@ class Login extends Component {
   }
   
   validirajFormu() {
-    var passwordRegex = /^[0-9a-z]+$/
+    var passwordRegex = /^[0-9a-z]+$/;
+    if(this.state.korisnickoIme.length == 0) {
+      error='Polje korisnicko ime ne moze ostati prazno'; 
+      return false;
+    }
+    if(this.state.sifra.length == 0) {
+      error='Polje sifra ne moze ostati prazno'; 
+      return false;
+    }
     if(this.state.sifra.length < 8) {
       error='Sifra mora imati preko 7 karaktera'; 
       return false;
@@ -56,44 +64,47 @@ class Login extends Component {
   }
    
   Submitaj = (e) => {
+    document.getElementById('dioGreske').style.display = "none";
     e.preventDefault();
 	  if(!this.validirajFormu()) {
       document.getElementById('greske').innerText = error;
       document.getElementById('dioGreske').style.display = "block";
 	  } else {
+      //validacija uspjesna
+      var baseUrl = 'http://localhost:31917';
 
-      /*var url = 'http://localhost:31917/auth/login';
-
-      var params = {
+      var body = {
         username: this.state.korisnickoIme,
         password: this.state.sifra
       }
       
       var headers = {
-        "Access-Control-Allow-Origin": "*"
+        "Access-Control-Allow-Origin": "*",
+        "Content-Type": "application/json"
       }
 
-      //poziv api-a
-      axios.get(url, {params, headers}).then( res => {
-        var nes = res;
-        var data = res.data;
-        var aa = 0;
+      axios.post(baseUrl + '/login', body, headers).then((res) => {
+        var dat = res.data;
+        console.log(dat);
 
-        this.setState({
+        /*this.setState({
           logiran: true
         })
         localStorage.setItem("token", "hardcoded for now")
-        return <Redirect to="/romeo/home" />
-      });*/
-
-      //validacija uspjesna
-      if(this.state.korisnickoIme == "nepostojeci") {
-        //autentikacija neuspjesna
-        document.getElementById('greske').innerText = "Korisnik ne postoji";
+        return <Redirect to="/romeo/home" />*/
+      }).catch((error) => {
+        var res = error.response;
+        if(res) {
+          if(res.status == 403) {
+            document.getElementById('greske').innerText = "Korisnik ne postoji!";
+          } else {
+            document.getElementById('greske').innerText = "Nešto nije u redu!";
+          }
+        } else {
+          document.getElementById('greske').innerText = "Aplikacija nije dobila odgovor od servera";
+        }
         document.getElementById('dioGreske').style.display = "block";
-      } else {
-        //autentikacija uspjesna
-      }
+      });
 
       
 

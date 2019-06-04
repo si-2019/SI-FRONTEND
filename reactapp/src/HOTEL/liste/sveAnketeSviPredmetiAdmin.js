9c75709e139baebda22f6199d00b01d8f4b74d46
nejiram <nmusic1@etf.unsa.ba>
2019-06-04 18:48:02 +0200

    Kreiranje komponente za brisanje ankete

--- reactapp/src/HOTEL/liste/sveAnketeSviPredmetiAdmin.js
+++ reactapp/src/HOTEL/liste/sveAnketeSviPredmetiAdmin.js
@@ -7,6 +7,7 @@ class App extends React.Component {
         this.state = {
             items: {}
         }
+        this.obrisiAnketu = this.obrisiAnketu.bind(this)
     }
     render() {
         const items = this.state.items
@@ -48,7 +49,8 @@ class App extends React.Component {
                             <th>{anketa.datumIstekaAnkete.substr(0,10)}</th>
                             <th><button type="button" class="btn btn-primary disabled" id="prikaziButton">PRIKAŽI</button></th>
                             <th><button type="button" class="btn btn-primary disabled" id="urediButton">UREDI</button></th>
-                            <th><button type="button" class="btn btn-primary disabled" id="obrisiButton">OBRIŠI</button></th>
+                            <th><button type="button" class="btn btn-primary disabled" id="obrisiButton" 
+                                onClick= {() => this.obrisiAnketu(anketa) } >OBRIŠI</button></th>
                             </tr>
                         )))
                     }) : "Loading..."
@@ -74,6 +76,12 @@ class App extends React.Component {
                 items: [error, "error"]
             })
         })
+    }
+    obrisiAnketu(anketaZaBrisanje){
+        console.log("Morel")
+        fetch(url + '/obrisiAnketu?idKorisnik=1&idAnketa=' + anketaZaBrisanje.idAnketa, { 
+            method: 'POST'
+        }).then(() => this.componentDidMount())
     }
 }
 

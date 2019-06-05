7bd49ba3d1ae4fac5f5eb9c5b117f70a07406d52
amujak1 <amujak1@etf.unsa.ba>
2019-06-05 14:18:43 +0200

    Dodana nova komponenta UrdjivanjeOPredmetu

---
+++ reactapp/src/GOLF/components/UredjivanjeOPredmetu.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+
+class UredjivanjeOPredmetu extends Component{
+    render(){
+        return(
+            <div>
+                
+            </div>
+        )
+    }
+}
+
+export default UredjivanjeOPredmetu
+

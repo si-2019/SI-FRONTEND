8718ebe9cd2cc9abb5bc97b73c3f8614cf48b251
amujak1 <amujak1@etf.unsa.ba>
2019-06-05 14:19:55 +0200

    Dodana forma za uredjivanje predmeta i polje za uredjivanje opisa unutar forme

--- reactapp/src/GOLF/components/UredjivanjeOPredmetu.js
+++ reactapp/src/GOLF/components/UredjivanjeOPredmetu.js
@@ -3,8 +3,18 @@ import React, { Component } from 'react'
 class UredjivanjeOPredmetu extends Component{
     render(){
         return(
-            <div>
-                
+            <div className="card border-success mb-3">
+                <div className="card-header">
+                    <a href="#"><h4>Dodavanje objave</h4></a>
+                </div>
+                <div className="card-body">
+                    <form>
+                        Opis: <br></br>
+                        <textarea rows="4" id="opisPredmeta"  name="opisPredmeta" class="form-control mr-sm-2"cols="2000"></textarea>
+                       <br></br>
+                       <button>Uredi</button>
+                    </form>
+                </div>
             </div>
         )
     }

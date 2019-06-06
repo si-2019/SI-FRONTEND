4003d4189342bec5aaafe9f2c55ce0ab8ddc593c
fkalaca1 <fkalaca1@etf.unsa.ba>
2019-06-06 23:08:12 +0200

    Tabele UI fix

--- reactapp/src/FOX/components/Ispiti/Tabela.js
+++ reactapp/src/FOX/components/Ispiti/Tabela.js
@@ -2,6 +2,7 @@ import React from 'react'
 import {withRouter} from 'react-router-dom';
 import ReactTable from 'react-table'
 import StudentiTabela from './StudentiTabela';
+import Table from 'react-bootstrap/Table';
 
 const dataX = [{
     tip: 'I parcijalni',
@@ -55,45 +56,41 @@ class TabelaPredmeti extends  React.Component {
     render() {
         const data2 = this.state.data;
         var brojac = 0;
-        console.log(data2);
-        const columns = [{
-          Header: 'Tip',
-          accessor: 'tip',
-          Cell: props => <span onClick={this.funkcija} value={data2[brojac++].tip}>{props.value}</span>
-        },
-        {
-          Header: 'Datum',
-          accessor: 'datum',
-          Cell: props => <span className='datum'>{props.value}</span>
-        },
-        {
-          Header: 'Sala',
-          accessor: 'sala',
-          Cell: props => <span className='sala'>{props.value}</span>
-        },
-        {
-          Header: 'BrStudenata',
-          accessor: 'brstudenata',
-          Cell: props => <span className='brstudenata'>{props.value}</span>
-        }]
-       
+        console.log(data2);       
         return (
             <div>
-                <div>
-                    {this.state.studenti===false && <ReactTable
-                    data={data2}
-                    columns={columns}
-                    />}
-                    {this.state.studenti===true && <StudentiTabela tipIspita={this.state.data.tip}/>}
-                </div>
-                <div>
-                    Tu
-                </div>
+                {this.state.studenti === false && 
+                    <Table striped bordered responsive bsPrefix="table">
+                    <thead>
+                        <tr className="table-primary">
+                            <th>Tip</th>
+                            <th>Datum</th>
+                            <th>Sala</th>
+                            <th>BrStudenata</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {
+                            data2.map((dataf, i) => {
+                                return <tr>
+                                    <td onClick={this.funkcija} value={data2[brojac++].tip}>{dataf.tip}</td>
+                                    <td>{dataf.datum}</td>
+                                    <td>{dataf.sala}</td>
+                                    <td>{dataf.brstudenata}</td>
+                                </tr>                            
+                            })
+                            }
+                        </tbody>
+                    </Table>
+                    
+                }
+                {this.state.studenti===true && <StudentiTabela tipIspita={this.state.data.tip}/>}
             </div>
         );
       }
       
 }
+
 
 
 export default withRouter(TabelaPredmeti);

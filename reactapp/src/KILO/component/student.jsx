f0c23426bb0a134b09defc682ef055692c674fa8
ekovac2 <ekovac2@etf.unsa.ba>
2019-06-06 12:30:34 +0200

    Dodani id zadace

--- reactapp/src/KILO/component/student.jsx
+++ reactapp/src/KILO/component/student.jsx
@@ -15,79 +15,71 @@ class Student extends Component {
 
     this.state = {
       zadacaState:{
-      listaZadaca: ["Zadaća 1", "Zadaća 2", "Zadaća 3", "Zadaća 4"],
-      listaZadataka: ["Zadatak 1", "Zadatak 2", "Zadatak 3"],
-      maxBodoviPoZadacimaPoZadacama: [
-        [2, 3, 4],
-        [1, 2, 3],
-        [1, 2, 3],
-        [1, 1, 3]
-      ],
-      
-      bodoviPoZadacimaZadaca: [
-        [2, 3, 4],
-        [1, 0, 3], 
-        [1, 2, 3], 
-        [0, 0, 3]
-      ],
-     
-      stanjeZadacaPoZadacima: [
-        [0, 0, 0], 
-        [2, 2, 2], 
-        [3, 4, 0], 
-        [1, 1, 1]
-      ], 
-      /* 0 "nije poslano", 
-      1 "nije pregledano", 
-      2 "pregledano", 
-      3 "prepisano", 
-      4 "komentar"*/ 
-      postavka: [],
-      rokZaPredaju: [
-        "2020-12-01 23:59",
-        "2020-12-01 23:59",
-        "2020-12-01 23:59",
-        "2020-12-01 23:59"
-      ]},
- potrebno: [
-        [], 
-        [], 
-        [], 
-        []
-      ],  
+        listaZadaca: ["Zadaća 1", "Zadaća 2", "Zadaća 3", "Zadaća 4"],
+        listaZadataka: ["Zadatak 1", "Zadatak 2", "Zadatak 3"],
+        maxBodoviPoZadacimaPoZadacama: [
+          [2, 3, 4],
+          [1, 2, 3],
+          [1, 2, 3],
+          [1, 1, 3]
+        ],
+        bodoviPoZadacimaZadaca: [
+          [2, 3, 4],
+          [1, 0, 3], 
+          [1, 2, 3], 
+          [0, 0, 3]
+        ],
+        stanjeZadacaPoZadacima: [
+          [0, 0, 0], 
+          [2, 2, 2], 
+          [3, 4, 0], 
+          [1, 1, 1]
+        ], 
+        /* 0 "nije poslano", 
+        1 "nije pregledano", 
+        2 "pregledano", 
+        3 "prepisano", 
+        4 "komentar"*/ 
+        postavka: [],
+        rokZaPredaju: [
+          "2020-12-01 23:59",
+          "2020-12-01 23:59",
+          "2020-12-01 23:59",
+          "2020-12-01 23:59"],
+        idPoZadacimaZadaca: []  
+      },
+      potrebno: [
+          [], 
+          [], 
+          [], 
+          []],  
       ukupnoBodova: [],
       moguceBodova: [],
-    
-    blokirajSelect:false,
-    blokirajSelect2:false,
+      blokirajSelect:false,
+      blokirajSelect2:false,
       brojZadace: 1,
       brojZadatka: 1,
       //lista tipova je lista (zadace) listi (zadaci) listi (tipovi po zadatku) [[[]]]
      //listaTipova se odnosi za trenutno selektovani zadatak tugyy, zivot sam bila zakomplikovala sa [[[]]] trostrukim pristupanjem :((
-      listaTipova:[".pdf", ".txt", ".rar", ".jpeg"
-        
-    ],
+      listaTipova:[".pdf", ".txt", ".rar", ".jpeg"],
       datumSlanja: "25.05.19",
       vrijemeSlanja: "23:23",
       nazivFajla: "Zadatak1",
       velicinaFajla: "1MB",
-      komentar:
-        "zadaca je ok zadaca je ok zadaca je ok",
+      komentar:"zadaca je ok zadaca je ok zadaca je ok",
       idStudenta: 1,
       idZadatak:3,
       idPredmeta: 3
       };
-     
   }
 
-  obracunBodova =async (bodoviPoZadacimaZadaca, maxBodoviPoZadacimaPoZadacama) => {
+  obracunBodova = async (bodoviPoZadacimaZadaca, maxBodoviPoZadacimaPoZadacama) => {
     
     var arr = new Array(bodoviPoZadacimaZadaca.length);
 
     for (var i = 0; i < bodoviPoZadacimaZadaca.length; i++) {
       arr[i] = new Array(bodoviPoZadacimaZadaca[i].length);
     }
-
     var pomocniUkupno = [];
     var pomocniMoguce = [];
 
@@ -95,62 +87,46 @@ class Student extends Component {
       var zbirUkupno = 0;
       var zbirMoguce = 0;
       for (var j = 0; j < bodoviPoZadacimaZadaca[i].length; j++) {
-        arr[i][j] =
-          bodoviPoZadacimaZadaca[i][j] +
-          "/" +
-         maxBodoviPoZadacimaPoZadacama[i][j];
+        arr[i][j] = bodoviPoZadacimaZadaca[i][j] + "/" +
+                    maxBodoviPoZadacimaPoZadacama[i][j];
         zbirUkupno = zbirUkupno + bodoviPoZadacimaZadaca[i][j];
-        zbirMoguce =
-          zbirMoguce + maxBodoviPoZadacimaPoZadacama[i][j];
+        zbirMoguce = zbirMoguce + maxBodoviPoZadacimaPoZadacama[i][j];
       }
       pomocniUkupno.push(zbirUkupno);
       pomocniMoguce.push(zbirMoguce);
     }
-    
-    
-      this.setState({
+    this.setState({
         potrebno:arr,
-      ukupnoBodova: pomocniUkupno,
-      moguceBodova: pomocniMoguce
+        ukupnoBodova: pomocniUkupno,
+        moguceBodova: pomocniMoguce
     });
   };
-
- 
-
 
   componentDidMount = async() => {
      //na osnovu indeksa studenta, prikupiti podatke o zadacama
     //2. parametar axiosa, je sta ce tamo biti u backendu req.body
     var pomoc = 3;
     try{
-    const res = await axios.get( `http://localhost:31911/dajZadaceZaStudenta/${this.state.idStudenta}/${this.state.idPredmeta}`); 
-     
-    this.setState({zadacaState:res.data});
-    this.obracunBodova(res.data.bodoviPoZadacimaZadaca, res.data.maxBodoviPoZadacimaPoZadacama);
+      const res = await axios.get( `http://localhost:31911/dajZadaceZaStudenta/${this.state.idStudenta}/${this.state.idPredmeta}`);      
+      this.setState({zadacaState:res.data});
+      this.obracunBodova(res.data.bodoviPoZadacimaZadaca, res.data.maxBodoviPoZadacimaPoZadacama);
     }
     catch (e) {
       console.error("Error fetching zadaca by id", e);
     }
-
     document.getElementById("tabelaPregledaZadaca").style.display = "block";
     document.getElementById("prviPutSlanjeZadatka").style.display = "none";
     document.getElementById("zadatakVecPoslan").style.display = "none";
   };
 
-  
-  
   klikNaPoslati = async(r, k) => {
-    //nasa = rok
     var povratna_vrijednost;
     var trengodina = new Date().getFullYear();
     var trenmjesec = new Date().getMonth() + 1;
     var trendan = new Date().getDate();
-   // console.log('g: '+trengodina+ ' m: '+trenmjesec+ ' d: '+trendan);
-    //console.log('broj zadace '+r);
     var nasagodina = Number.parseInt(this.state.zadacaState.rokZaPredaju[r].substring(0, 4));
     var nasmjesec = Number.parseInt(this.state.zadacaState.rokZaPredaju[r].substring(5, 7));
     var nasdan = Number.parseInt(this.state.zadacaState.rokZaPredaju[r].substring(8, 10));
-   // console.log('***g: '+nasagodina+ ' m: '+nasmjesec+ ' d: '+nasdan);
     if (trengodina > nasagodina) povratna_vrijednost =false;
     else if (trengodina === nasagodina && trenmjesec > nasmjesec) povratna_vrijednost = false;
     else if (trengodina === nasagodina && trenmjesec === nasmjesec && trendan > nasdan)
@@ -164,30 +140,22 @@ class Student extends Component {
       povratna_vrijednost= false;
     else povratna_vrijednost= true;
 
-    
-//console.log('povratna je '+povratna_vrijednost);
-
  //validacija ako je rok prosao, nema liste tipova
     if(povratna_vrijednost) {
-   
-     
       await axios.get(`http://localhost:31911/dozvoljeniTipoviZadatka/${this.state.idZadatak}`).then(res => { 
       this.setState({listaTipova:res.data});
-      
     });
     document.getElementById("uploadButton").disabled=false;
     this.setState({blokirajSelect:false}); 
-  }
-   else {
+    }
+    else {
      this.setState({blokirajSelect:true}); 
      document.getElementById("uploadButton").disabled=true;
-   }
-    
+    }
     this.setState({
       brojZadace: r+1,
       brojZadatka: k+1
     });
-
     document.getElementById("tabelaPregledaZadaca").style.display = "none";
     document.getElementById("prviPutSlanjeZadatka").style.display = "block";
     document.getElementById("zadatakVecPoslan").style.display = "none";
@@ -196,20 +164,13 @@ class Student extends Component {
   
 
   klikNaVecPoslano = async(r, k) => {
-    
-
     var povratna_vrijednost;
     var trengodina = new Date().getFullYear();
     var trenmjesec = new Date().getMonth() + 1;
     var trendan = new Date().getDate();
-    //console.log('g: '+trengodina+ ' m: '+trenmjesec+ ' d: '+trendan);
-    //console.log('broj zadace '+r);
-    //console.log(this.state.zadacaState.rokZaPredaju[r]);
     var nasagodina = Number.parseInt(this.state.zadacaState.rokZaPredaju[r].substring(0, 4));
     var nasmjesec = Number.parseInt(this.state.zadacaState.rokZaPredaju[r].substring(5, 7));
-   
     var nasdan = Number.parseInt(this.state.zadacaState.rokZaPredaju[r].substring(8, 10));
-   // console.log('***g: '+nasagodina+ ' m: '+nasmjesec+ ' d: '+nasdan);
     if (trengodina > nasagodina) povratna_vrijednost =false;
     else if (trengodina == nasagodina && trenmjesec > nasmjesec) povratna_vrijednost = false;
     else if (trengodina == nasagodina && trenmjesec == nasmjesec && trendan > nasdan)
@@ -219,12 +180,9 @@ class Student extends Component {
       trenmjesec == nasmjesec &&
       trendan == nasdan &&
       this.state.vrijeme != "23:59"
-    )
-      povratna_vrijednost= false;
-    else povratna_vrijednost= true;//  kdkdkk
+    ) povratna_vrijednost= false;
+    else povratna_vrijednost= true;
     
-//console.log('povratna je '+povratna_vrijednost);
-
  //validacija ako je rok prosao, nema liste tipova
     if(povratna_vrijednost) {
       await axios.get(`http://localhost:31911/dozvoljeniTipoviZadatka/${this.state.idZadatak}`).then(res => { 
@@ -239,13 +197,12 @@ class Student extends Component {
    document.getElementById("uploadButton2").disabled=true;
 }
  await axios.get(`http://localhost:31911/popuniZadatakVecPoslan/${this.state.idZadatak}`).then(res => { 
-    //console.log(res.data);
-    //console.log(res.data);
+    
         this.setState({datumSlanja:res.data.datumSlanja,
         vrijemeSlanja:res.data.vrijemeSlanja,
-      nazivFajla:res.data.nazivFajla,
-    velicinaFajla:res.data.velicinaFajla,
-  komentar:res.data.komentar});
+        nazivFajla:res.data.nazivFajla,
+        velicinaFajla:res.data.velicinaFajla,
+        komentar:res.data.komentar});
       });
 
     this.setState({
@@ -280,40 +237,28 @@ class Student extends Component {
           //poslati backendu fajl
           //kod post, drugi parametar je body!!! postttt
            await axios.post("http://localhost:31911/slanjeZadatka",nazivUploada).then(res => { 
-   //console.log('odgovor ');
-   console.log(res.data);
-  });
+             console.log(res.data);
+           });
 }
         }
-          
-           
         break;
       }
       case "ponisti": {
         var temp=document.getElementById("uploadButton").value;
         if(temp==="") document.getElementById("uploadButton2").value="";
         else document.getElementById("uploadButton").value="";
-        //imala 2 elementa sa istim id-em, pa je to pravilo probleme
         break;
       }
-
-      
       case "preuzmi": {
         //salji na rutu u backendu
         await axios.get("http://localhost:31911/getDatoteku").then(res => { 
-   //console.log('odgovor ');
-   //console.log(res.data);
-  });
-        
+        }); 
         break;
       }
-
       case "pregled": {
         //salji na rutu u backendu
 
          await axios.get("http://localhost:31911/getPregledDatoteke").then(res => { 
-          //console.log('odgovor ');
-          //console.log(res.data);
          });
         break;
       }
@@ -322,20 +267,12 @@ class Student extends Component {
     }
   };
   
- handleBack=()=>{
-  document.getElementById("tabelaPregledaZadaca").style.display = "block";
-  document.getElementById("prviPutSlanjeZadatka").style.display = "none";
-  document.getElementById("zadatakVecPoslan").style.display = "none";
-  
-
- }
-
- 
-
-
+  handleBack = () => {
+    document.getElementById("tabelaPregledaZadaca").style.display = "block";
+    document.getElementById("prviPutSlanjeZadatka").style.display = "none";
+    document.getElementById("zadatakVecPoslan").style.display = "none";
+  }
   render() {
-   
-    //console.log('medi: '+this.state.zadacaState);
     console.log('potrebno: '+this.state);
     console.log(this.state);
     return (

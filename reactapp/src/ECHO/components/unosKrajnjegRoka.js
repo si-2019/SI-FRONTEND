cf84d38e9aa0664cbafc6e8e3c0b4c2b72a49a1c
Armin <armin.bilal@hotmail.com>
2019-06-07 17:46:17 +0200

    Popravljen UI kalendar.

---
+++ reactapp/src/ECHO/components/unosKrajnjegRoka.js
@@ -0,0 +1,39 @@
+// Let's use moment static reference in the Datetime component.
+import React, { Component } from 'react';
+import Datetime from 'react-datetime';
+import "./unosKrajnjegRoka.css";
+
+
+class UnosKrajnjegRoka extends Component{
+    render() {
+        return (
+            <div className="divFormaKrajnjegRoka">
+                
+                <form>
+                <div class="card">
+                    <div class="card-body">
+                    <p class="card-text">Unesite krajnji rok za unos i promjenu slobodnih termina nastavnog osoblja:</p>
+                <br></br>
+
+                    <div id="KalendarZaUnos">
+                    <Datetime input={ false } isValidDate={ valid }   />
+                    </div>
+                    <button type="button" id="Potvrda" class="btn btn-primary">Unesi</button>
+                </div>
+                </div>
+                </form>
+            
+            </div>
+        );
+    }
+}
+    
+
+    var yesterday = Datetime.moment().subtract(1, 'day');
+    var valid = function( current ){
+    return current.isAfter( yesterday );
+    }
+
+
+export default UnosKrajnjegRoka;
+

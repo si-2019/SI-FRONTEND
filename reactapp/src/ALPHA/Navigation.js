fe413f392569d4b4133aeb1da1b0a2ad1d039e16
Amra Memagic <amemagic1@etf.unsa.ba>
2019-06-07 20:59:00 +0200

    3-91

--- reactapp/src/ALPHA/Navigation.js
+++ reactapp/src/ALPHA/Navigation.js
@@ -10,7 +10,8 @@ const Navigation = () => {
       <NavLink to="/student" className="col-sm-2" style={{color: "#ecf0f1"}}>Student</NavLink>
       <NavLink to="/odsjek" className="col-sm-2" style={{color: "#ecf0f1"}}>Odsjek</NavLink>
       <NavLink to="/profesor" className="col-sm-2" style={{color: "#ecf0f1"}}>Profesor</NavLink>
-      <NavLink to="/asistent" className="col-sm-2" style={{color: "#ecf0f1"}}>Asistent</NavLink>
+      <NavLink to="/asistent" className="col-sm-2" style={{color: "#ecf0f1"}}>Asistent</NavLink> 
+      <NavLink to="/predmet" className="col-sm-2" style={{color: "#ecf0f1"}}>Predmet</NavLink> 
     </div>
     </nav>
   );

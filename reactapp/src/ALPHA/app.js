88fd96d336cdda8f9b400e6ce16bfbab1f8ade1f
ehanjalic <ehanjalic1@etf.unsa.ba>
2019-06-07 13:27:57 +0200

    Alpha Sprint3 Story 96

--- reactapp/src/ALPHA/app.js
+++ reactapp/src/ALPHA/app.js
@@ -4,6 +4,7 @@ import DodajStudenta from './dodajStudenta'
 import DodajOdsjek from './dodajOdsjek'
 import DodajProfesora from './dodajProfesora'
 import DodajAsistenta from './dodajAsistenta'
+import DodajPredmet from './dodajPredmet'
 import Navigation from './Navigation'
 
 //rutiranje, prebacivanje sa student na odsjek page i obrnuto
@@ -15,10 +16,11 @@ class App extends Component{
             <div>
              <Navigation />
                 <Switch>
-                    <Route path="/Alpha" component={DodajStudenta} exact/>
-                    <Route path="/Alpha/odsjek" component={DodajOdsjek} />
-                    <Route path="/Alpha/profesor" component={DodajProfesora} />
-                    <Route path="/Alpha/asistent" component={DodajAsistenta} />
+                    <Route path="/student" component={DodajStudenta} exact/>
+                    <Route path="/odsjek" component={DodajOdsjek} />
+                    <Route path="/profesor" component={DodajProfesora} />
+                    <Route path="/asistent" component={DodajAsistenta} />
+                    <Route path="/predmet" component={DodajPredmet} />
                 </Switch>
             </div>
             </BrowserRouter>
---
+++ reactapp/src/ALPHA/dodajPredmet.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react'
+import FormaPredmet from './FormaPredmet'
+import FormaProfPred from './FormaProfPred'
+
+import Tabs from 'react-bootstrap/Tabs'
+import Tab from 'react-bootstrap/Tab'
+
+class DodajPredmet extends Component {
+    render () {
+        return (
+            <div className="dodajPredmet" style={{color: "#2C3E50"}}>
+                <h1 style={{color: "#18BC9C"}}>Predmet</h1>
+                <Tabs defaultActiveKey="home" id="uncontrolled-tab-example">
+                    <Tab eventKey="home" title="Dodaj predmet">
+                        <FormaPredmet />
+                    </Tab>
+                    
+                    <Tab eventKey="profile" title="Dodaj profesora na predmet">
+                        <FormaProfPred />
+                    </Tab>
+                  
+                </Tabs>
+</div>
+        )
+    }
+}
+
+export default DodajPredmet

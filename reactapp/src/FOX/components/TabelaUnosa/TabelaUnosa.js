76d95f95e9d630df330afcb8eb66e0fdc3bf3776
Sejla Pljakic <sejla.pljakic@gmail.com>
2019-06-07 19:19:38 +0200

    Izmjene zavrsene

--- reactapp/src/FOX/components/TabelaUnosa/TabelaUnosa.js
+++ reactapp/src/FOX/components/TabelaUnosa/TabelaUnosa.js
@@ -70,26 +70,16 @@ class TabelaUnosa extends Component {
                                 <Card.Body>
                                     <Card.Title style={{ display: "inline-block", fontSize: '30px', textAlign: "center"}} >Unos bodova ispita </Card.Title>
                                     <Card.Subtitle className="mb-2 text-muted">U ovoj formi možete unijeti bodove pojedinačnom studentu</Card.Subtitle>
-                                    <Form.Group as={Col} sm={{span: 4, offset: 4}}>
+                                    <Form.Group as={Col} sm={{span: 6, offset: 2}}>
                                     <select class="custom-select">
-                                        <option selected="">Open this select menu</option>
-                                        <option value="1">I parcijalni, 20.4.2019</option>
-                                        <option value="2">Two</option>
-                                        <option value="3">Three</option>
+                                        <option selected="">Otvori za odabir ispita</option>
+                                        <option value="1">I parcijalni, 20.4.2019.</option>
+                                        <option value="2">II parcijalni, 20.6.2019.</option>
+                                        <option value="3">Popravni I parcijalni, 1.7.2019.</option>
+                                        <option value="3">Popravni II parcijalni, 1.7.2019.</option>
+                                        <option value="3">Integralni ispit 1.9.2019.</option>
                                     </select>
-
-                                        <Form.Control
-                                            as="select">
-                                            <option>.</option>
-                                            <option> II parcijalni, 20.6.2019.</option>
-                                            <option>Popravni I parcijalni, 1.7.2019.</option>
-                                            <option>Popravni II parcijalni, 1.7.2019.</option>
-                                            <option>Integralni ispit 1.9.2019.</option>
-                                        </Form.Control>
                                     </Form.Group>
-                                    
-                                    
-                                    
                                     <Form.Group as={Row} controlId = "index">
                                         <Col lg="6" style={{textAlign: "left"}} > 
                                         <Form.Label>Index:</Form.Label>

a2207798bbb74c1c872c834e9df3df752e695333
Amra Memagic <amemagic1@etf.unsa.ba>
2019-06-07 21:12:24 +0200

    3-94

--- reactapp/src/ALPHA/dodajOdsjek.js
+++ reactapp/src/ALPHA/dodajOdsjek.js
@@ -1,12 +1,35 @@
 import React, { Component } from 'react'
 import Forma from './FormaOdsjek'
+import FormaOdsjekPred from './FormaOdsjekPred'
+import Prikaz from './prikazOdsjeka'
+import Edit from './editOdsjeka'
+
+import Tabs from 'react-bootstrap/Tabs'
+import Tab from 'react-bootstrap/Tab'
 
 class DodajOdsjek extends Component {
     render () {
         return (
             <div className="dodajStudenta" style={{color: "#2C3E50"}}>
                 <h1 style={{color: "#18BC9C"}}>Odsjek</h1>
-                <Forma />
+                <Tabs defaultActiveKey="home" id="uncontrolled-tab-example">
+                    <Tab eventKey="home" title="Dodaj Odsjek">
+                        <Forma />
+                    </Tab>
+                    
+                    <Tab eventKey="profile" title="Dodaj predmet na odsjek">
+                        <FormaOdsjekPred />
+                    </Tab>
+					
+					<Tab eventKey="profile1" title="Prikaz odsjeka">
+                    <Prikaz />
+                </Tab>
+                <Tab eventKey="profile2" title="Izmjeni odsjek">
+                    <Edit />
+                </Tab>
+                  
+                </Tabs>               
+
             </div>
         )
     }

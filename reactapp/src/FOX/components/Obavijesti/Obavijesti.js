28472691f9f0dcb8dc370d0c910630af76ee4b65
Sejla Pljakic <sejla.pljakic@gmail.com>
2019-06-07 18:44:13 +0200

    Forma obavijesti izmjene

--- reactapp/src/FOX/components/Obavijesti/Obavijesti.js
+++ reactapp/src/FOX/components/Obavijesti/Obavijesti.js
@@ -5,9 +5,7 @@ import Header from '../Header/Header';
 import Footer from '../Footer/Footer';
 import  Row  from 'react-bootstrap/Row';
 import Button from 'react-bootstrap/Button'
-import Row from 'react-bootstrap/Row';
 import Card from 'react-bootstrap/Card';
-import axios from 'axios';
 import '../../ZajednickiCSS.css';
 import Container from 'react-bootstrap/Container';
 
@@ -19,36 +17,34 @@ class Obavijesti extends Component {
                 <Col sm={{span: 4}} style={{textAlign: "left"}}>
                     <Card>
                         <Card.Body>
-                            <Card.Title style={{ display: "inline-block", fontSize: '30px', textAlign: "center"}} >Unos ocjene </Card.Title>
-                            <Card.Subtitle className="mb-2 text-muted">U ovoj formi možete unijeti ocjenu pojedinačnom studentu</Card.Subtitle>
-                            <Form.Group as={Row} controlId = "index">
+                            <Card.Title style={{ display: "inline-block", fontSize: '30px', textAlign: "center"}} >Obavijesti </Card.Title>
+                            <Card.Subtitle className="mb-2 text-muted">U ovoj formi možete poslati obavijest </Card.Subtitle>
+                            <Form.Group as={Row} controlId = "naziv">
                                 <Col lg="6" style={{textAlign: "left"}} > 
-                                <Form.Label>Naslov:</Form.Label>
+                                <Form.Label>Naziv:</Form.Label>
                                     <Form.Control 
                                         required 
                                         type="text" 
-                                        placeholder="Unesi index"
+                                        placeholder="Unesi naziv obavijesti"
                                     />
                                 <br/>
                                 </Col>   
                             </Form.Group>
-                            <Form.Group as={Row} controlId = "formNoviOpis">
+                            <Form.Group as={Row} controlId = "formNovi">
                                 <Col style={{textAlign: "left"}}>
-                                            <Form.Label>Ocjena:</Form.Label>
-                                            <Col lg="4">
-                                                <Form.Control as="textarea" rows="5">
-                                                </Form.Control>
-                                            </Col>
-                                            <br/>
-                                            <Button variant="primary" type="submit" >Pošalji </Button> 
-                                    </Col>
-                                    <Col></Col>
-                                </Form.Group>
-                            </Card.Body>  
-                        </Card>
-                    </Col>
-                </Row>
-        </Container> 
+                                    <Form.Label>Sadržaj:</Form.Label>
+                                    <Form.Control as="textarea" rows="5">
+                                    </Form.Control>
+                                    <br/>
+                                    <Button variant="primary" type="submit" >Pošalji </Button> 
+                                </Col>
+                                <Col></Col>
+                            </Form.Group>
+                        </Card.Body>  
+                    </Card>
+                </Col>
+            </Row>
+    </Container> 
         );
     }
 }

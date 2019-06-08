ebd6fb7dd4805f2b35101a9d3c9fef6e1653d11b
Nedim Tarakčija <nedimt@hotmail.com>
2019-06-08 17:38:39 +0200

    Izmijenjen layout komponenti UnosOcjene i Ocjena.

--- reactapp/src/FOX/components/UnosOcjene/UnosOcjene.js
+++ reactapp/src/FOX/components/UnosOcjene/UnosOcjene.js
@@ -2,14 +2,30 @@ import React, { Component} from 'react';
 import Ocjena from '../Ocjena/Ocjena';
 import Header from '../Header/Header';
 import Footer from '../Footer/Footer';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+
 
 class UnosOcjene extends Component {
      render()
      {
          return (
             <div>
-                <Header isPocetna={false}/>
-                <Ocjena/>
+                <Container fluid style={{padding:"0", margin: "0"}}>
+                    <Row>
+                        <Col md="3">
+                            <Header isPocetna={false}/>
+                        </Col>
+                        <Col>
+                            <br/>       
+                            <h4>Unos ocjene</h4>
+                            <br/>
+                            <Ocjena/>
+                        </Col>
+                    </Row>
+                </Container>
+                
                 <Footer/>
             </div>        
          );

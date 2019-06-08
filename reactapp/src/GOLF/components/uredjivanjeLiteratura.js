c7180b8f7be16b9c10c887babd3b2d4b315f9488
Samira Mamela <37188725+smamela2@users.noreply.github.com>
2019-06-08 15:17:23 +0200

    popravke

--- reactapp/src/GOLF/components/uredjivanjeLiteratura.js
+++ reactapp/src/GOLF/components/uredjivanjeLiteratura.js
@@ -5,10 +5,10 @@ class uredjivanjeLiteratura extends Component {
  render(){
 
     return (
-        
+        <div>
 			<textarea class="opis-uredjivanje" id="uredjivanjeTextLiteratura" placeholder="Tekst/sažetak literature" rows="3"></textarea>
 			<input className="dugmic" type="file" multiple></input>
-        
+        </div>
         
     );
   }

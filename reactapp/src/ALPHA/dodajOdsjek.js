2b9437c70dc72551444826c341414335c1c07792
ehanjalic <ehanjalic1@etf.unsa.ba>
2019-06-08 23:30:36 +0200

    AlphaUI test

--- reactapp/src/ALPHA/dodajOdsjek.js
+++ reactapp/src/ALPHA/dodajOdsjek.js
@@ -3,6 +3,7 @@ import Forma from './FormaOdsjek'
 import FormaOdsjekPred from './FormaOdsjekPred'
 import Prikaz from './prikazOdsjeka'
 import Edit from './editOdsjeka'
+import './App.css'
 
 import Tabs from 'react-bootstrap/Tabs'
 import Tab from 'react-bootstrap/Tab'
@@ -10,8 +11,9 @@ import Tab from 'react-bootstrap/Tab'
 class DodajOdsjek extends Component {
     render () {
         return (
-            <div className="dodajStudenta" style={{color: "#2C3E50"}}>
-                <h1 style={{color: "#18BC9C"}}>Odsjek</h1>
+            <div className="dodajOdsjek" style={{color: "#2C3E50"}} id="content">
+                <h1 style={{color: "#fff", background: "#2C3E50"}} id="h1">Odsjek</h1>
+            
                 <Tabs defaultActiveKey="home" id="uncontrolled-tab-example">
                     <Tab eventKey="home" title="Dodaj Odsjek">
                         <Forma />

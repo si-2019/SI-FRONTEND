1c46ce901548d3d4d135be8a3f2691f9c919d0d6
Nedim Tarakčija <nedimt@hotmail.com>
2019-06-09 18:22:06 +0200

    Centrirane kartice sa predmetima.

--- reactapp/src/FOX/components/Predmet/Predmet.js
+++ reactapp/src/FOX/components/Predmet/Predmet.js
@@ -37,43 +37,26 @@ class Predmet extends Component {
           return (
                <div>
 
-               <Container fluid>
-                    <Row style={{margin: "0"}}>
-                    {this.state.predmeti.map(p => {
-                         return (
-                              <div as={Col} class="card" key={p.naziv} style={{maxWidth: "300px", margin: "5px"}}>
-                                   <div class="card-body">
-                                        <h4 class="card-title text-center"><a href={`fox/stranicaPredmeta?predmetId=${p.naziv}`} style={{color: "primary"}}> {p.naziv} </a></h4>
-                                        <h6 class="card-subtitle mb-2 text-muted">Broj studenata: 150</h6>
+                    <Container fluid>
+                         <Row style={{margin: "0"}} className="justify-content-md-center">
+                         {this.state.predmeti.map(p => {
+                              return (
+                                   <div as={Col} class="card" key={p.naziv} style={{maxWidth: "300px", margin: "5px"}}>
+                                        <div class="card-body">
+                                             <h4 class="card-title text-center"><a href={`fox/stranicaPredmeta?predmetId=${p.naziv}`} style={{color: "primary"}}> {p.naziv} </a></h4>
+                                             <h6 class="card-subtitle mb-2 text-muted">Broj studenata: 150</h6>
+                                        </div>
+                                        <div class="list-group list-group-flush">
+                                             {
+                                                  this.state.grupe.map(g => <a class="list-group-item list-group-item-action" key={g.naziv} href={`fox/stranicaPredmeta?predmetId=${p.naziv}?${g.naziv}`}> {g.naziv} </a>)
+                                             }
+                                        </div>
                                    </div>
-                                   <div class="list-group list-group-flush">
-                                        {
-                                             this.state.grupe.map(g => <a class="list-group-item list-group-item-action" key={g.naziv} href={`fox/stranicaPredmeta?predmetId=${p.naziv}?${g.naziv}`}> {g.naziv} </a>)
-                                        }
-                                   </div>
-                              </div>
-                         );
-                    })}
-                    </Row>
-               </Container>
+                              );
+                         })}
+                         </Row>
+                    </Container>
 
-               {/* <CardDeck>
-                    {this.state.predmeti.map(p => {
-                         return (
-                              <div class="card" style={{color: "black"}} key={p.naziv}>
-                                   <div class="card-body">
-                                        <h4 class="card-title text-center"><a href={`fox/stranicaPredmeta?predmetId=${p.naziv}`}> {p.naziv} </a></h4>
-                                        <h6 class="card-subtitle mb-2 text-muted">Broj studenata: 150</h6>
-                                   </div>
-                                   <div class="list-group list-group-flush">
-                                        {
-                                             this.state.grupe.map(g => <a class="list-group-item list-group-item-action" key={g.naziv} href={`fox/stranicaPredmeta?predmetId=${p.naziv}?${g.naziv}`}> {g.naziv} </a>)
-                                        }
-                                   </div>
-                              </div>
-                         );
-                    })}
-               </CardDeck> */}
                </div> 
           );
      }

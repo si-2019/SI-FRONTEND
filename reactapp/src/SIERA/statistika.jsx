7d96b7c9a1bce0996b1bdb974dda90652bf4cdd3
nudiey <dpozderac1@etf.unsa.ba>
2019-06-09 22:47:38 +0200

    Dodavanje dugmeta za sortiranje predmeta po ocjenama

--- reactapp/src/SIERA/statistika.jsx
+++ reactapp/src/SIERA/statistika.jsx
@@ -2,13 +2,26 @@ import React, { Component } from "react";
 import axios from "axios";
 
 class Statistika extends Component {
-    state = { }
+    state = {
+        prikaziSortiranePoOcjeni: false
+    };
+    
+    promijeniPrikazPoOcjeni() {
+        this.setState({ prikaziSortiranePoOcjeni: true });
+    }
+
     render() {
       return (
         <div className="container-fluid">
           <div className="row" style={{ marginTop: "0.8em" }}>
             <div>
-              Div
+                <input
+                type="button"
+                className="btn btn-primary"
+                value="Sortiraj predmete po ocjeni"
+                onClick={() => this.promijeniPrikazPoOcjeni()}
+                style={{marginLeft:"20px"}}
+                />
             </div>
           </div>
         </div>

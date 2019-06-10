10fd892b745949eb79fcfa97076f05523ae0a6a3
samrabejtic <sbejtic1@etf.unsa.ba>
2019-06-10 19:01:35 +0200

    2-90

--- reactapp/src/ALPHA/dodajOdsjek.js
+++ reactapp/src/ALPHA/dodajOdsjek.js
@@ -1,37 +1,26 @@
 import React, { Component } from 'react'
 import Forma from './FormaOdsjek'
-import FormaOdsjekPred from './FormaOdsjekPred'
-import Prikaz from './prikazOdsjeka'
-import Edit from './editOdsjeka'
-
-
+import Povezivanje from './povezivanjeOdsjekPredmet'
 import Tabs from 'react-bootstrap/Tabs'
 import Tab from 'react-bootstrap/Tab'
 
 class DodajOdsjek extends Component {
     render () {
         return (
-            <div className="dodajOdsjek" style={{color: "#2C3E50"}} id="content">
-                <h1 style={{color: "#fff", background: "#2C3E50"}} id="h1">Odsjek</h1>
-            
+            <div className="dodajStudenta" style={{color: "#2C3E50"}}>
+                <h1 style={{color: "#18BC9C"}}>Odsjek</h1>
                 <Tabs defaultActiveKey="home" id="uncontrolled-tab-example">
-                    <Tab eventKey="home" title="Dodaj Odsjek">
-                        <Forma />
-                    </Tab>
-                    
-                    <Tab eventKey="profile" title="Dodaj predmet na odsjek">
-                        <FormaOdsjekPred />
-                    </Tab>
-					
-					<Tab eventKey="profile1" title="Prikaz odsjeka">
-                    <Prikaz />
-                </Tab>
-                <Tab eventKey="profile2" title="Izmjeni odsjek">
-                    <Edit />
-                </Tab>
-                  
-                </Tabs>               
 
+                <Tab eventKey="home" title="Dodaj odsjek">
+                    <Forma />
+                </Tab>
+
+                <Tab eventKey="profile" title="Povezi odsjek sa predmetom">
+                    <Povezivanje />
+                </Tab>
+
+                
+            </Tabs>
             </div>
         )
     }
--- reactapp/src/ALPHA/dodajStudenta.js
+++ reactapp/src/ALPHA/dodajStudenta.js
@@ -1,19 +1,15 @@
 import React, { Component } from 'react'
 import Forma from './Forma'
 import FormaUpis from './FormaUpis'
-import FormaPrikazStud from './FormaPrikazStud'
-import './alphaCss.css'
-
-
+import EditForm from './EditStudentForma'
 import Tabs from 'react-bootstrap/Tabs'
 import Tab from 'react-bootstrap/Tab'
 
 class DodajStudenta extends Component {
     render () {
         return (
-            <div className="dodajStudenta" style={{color: "#2C3E50"}} id="content">
-                <h1 style={{color: "#fff", background: "#2C3E50"}} id="h1">Student</h1>
-            
+            <div className="dodajStudenta" style={{color: "#2C3E50"}}>
+                <h1 style={{color: "#18BC9C"}}>Student</h1>
             <Tabs defaultActiveKey="home" id="uncontrolled-tab-example">
 
                 <Tab eventKey="home" title="Dodaj studenta">
@@ -24,8 +20,8 @@ class DodajStudenta extends Component {
                     <FormaUpis />
                 </Tab>
 
-                <Tab eventKey="prikaz" title="Prikaz studenata">
-                    <FormaPrikazStud />
+                <Tab eventKey="edit" title="Izmjene podataka">
+                    <EditForm />
                 </Tab>
             </Tabs>
             

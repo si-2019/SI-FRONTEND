faa3c5293699ba0bda60afc65117147af77a9c16
samrabejtic <sbejtic1@etf.unsa.ba>
2019-06-10 19:23:19 +0200

    2-93

--- reactapp/src/ALPHA/FormaUpis.js
+++ reactapp/src/ALPHA/FormaUpis.js
@@ -56,9 +56,12 @@ class FormaUpis extends Component {
         const { name, ciklus, sem, tip, odsjek } = this.state;
 
         return (
+          
           <div className="col-md-2">
           
             <form  onSubmit={this.OnSubmit} className="container-fluid">
+              
+              <br />
               <label>Ime i prezime studenta </label>
               <input className="form-control" type="text" name="name" value={name} onChange={this.handleInputChange} /><br />
 

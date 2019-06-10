8804877a84bbed8ed9ef0eff990218f235faf440
skamencic1 <skamencic1@etf.unsa.ba>
2019-06-10 21:11:10 +0200

    2-233

--- reactapp/src/ALPHA/dodajPredmet.js
+++ reactapp/src/ALPHA/dodajPredmet.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react'
 import FormaPredmet from './FormaPredmet'
-import FormaProfPred from './FormaProfPred'
+import Forma from './editPredmeta'
+import FormaPrikaz from './prikazPredmeta'
+
 
 import Tabs from 'react-bootstrap/Tabs'
 import Tab from 'react-bootstrap/Tab'
@@ -8,15 +10,20 @@ import Tab from 'react-bootstrap/Tab'
 class DodajPredmet extends Component {
     render () {
         return (
-            <div className="dodajPredmet" style={{color: "#2C3E50"}}>
-                <h1 style={{color: "#18BC9C"}}>Predmet</h1>
+            <div className="dodajPredmet" style={{color: "#2C3E50"}} id="content">
+                <h1 style={{color: "#fff", background: "#2C3E50"}} id="h1">Predmet</h1>
+            
                 <Tabs defaultActiveKey="home" id="uncontrolled-tab-example">
                     <Tab eventKey="home" title="Dodaj predmet">
                         <FormaPredmet />
                     </Tab>
                     
-                    <Tab eventKey="profile" title="Dodaj profesora na predmet">
-                        <FormaProfPred />
+                    <Tab eventKey="profile2" title="Uredi predmet">
+                        <Forma />
+                    </Tab>
+					
+					<Tab eventKey="profile3" title="Prikaz predmeta">
+                        <FormaPrikaz />
                     </Tab>
                   
                 </Tabs>

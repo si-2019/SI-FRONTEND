9ae7f5f341b6348ac5342b06084ce1ebd467026f
eminacatic <ecatic1@etf.unsa.ba>
2019-06-11 18:15:58 +0200

    inicijalni commit

--- reactapp/src/UNIFORM/components/Raspored/rasporedSaleZaStudenta/body_cell.js
+++ reactapp/src/UNIFORM/components/Raspored/rasporedSaleZaStudenta/body_cell.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+﻿import React, { Component } from 'react';
 import ReactDom from 'react-dom';
 import Popup from 'react-popup';
 import Modal from 'react-modal';
@@ -31,7 +31,7 @@ const MojModal = (ovaj) => {
                style = {customStyles}
             >
               <b>Tip aktivnosti: </b>{ovaj.props.termin.title} <br></br>
-              <b>Predmet: </b><a href="http://www.etf.unsa.ba" >{ovaj.props.termin.predmet}</a> <br></br>
+              
               <b>Sala: </b><a href="http://www.etf.unsa.ba">{ovaj.props.termin.sala}</a><br></br> 
               <b>Bilješka: </b>{ovaj.props.termin.biljeska} <br></br>            
               <b>Unesi novu/ažuriraj staru bilješku</b>    <br/>

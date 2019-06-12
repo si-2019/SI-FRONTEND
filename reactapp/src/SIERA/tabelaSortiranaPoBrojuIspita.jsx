d883d56da34a974a8f2e6ecee26b2bfdba132bf0
nudiey <dpozderac1@etf.unsa.ba>
2019-06-12 16:11:34 +0200

    Pocetak rada na tabeli

---
+++ reactapp/src/SIERA/tabelaSortiranaPoBrojuIspita.jsx
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+import axios from "axios";
+
+class TabelaSortiranaPoBrojuIspita extends Component {
+  state = {};
+
+  render() {
+    return (
+      <div className="col-sm-12 col-xs-12 col-md-12 col-lg-4">
+        Div
+      </div>      
+    );
+  }
+}
+
+export default TabelaSortiranaPoBrojuIspita;

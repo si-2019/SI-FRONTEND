83f3451eae56605e8c3f81aebf7b15aac0a0b5c2
nudiey <dpozderac1@etf.unsa.ba>
2019-06-12 16:55:33 +0200

    Povezivanje sa API-jem

--- reactapp/src/SIERA/tabelaSortiranaPoBrojuIspita.jsx
+++ reactapp/src/SIERA/tabelaSortiranaPoBrojuIspita.jsx
@@ -4,13 +4,29 @@ import axios from "axios";
 class TabelaSortiranaPoBrojuIspita extends Component {
     state = {
         sortiraniPredmetiPoBrojuIspita: [],
-        brojIspita: []
+        brojIspita: [],
+        trenutnoLogovaniStudentID: 100
     };
+
+    componentDidMount() {
+        axios
+            .get(
+                `http://localhost:31918/predmeti/brojIspita/` +
+                this.state.trenutnoLogovaniStudentID + "/sort"
+            )
+            .then(res => {
+                if (res.data.ispiti != undefined) {
+                    const sortiraniPredmeti = res.data.ispiti.map(obj => obj.naziv);
+                    const brojIspita = res.data.ispiti.map(obj => obj.brojPolaganja);
+                    this.setState({ sortiraniPredmetiPoBrojuIspita: sortiraniPredmeti, brojIspita: brojIspita });
+                }
+            });
+    }
 
     render() {
         return (
             <div className="col-sm-12 col-xs-12 col-md-12 col-lg-4">
-                {this.state.sortiraniPredmetiPoBrojuIspita.length == 0 ? <span style={{ float: "left", marginLeft: "30px" }}>Student trenutno nema ocjena</span> :
+                {this.state.sortiraniPredmetiPoBrojuIspita.length == 0 ? <span style={{ float: "left", marginLeft: "30px" }}>Student nije polagao nijedan ispit</span> :
                     <table className="table table-bordered text-center bg-active border-solid" style={{ float: "left", marginLeft: "20px" }}>
                         <tbody>
                             <tr class="bg-primary text-light">

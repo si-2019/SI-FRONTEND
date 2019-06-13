d892b5bd6262da2c408b72719128d37bdb34afe1
esmagalijatovic <egalijatov1@etf.unsa.ba>
2019-06-13 13:52:01 +0200

    kreirana ikona

--- reactapp/src/CHARLIE/components/SharedComponents/IspitCard.js
+++ reactapp/src/CHARLIE/components/SharedComponents/IspitCard.js
@@ -1,15 +1,33 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
 class IspitCard extends Component {
+  dajBoju (el) {
+    var ONEDAY = 1000 * 60 * 60 * 24;
+    var danas = new Date().getTime()
+    var difference_ms = Math.abs(el.rokPrijave - danas);
+    var brDana = Math.round(difference_ms/ONEDAY);
+    if(brDana > 2){
+      return "#98f442"
+    }
+    else if(brDana > 0)
+      return "#f4f442" 
+    else 
+      return "#ce0e0e"
+  }
   renderIspit = () =>
     this.props.ispiti
       .filter(ispit => ispit.tipIspita === this.props.tipIspita)
       .map(el => (
-        <li key={el.idIspit}>
-          {el.idPredmet}
-          <button type="button" class="btn btn-primary" style={{float:"right"}}>
-                  Info o ispitu
-            </button>
+        <li key={el.idIspit} style={{width:"100%", marginBottom:"20px"}}>
+            <div style={{width:"100%", display:"flex"}}>
+              <div style={{width:"10%"}}>
+                <div style={{borderRadius:"50px", width:"15px", height:"15px", backgroundColor:this.dajBoju(el)}}></div>
+              </div>
+              <div style={{width:"45%"}} >{el.idPredmet}</div>
+              <button style={{width:"45%"}} type="button" class="btn btn-primary" >
+                        Info o ispitu
+              </button>
+            </div>
         </li>
       ));
 

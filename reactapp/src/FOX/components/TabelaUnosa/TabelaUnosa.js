9e6c3374cd3c1715185d5d7f5e7c40331c7fdeeb
Nedim Tarakčija <nedimt@hotmail.com>
2019-06-13 21:05:30 +0200

    Dodan alert u komponentu za unos bodova.

--- reactapp/src/FOX/components/TabelaUnosa/TabelaUnosa.js
+++ reactapp/src/FOX/components/TabelaUnosa/TabelaUnosa.js
@@ -3,18 +3,20 @@ import Col from 'react-bootstrap/Col';
 import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
 import axios from 'axios';
+import Poruka from '../Poruka/Poruka'
+
 class TabelaUnosa extends Component {
-    constructor(props){
-        super(props);
-        this.state ={
-            validated: false,
-            greskaBaza: 0
-        }
-        this.ocjena=React.createRef();
+    state = {
+        validated: false,
+        greskaBaza: 0
     }
-    handleSubmit(event) {
+    //this.ocjena=React.createRef();
+
+    handleSubmit = (event) => {
+        event.preventDefault();
         const form = event.currentTarget;
         console.log(event.currentTarget.checkValidity());
+
         if (form.checkValidity() === false) {
             event.preventDefault();
             event.stopPropagation();
@@ -24,38 +26,42 @@ class TabelaUnosa extends Component {
             //Promise
             /* 3. Get Ref Value here (or anywhere in the code!) */
            
-            console.log(this.ocjena.current.value);
             let reqBody = {
               /*  idStudent: 2, //pristup lokalnom storage-u
                 idPredmet: 64,
                 idAkademskaGodina: 11,
                 ocjena: this.ocjena.current.value*/
             };
-            if(this.ocjena.current.value>10 || this.ocjena.current.value<6)   this.setState({ greskaBaza: 1 });
-            console.log(this.ocjena.current.value);
+            
+            //if(this.ocjena.current.value>10 || this.ocjena.current.value<6)   this.setState({ greskaBaza: 1 });
+            
             axios.post('', reqBody)
-            .then((res) => {
-                console.log(res);
+            .then(() => {
                 this.setState({greskaBaza: 2});
             })
-            .catch((err)=> {
-                console.log("Greska:" + err);
+            .catch(()=> {
                 this.setState({greskaBaza: 1});
             });
-       }
+        }
         this.setState({ validated: true });
-        event.preventDefault();
     }
      
     render() {
         return(
             <div class="card" style={{margin: "0"}}>
                 <div class="card-body">
+                    <Poruka
+                    greska={this.state.greskaBaza}
+                    naslovUspjeh="Bodovi sačuvani!"
+                    naslovGreska="Bodovi nisu sačuvani!"
+                    opisUspjeh="Bodovi su dodani u bazu podataka."
+                    opisGreska="Baza podataka nije dostupna."
+                    />
                     <h4 class="card-title text-center" >Unos bodova ispita</h4>
                     <h6 class="card-subtitle mb-2 text-muted text-center">Omogućava pretraživanje studenata i unos bodova.</h6>
                     <br/>
                     <div>
-                        <Form>
+                        <Form onSubmit={this.handleSubmit}>
 
                             <Form.Row className="justify-content-center">
                                 <Form.Group as={Col} lg="4" md="6" sm="8" xs="12" style={{textAlign: "left"}}>
@@ -86,6 +92,8 @@ class TabelaUnosa extends Component {
 
                             <Form.Row>
                                 <Col style={{textAlign: "center"}}>
+                                    <br/>
+                                    {/* Ovdje ubaciti prikaz pretraživanja */}
                                 </Col>
                             </Form.Row>
 
@@ -101,13 +109,14 @@ class TabelaUnosa extends Component {
 
                             <Form.Row style={{paddingTop: "10px"}} className="justify-content-center">
                                 <Col lg="4" md="6" sm="8" xs="12" style={{textAlign: "right"}}>
-                                    <Button onClick={this.handleCli}> Unesi </Button>
+                                    <Button type="submit"> Unesi </Button>
                                 </Col>
                             </Form.Row>
 
                             <Form.Row>
                                 <Col style={{textAlign: "center"}}>
                                     <br/>
+                                    {/* Ovdje ubaciti prikaz unosa */}
                                 </Col>
                             </Form.Row>
                     </Form>

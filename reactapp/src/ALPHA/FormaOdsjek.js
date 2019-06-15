cba279ed895e68fd221be027389ce2539a534d2d
ATalic2 <azurtalic@yahoo.com>
2019-06-15 18:22:47 +0200

    Update FormaOdsjek.js

--- reactapp/src/ALPHA/FormaOdsjek.js
+++ reactapp/src/ALPHA/FormaOdsjek.js
@@ -34,7 +34,7 @@ class FormaOdsjek extends Component {
         console.log(token);
           console.log(currentUsername);
         //Drugi URL
-        xhr.open('POST', 'http://localhost:31901/api/odsjek/AddNewOdsjek?currentUsername=' + currentUsername + '?token=' + token, true);
+        xhr.open('POST', 'https://si2019alpha.herokuapp.com/api/odsjek/AddNewOdsjek?currentUsername=' + currentUsername + '?token=' + token, true);
         xhr.setRequestHeader('Content-Type', 'application/json');
         xhr.onload = () => {
           if(xhr.status === 200) {

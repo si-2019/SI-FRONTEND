8030b164a25b80fe40cbb56e4915ebe84536c6bb
amirmu-at-587105673553 <amuminovic3@etf.unsa.ba>
2019-06-15 13:24:51 +0200

    Dodana autorizacija

--- reactapp/src/CHARLIE/app.js
+++ reactapp/src/CHARLIE/app.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import { BrowserRouter, Route } from "react-router-dom";
+import axios from 'axios'
 
 import InformacijeOIspitu from "./components/InformacijeOIspitu";
 import KreirajIspit from "./components/KreirajIspit";
@@ -29,7 +30,21 @@ class App extends Component {
       }, {
         btnText: "Kreiraj ispit detalji",
         component: <KreirajIspitDetalji />
-      }, {
+      }],
+      menuComponentsProfesor: [{
+        naziv: "Profil",
+        changeId: 0,
+        component: <KreirajIspit /> //Prof
+      },{
+        naziv: "Profil",
+        changeId: 1,
+        component: <KreiraniIspiti /> //Prof
+      },{
+        naziv: "Profil",
+        changeId: 2,
+        component: <KreirajIspitDetalji/> //Prof
+      }],
+      menuButtonsStudent: [{
         btnText: "Informacije o ispitu",
         component: <InformacijeOIspitu />
       }, {
@@ -39,42 +54,19 @@ class App extends Component {
         btnText: "Prijavljeni ispiti",
         component: <PrijavljeniIspiti />
       }],
-      menuComponentsProfesor: [{
-        naziv: "Profil",
-        changeId: 0,
-        component: <KreirajIspit />
-      },{
-        naziv: "Profil",
-        changeId: 1,
-        component: <KreiraniIspiti />
-      },{
-        naziv: "Profil",
-        changeId: 2,
-        component: <KreirajIspitDetalji/>
-      },{
-        naziv: "Profil",
-        changeId: 3,
-        component: <InformacijeOIspitu />
-      }, {
-        naziv: "Profil",
-        changeId: 4,
-        component: <PrijavaIspita />
-      }, {
-        naziv: "Profil",
-        changeId: 5,
-        component: <PrijavljeniIspiti />
-      }],
-      menuButtonsStudent: [{
-        btnText: "Prijava ispita",
-        component: <PrijavaIspita />
-      }, {
-        btnText: "Prijavljeni ispiti",
-        component: <PrijavljeniIspiti />
-      }],
       menuComponentsStudent: [{
         naziv: "Profil",
         changeId: 0,
-        component: <PrijavaIspita />
+        component: <PrijavaIspita /> // Stud
+      }, {
+        naziv: "Profil",
+        changeId: 1,
+        component: <PrijavljeniIspiti /> //Stud
+      },
+      {
+        naziv: "Profil",
+        changeId: 2,
+        component: <InformacijeOIspitu /> // Stud
       }],
     }
     this.onChangeActiveId = this.onChangeActiveId.bind(this);
@@ -114,12 +106,33 @@ class App extends Component {
       activeContentId: id,
     })
   };
+
+  isProfesor = async() =>{
+    const id = window.localStorage.getItem("id") || 1;
+    const token = window.localStorage.getItem("token");
+    const username = window.localStorage.getItem("username");
+
+    // axios.get('https://si2019romeo.herokuapp.com/users/validate', username, {
+    //   "Authorization": token
+    // })
+    const uloga = 'PROFESOR';
+    return false
+    //Kad oscar rijesi cors, ubaci ovaj dio
+    // try {
+    //   return false
+    //   const {data, status} = await axios.get(`https://si2019oscar.herokuapp.com/pretragaId/imaUlogu/${id}/${uloga}`)
+    //   return data  
+    // } catch (error) {
+    //   console.log(error.message)
+    // }
+    
+  }
+
   render() {
     return (
       <>
       
         <div className="App" id="appCharlie">
-      
           <div className="containter-fluid">
             <div className="row" id="rowCharlie" style={{ margin: "0px", padding: "0px"}}>
               <div className="col-lg-2 col-md-3 col-sm-12" style={{
@@ -130,7 +143,8 @@ class App extends Component {
               }}>
                 <LeftMenuCharlie
                   triggerChangeActiveId={this.onChangeActiveId}
-                  btnList={this.state.menuComponentsProfesor}
+                  btnList={this.isProfesor() && this.state.menuComponentsProfesor
+                    || this.state.menuComponentsStudent}
                 />
               </div>
               <div className="col-lg flex-grow-1 col-sm-12 col-md" style={{
@@ -139,10 +153,9 @@ class App extends Component {
                 margin: "0px",
                 padding: "0px"
               }}>
-
-                {this.state.menuComponentsProfesor[this.state.activeContentId].component}
-
-
+                { this.isProfesor() && this.state.menuComponentsProfesor[this.state.activeContentId].component
+                  || this.state.menuComponentsStudent[this.state.activeContentId].component
+                }
               </div>
             </div>
           </div>

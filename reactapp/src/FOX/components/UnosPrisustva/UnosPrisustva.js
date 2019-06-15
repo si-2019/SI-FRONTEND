c9cbdde843c3193d7d1704e0fbe876df4e803f63
Nedim Tarakčija <nedimt@hotmail.com>
2019-06-15 20:16:32 +0200

    Izmijenjen redoslijed ažuriranja state-a u komponenti za unos prisustva.

--- reactapp/src/FOX/components/UnosPrisustva/UnosPrisustva.js
+++ reactapp/src/FOX/components/UnosPrisustva/UnosPrisustva.js
@@ -137,9 +137,7 @@ class UnosPrisustva extends React.Component {
     }
 
     handleClickSedmica = (brojSedmice) => {
-        this.setState({sedmica: brojSedmice});
-
-        const idPredmeta = window.localStorage.getItem('idPredmeta') !== null ? window.localStorage.getItem("idPredmeta") : 101;
+        const idPredmeta = window.localStorage.getItem('idPredmeta') !== null ? window.localStorage.getItem("idPredmeta") : 100;
 
         axios.get(`https://si2019fox.herokuapp.com/api/fox/prisustvo?idPredmeta=${idPredmeta}&brojSedmice=${brojSedmice}`).then(response => {
             let studenti = response.data.map(s => {
@@ -156,6 +154,8 @@ class UnosPrisustva extends React.Component {
         })
         .catch(()=> {
         });
+
+        this.setState({sedmica: brojSedmice});
     }
 
     handleNazad = () => {

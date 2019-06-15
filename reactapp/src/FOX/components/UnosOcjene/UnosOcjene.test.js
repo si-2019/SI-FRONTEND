8f664734769c433410ec54150499f4be0b9ff438
Sejla Pljakic <sejla.pljakic@gmail.com>
2019-06-15 13:33:28 +0200

    Unit testovi gotovi

--- reactapp/src/FOX/components/UnosOcjene/UnosOcjene.test.js
+++ reactapp/src/FOX/components/UnosOcjene/UnosOcjene.test.js
@@ -5,7 +5,7 @@ import UnosOcjene  from '.UnosOcjene'
 describe('<UnosOcjene />', () => {
   it('renderuje formu za unos ocjene', () => {
     const wrapper = shallow(<UnosOcjene/>)
-    expect(wrapper.find('form').exists()).toBe(true)
+    expect(wrapper.find('#form').exists()).toBe(true)
   })
   it('da li ima container za ocjenu', () => {
     const wrapper = shallow(<UnosOcjene/>)

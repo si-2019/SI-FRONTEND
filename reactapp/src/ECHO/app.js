ab60498c15eb2644b2b07e1b868edda5f08b42e5
ajajcanin <jajcanin.ajdin@gmail.com>
2019-06-16 00:16:04 +0200

    prep merge with develop

--- reactapp/src/ECHO/app.js
+++ reactapp/src/ECHO/app.js
@@ -1,15 +1,20 @@
-import React, { Component } from "react";
-import PocetnaStranica from "./pocetnaStranica";
-import "./app.css";
-class App extends Component {
-  render() {
-    return (
-      <div id="glavniDivEcho">
-        <h1>ECHO</h1>
-        <PocetnaStranica />
-      </div>
-    );
-  }
+import React from 'react';
+import { BrowserRouter, Route } from "react-router-dom";
+
+import ProfessorsAvailability from "./ProfessorsAvailability/ProfessorsAvailability";
+
+class App extends React.Component {
+    render() {
+        return (
+            <div className="container">
+                <BrowserRouter>
+
+                    <Route path="/echo" exact component={ProfessorsAvailability} />
+                    <Route path="/echo/professors-availability" exact component={ProfessorsAvailability} />
+                </BrowserRouter>
+            </div>
+        );
+    }
 }
 
 export default App;

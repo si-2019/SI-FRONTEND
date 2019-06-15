a3f2ade6a24f634ab2f2fb41788129596ba66c0b
Sejla Pljakic <sejla.pljakic@gmail.com>
2019-06-15 13:32:52 +0200

    Unit testovi

--- reactapp/src/FOX/components/UnosBodova/UnosBodova.test.js
+++ reactapp/src/FOX/components/UnosBodova/UnosBodova.test.js
@@ -5,7 +5,7 @@ import UnosBodova  from '.UnosBodova'
 describe('<UnosBodova />', () => {
   it('renderuje formu za unos bodova', () => {
     const wrapper = shallow(<UnosBodova/>)
-    expect(wrapper.find('form').exists()).toBe(true)
+    expect(wrapper.find('#form').exists()).toBe(true)
   })
   it('da li ima container za bodove', () => {
     const wrapper = shallow(<UnosBodova/>)

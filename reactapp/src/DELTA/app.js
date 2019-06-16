96be57f6d3a63b06d4a228d7fc3a23868a751f6f
asijah1 <asijah1@etf.unsa.ba>
2019-06-16 14:10:31 +0200

    provjera tokena

--- reactapp/src/DELTA/app.js
+++ reactapp/src/DELTA/app.js
@@ -1,9 +1,36 @@
 import React from 'react';
 import { BrowserRouter, Route } from "react-router-dom";
-
+import { Redirect } from 'react-router-dom';
 import Predmet from "./components/Predmet";
 
 class App extends React.Component {
+  constructor(props) {
+    super(props)
+    const token = localStorage.getItem("token")
+
+    let logiran = true
+    if(token == null) {
+      logiran = false
+    }
+
+    this.state = {
+      korisnickoIme: '',
+      sifra: '',
+      logiran
+    }
+  }
+
+  componentDidMount() {
+    if(this.state.logiran == true) {
+      //ubaciti backend provjeru validnosti tokena
+      return <Redirect to="/Delta" />
+    }
+    else{
+        return <Redirect to="/Romeo" />
+    }
+  }
+  //provjera tokena na frontendu, dio za backend uraditi tj. dio vezan za validnost tokena
+
   render() {
     return (
       <div className="container">
